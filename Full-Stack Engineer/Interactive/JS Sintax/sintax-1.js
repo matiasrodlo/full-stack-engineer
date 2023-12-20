0e42f5b657ea51999858b63ea262b88a6ea27e65
Matías Rodlo <m@hablaqui.com>
2023-12-20 19:31:49 -0300

    Update sintax-1.js

--- Full-Stack Engineer/Interactive/JS Sintax/sintax-1.js
+++ Full-Stack Engineer/Interactive/JS Sintax/sintax-1.js
@@ -33,7 +33,38 @@ quarterMe /= 4;
 
 // These console.log() statements below will help you check the values of the variables.
 // You do not need to edit these statements.
+
 console.log("The value of levelUp:", levelUp);
 console.log("The value of powerLevel:", powerLevel);
 console.log("The value of multiplyMe:", multiplyMe);
 console.log("The value of quarterMe:", quarterMe);
+
+////
+
+let favoriteAnimal = "giraffe";
+console.log("My favorite animal: " + favoriteAnimal);
+
+////
+
+let gainedDollar = 3;
+let lostDollar = 50;
+
+gainedDollar++;
+lostDollar--;
+
+////
+
+const myName = "Matias";
+const myCity = "New York";
+
+console.log(`My name is ${myName}. My favorite city is ${myCity}.`);
+
+////
+
+let newVariable = "Playing around with typeof.";
+
+console.log(typeof newVariable);
+
+newVariable = 1;
+
+console.log(typeof newVariable);
