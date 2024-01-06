f84e058f0203dddcbfa87c56a48ee534dd9ceddf
Matías Rodlo <m@hablaqui.com>
2024-01-06 19:11:17 -0300

    Create truthyOrFalsy.js

---
+++ Full-Stack Engineer/Interactive/JS Sintax/challenges/truthyOrFalsy.js
@@ -0,0 +1,10 @@
+// Write your function here:
+
+function truthyOrFalsy(value) {
+  return Boolean(value);
+}
+
+// Uncomment the line below when you're ready to try out your function
+console.log(truthyOrFalsy(0)); // Should print false
+
+// We encourage you to add more function calls of your own to test your code!
