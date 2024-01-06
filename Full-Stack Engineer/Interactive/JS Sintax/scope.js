4eb3be34996d2397b34d5f01f406b4dc02a942d5
Matías Rodlo <m@hablaqui.com>
2024-01-05 22:04:36 -0300

    Update scope.js

--- Full-Stack Engineer/Interactive/JS Sintax/scope.js
+++ Full-Stack Engineer/Interactive/JS Sintax/scope.js
@@ -29,3 +29,17 @@ function logVisibleLightWaves() {
 logVisibleLightWaves(); // Moonlight
 
 console.log(lightWaves); // Error variable not defined
+
+///////////////////////////////////////////////////////////
+
+const satellite = "The Moon";
+const galaxy = "The Milky Way";
+let stars = "North Star";
+
+const callMyNightSky = () => {
+  stars = "Sirius";
+  return "Night Sky: " + satellite + ", " + stars + ", " + galaxy;
+};
+
+console.log(callMyNightSky());
+console.log(stars);
