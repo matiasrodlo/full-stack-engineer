1dda7fe1e0219f241c54a7f34ac0173c8cace2c3
Matías Rodlo <m@hablaqui.com>
2024-01-06 17:36:16 -0300

    Create canIVote.js

---
+++ Full-Stack Engineer/Interactive/JS Sintax/challenges/canIVote.js
@@ -0,0 +1,10 @@
+// Write your function here:
+
+function canIVote(age) {
+  return age >= 18;
+}
+
+// Uncomment the line below when you're ready to try out your function
+console.log(canIVote(12)); // Should print true
+
+// We encourage you to add more function calls of your own to test your code!
