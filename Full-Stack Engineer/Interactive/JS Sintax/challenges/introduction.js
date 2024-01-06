818520868ddc3c8edfdd99e5ec206845b69dce50
Matías Rodlo <m@hablaqui.com>
2024-01-06 17:27:01 -0300

    Create introduction.js

---
+++ Full-Stack Engineer/Interactive/JS Sintax/challenges/introduction.js
@@ -0,0 +1,9 @@
+// Write your function here:
+
+function greetWorld() {
+  return "Hello, World!";
+}
+
+greetWorld();
+
+//Write code to test your function. Even if we provide code to test your function, you should add further tests to make sure your function works correctly in all the specified situations
