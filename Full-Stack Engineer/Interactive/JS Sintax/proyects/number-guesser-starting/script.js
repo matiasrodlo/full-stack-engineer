8a3cb7142e489d2ab58e3177f23554763e216ca4
Matías Rodlo <m@hablaqui.com>
2024-01-06 22:04:15 -0300

    Update script.js

--- Full-Stack Engineer/Interactive/JS Sintax/proyects/number-guesser-starting/script.js
+++ Full-Stack Engineer/Interactive/JS Sintax/proyects/number-guesser-starting/script.js
@@ -4,3 +4,21 @@ let currentRoundNumber = 1;
 
 // Write your code below:
 
+function generateTarget() {
+  return Math.floor(Math.random() * 10);
+}
+
+// generateTarget();
+
+function compareGuesses(humanGuess, computerGuess, secretGuess) {
+  let humanGuessDif = humanGuess - secretGuess;
+  let computerGuessDif = computerGuess - secretGuess;
+
+  if (humanGuess < computerGuess) {
+    return "true";
+  } else {
+    return "false";
+  }
+}
+
+console.log(compareGuesses(4, 9, 3));
