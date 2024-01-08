5f51e2c9396c63670b45edb52fdfa070e69b8197
Matías Rodlo <m@hablaqui.com>
2024-01-08 20:53:41 -0300

    Update loops.js

--- Full-Stack Engineer/Interactive/JS Sintax 2/loops.js
+++ Full-Stack Engineer/Interactive/JS Sintax 2/loops.js
@@ -41,3 +41,15 @@ for (let i = 0; i < bobsFollowers.length; i++) {
 }
 
 console.log(mutualFollowers);
+
+///////////////////////////////////////////////////
+
+const cards = ["diamond", "spade", "heart", "club"];
+
+// Write your code below
+
+let currentCard = "";
+while (currentCard !== "spade") {
+  currentCard = cards[Math.floor(Math.random() * 4)];
+  console.log(currentCard);
+}
