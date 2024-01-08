e66be4aa88cd78f080aa061fd6fe17e1b632ec8e
Matías Rodlo <m@hablaqui.com>
2024-01-08 16:07:58 -0300

    Update arrays.js

--- Full-Stack Engineer/Interactive/JS Sintax 2/arrays.js
+++ Full-Stack Engineer/Interactive/JS Sintax 2/arrays.js
@@ -127,3 +127,13 @@ removeElement(concept);
 console.log(removeElement(concept));
 
 ////////////////////////////////////////////////////
+
+const numberClusters = [
+  [1, 2],
+  [3, 4],
+  [5, 6],
+];
+
+const target = numberClusters[2][1];
+
+console.log(target);
