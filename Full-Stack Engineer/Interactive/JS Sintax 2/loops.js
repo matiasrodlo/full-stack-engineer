a7821a40279393326557d7a02cb003fe32a1f5a6
Matías Rodlo <m@hablaqui.com>
2024-01-08 19:38:50 -0300

    Create loops.js

---
+++ Full-Stack Engineer/Interactive/JS Sintax 2/loops.js
@@ -0,0 +1,13 @@
+// Write your code below
+
+var vacationSpots = ["New York", "Paris", "London"];
+
+console.log(vacationSpots[0]);
+console.log(vacationSpots[1]);
+console.log(vacationSpots[2]);
+
+///////////////////////////////////////////////////
+
+for (let i = 5; i < 11; i++) {
+  console.log(i);
+}
