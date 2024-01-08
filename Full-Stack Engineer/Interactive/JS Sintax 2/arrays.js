34c2820d3ce01ad829ca2879b52bf89c32505c90
Matías Rodlo <m@hablaqui.com>
2024-01-07 22:49:50 -0300

    Update arrays.js

--- Full-Stack Engineer/Interactive/JS Sintax 2/arrays.js
+++ Full-Stack Engineer/Interactive/JS Sintax 2/arrays.js
@@ -11,3 +11,23 @@ console.log(newYearsResolutions);
 const hobbies = ["hola", "hola", "hola"];
 
 console.log(hobbies);
+
+////////////////////////////////////////////////////
+
+const famousSayings = [
+  "Fortune favors the brave.",
+  "A joke is a very serious thing.",
+  "Where there is love there is life.",
+];
+
+const listItem = famousSayings[0];
+
+console.log(famousSayings[2]);
+
+console.log(famousSayings[3]);
+
+////////////////////////////////////////////////////
+
+let groceryList = ["bread", "tomatoes", "milk"];
+
+groceryList[1] = "avocados";
