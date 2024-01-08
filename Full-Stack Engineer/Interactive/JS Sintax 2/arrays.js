0181f3ac03d48cd7bd77ffcc374b20b1f142a39f
Matías Rodlo <m@hablaqui.com>
2024-01-08 15:21:12 -0300

    Update arrays.js

--- Full-Stack Engineer/Interactive/JS Sintax 2/arrays.js
+++ Full-Stack Engineer/Interactive/JS Sintax 2/arrays.js
@@ -77,3 +77,31 @@ const chores = [
 chores.pop();
 
 console.log(chores);
+
+////////////////////////////////////////////////////
+
+const groceryList = [
+  "orange juice",
+  "bananas",
+  "coffee beans",
+  "brown rice",
+  "pasta",
+  "coconut oil",
+  "plantains",
+];
+
+groceryList.shift();
+
+console.log(groceryList);
+
+groceryList.unshift("popcorn");
+
+console.log(groceryList);
+
+console.log(groceryList.slice(1, 4));
+
+console.log(groceryList);
+
+const pastaIndex = groceryList.indexOf("pasta");
+
+console.log(pastaIndex);
