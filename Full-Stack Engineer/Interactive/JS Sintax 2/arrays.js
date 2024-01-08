5bac8d3f8aa30868635332c918cb798baee2c667
Matías Rodlo <m@hablaqui.com>
2024-01-08 15:58:49 -0300

    Update arrays.js

--- Full-Stack Engineer/Interactive/JS Sintax 2/arrays.js
+++ Full-Stack Engineer/Interactive/JS Sintax 2/arrays.js
@@ -105,3 +105,25 @@ console.log(groceryList);
 const pastaIndex = groceryList.indexOf("pasta");
 
 console.log(pastaIndex);
+
+////////////////////////////////////////////////////
+
+const concept = ["arrays", "can", "be", "mutated"];
+
+function changeArr(arr) {
+  arr[3] = "MUTATED";
+}
+
+changeArr(concept);
+
+console.log(concept);
+
+function removeElement(newArr) {
+  newArr.pop();
+}
+
+removeElement(concept);
+
+console.log(removeElement(concept));
+
+////////////////////////////////////////////////////
