c7fc6e621c2ba2585608434ac540349aece87bca
Matías Rodlo <m@hablaqui.com>
2024-01-08 20:36:27 -0300

    Update loops.js

--- Full-Stack Engineer/Interactive/JS Sintax 2/loops.js
+++ Full-Stack Engineer/Interactive/JS Sintax 2/loops.js
@@ -11,3 +11,33 @@ console.log(vacationSpots[2]);
 for (let i = 5; i < 11; i++) {
   console.log(i);
 }
+
+///////////////////////////////////////////////////
+
+for (let counter = 3; counter >= 0; counter--) {
+  console.log(counter);
+}
+
+///////////////////////////////////////////////////
+
+const vacationSpots = ["Bali", "Paris", "Tulum"];
+
+for (let i = 0; i < vacationSpots.length; i++) {
+  console.log("I would love to visit " + vacationSpots[i]);
+}
+
+///////////////////////////////////////////////////
+
+var bobsFollowers = ["Matias", "Mario", "Lucia", "Jaime"];
+var tinasFollowers = ["Matias", "Mario", "Carlos"];
+var mutualFollowers = [];
+
+for (let i = 0; i < bobsFollowers.length; i++) {
+  for (let t = 0; t < tinasFollowers.length; t++) {
+    if (bobsFollowers[i] === tinasFollowers[t]) {
+      mutualFollowers.push(bobsFollowers[i]);
+    }
+  }
+}
+
+console.log(mutualFollowers);
