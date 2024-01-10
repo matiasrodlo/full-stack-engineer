e693cfe5ac7f5644614b21275f230ae825de0a99
Matías Rodlo <m@hablaqui.com>
2024-01-09 21:34:55 -0300

    Update objects.js

--- Full-Stack Engineer/Interactive/JS Sintax 2/objects.js
+++ Full-Stack Engineer/Interactive/JS Sintax 2/objects.js
@@ -13,7 +13,24 @@ let spaceship = {
   flightPath: ["Venus", "Mars", "Saturn"],
 };
 
-// Write your code below
-
 let crewCount = spaceship.numCrew;
 let planetArray = spaceship.flightPath;
+
+///////////////////////////////////////////////
+
+let spaceship = {
+  "Fuel Type": "Turbo Fuel",
+  "Active Mission": true,
+  homePlanet: "Earth",
+  numCrew: 5,
+};
+
+let propName = "Active Mission";
+
+// Write your code below
+
+let isActive = spaceship["Active Mission"];
+
+console.log(spaceship[propName]);
+
+console.log(isActive);
