38974a7a4f06ae67745d1d5d6bb169c670c9ecb3
Matías Rodlo <m@hablaqui.com>
2024-01-10 16:49:26 -0300

    Update objects.js

--- Full-Stack Engineer/Interactive/JS Sintax 2/objects.js
+++ Full-Stack Engineer/Interactive/JS Sintax 2/objects.js
@@ -50,3 +50,19 @@ spaceship.numEngines = 2;
 delete spaceship["Secret Mission"];
 
 ///////////////////////////////////////////////
+
+let retreatMessage =
+  "We no longer wish to conquer your planet. It is full of dogs, which we do not care for.";
+
+// Write your code below
+const alienShip = {
+  retreat() {
+    console.log(retreatMessage);
+  },
+  takeOff() {
+    console.log("Spim... Borp... Glix... Blastoff!");
+  },
+};
+
+alienShip.retreat();
+alienShip.takeOff();
