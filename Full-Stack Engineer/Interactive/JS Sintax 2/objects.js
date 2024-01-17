a0a4887add66068df6b42a1a3a20bf3fa1e71ff9
Matías Rodlo <m@hablaqui.com>
2024-01-17 15:59:27 -0300

    Update objects.js

--- Full-Stack Engineer/Interactive/JS Sintax 2/objects.js
+++ Full-Stack Engineer/Interactive/JS Sintax 2/objects.js
@@ -229,3 +229,22 @@ const robot = {
 };
 
 console.log(robot.energyLevel);
+
+/////////////////////////////////////////////////////////
+
+const robot = {
+  _model: "1E78V2",
+  _energyLevel: 100,
+  _numOfSensors: 15,
+  set numOfSensors(num) {
+    if (typeof num === "number" && num >= 0) {
+      this._numOfSensors = num;
+    } else {
+      console.log("Pass in a number that is greater than or equal to 0");
+    }
+  },
+};
+
+robot.numOfSensors = 100;
+
+console.log(robot._numOfSensors);
