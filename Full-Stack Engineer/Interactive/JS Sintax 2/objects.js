d81e7c58057bd05c1b0e99b92e376b8874885634
Matías Rodlo <m@hablaqui.com>
2024-01-17 15:27:34 -0300

    Update objects.js

--- Full-Stack Engineer/Interactive/JS Sintax 2/objects.js
+++ Full-Stack Engineer/Interactive/JS Sintax 2/objects.js
@@ -199,3 +199,17 @@ const robot = {
 };
 
 robot.checkEnergy();
+
+/////////////////////////////////////////////////////////
+
+const robot = {
+  _energyLevel: 100,
+  recharge() {
+    this._energyLevel += 30;
+    console.log(`Recharged! Energy is currently at ${this._energyLevel}%.`);
+  },
+};
+
+robot._energyLevel = "high";
+
+robot.recharge();
