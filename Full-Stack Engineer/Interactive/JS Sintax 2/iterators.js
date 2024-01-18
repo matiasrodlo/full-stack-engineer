5252f73b3ff272eee886266d793b7948a57d19eb
Matías Rodlo <m@hablaqui.com>
2024-01-18 19:32:34 -0300

    Update iterators.js

--- Full-Stack Engineer/Interactive/JS Sintax 2/iterators.js
+++ Full-Stack Engineer/Interactive/JS Sintax 2/iterators.js
@@ -126,3 +126,29 @@ const newSum = newNumbers.reduce((accumulator, currentValue) => {
 }, 10);
 
 console.log(newSum);
+
+///////////////////////////////////////////////////
+
+const words = ["unique", "uncanny", "pique", "oxymoron", "guise"];
+
+// Something is missing in the method call below
+
+console.log(
+  words.some((word) => {
+    return word.length < 6;
+  })
+);
+
+// Use filter to create a new array
+
+const interestingWords = words.filter((param) => {
+  return param.length > 5;
+});
+
+// Make sure to uncomment the code below and fix the incorrect code before running it
+
+console.log(
+  interestingWords.every((word) => {
+    return word.length > 5;
+  })
+);
