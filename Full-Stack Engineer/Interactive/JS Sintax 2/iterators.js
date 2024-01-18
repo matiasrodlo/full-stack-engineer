55646b9525a5f3a313c886da57199559acfdfad4
Matías Rodlo <m@hablaqui.com>
2024-01-17 21:42:31 -0300

    Update iterators.js

--- Full-Stack Engineer/Interactive/JS Sintax 2/iterators.js
+++ Full-Stack Engineer/Interactive/JS Sintax 2/iterators.js
@@ -13,3 +13,20 @@ const isTwoPlusTwo = checkThatTwoPlusTwoEqualsFourAMillionTimes;
 isTwoPlusTwo();
 
 console.log(isTwoPlusTwo.name);
+
+///////////////////////////////////////////////////
+
+const addTwo = (num) => {
+  return num + 2;
+};
+
+const checkConsistentOutput = (func, val) => {
+  let checkA = val + 2;
+  let checkB = func(val);
+  if (checkA === checkB) {
+    return checkA;
+  } else {
+    return "inconsistent results";
+  }
+};
+console.log(checkConsistentOutput(addTwo, 2));
