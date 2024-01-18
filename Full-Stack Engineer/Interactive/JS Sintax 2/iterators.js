1193576aed29834542d62583cf82b7ec5fd91cc9
Matías Rodlo <m@hablaqui.com>
2024-01-18 17:44:32 -0300

    Update iterators.js

--- Full-Stack Engineer/Interactive/JS Sintax 2/iterators.js
+++ Full-Stack Engineer/Interactive/JS Sintax 2/iterators.js
@@ -123,6 +123,6 @@ const newSum = newNumbers.reduce((accumulator, currentValue) => {
   console.log("The value of accumulator: ", accumulator);
   console.log("The value of currentValue: ", currentValue);
   return accumulator + currentValue;
-});
+}, 10);
 
 console.log(newSum);
