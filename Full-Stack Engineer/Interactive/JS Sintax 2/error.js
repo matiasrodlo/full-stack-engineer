362f7a0cfd4bd2a701ced7186a84ae96eacab3fe
Matías Rodlo <m@hablaqui.com>
2024-01-19 15:00:29 -0300

    Update error.js

--- Full-Stack Engineer/Interactive/JS Sintax 2/error.js
+++ Full-Stack Engineer/Interactive/JS Sintax 2/error.js
@@ -71,4 +71,32 @@ function capitalizeASingleWord(word) {
 console.log("capitalizeASingleWord('hey') returns: " + capitalizeASingleWord('hey')); 
 
 // Should return null
-console.log("capitalizeASingleWord('hey ho') returns: " + capitalizeASingleWord('hey ho'));
+    console.log("capitalizeASingleWord('hey ho') returns: " + capitalizeASingleWord('hey ho'));
+
+///////////////////////////////////////////////////////////
+
+// Returns the string whose first letter is later in the alphabet. If both first letters are equal, returns null.
+function getLaterFirstLetter(string1, string2) {
+  const firstLetter1 = string1.charAt(0);
+  const firstLetter2 = string2.charAt(0);
+  
+  console.log(firstLetter1);
+  console.log(firstLetter2);
+  
+  if (firstLetter1 === firstLetter2) {
+    return null;
+  } else if (firstLetter1 < firstLetter2) {
+    return string2;
+  } else {
+    return string1;
+  }
+}
+
+// Should return "blueberry"
+console.log("getLaterFirstLetter('avocado', 'blueberry') returns: " + getLaterFirstLetter('avocado', 'blueberry'));
+
+// Should return "zebra"
+console.log("getLaterFirstLetter('zebra', 'aardvark') returns : " + getLaterFirstLetter('zebra', 'aardvark'));
+
+// Should return null
+console.log("getLaterFirstLetter('astro', 'afar') returns: " + getLaterFirstLetter('astro', 'afar'));    
