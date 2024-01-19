3bb884390d192a9c1516a1118a5e8968335bf606
Matías Rodlo <m@hablaqui.com>
2024-01-19 14:08:29 -0300

    Update error.js

--- Full-Stack Engineer/Interactive/JS Sintax 2/error.js
+++ Full-Stack Engineer/Interactive/JS Sintax 2/error.js
@@ -21,3 +21,12 @@ What is the description of the error in this stack trace?
 
 ///////////////////////////////////////////////////////////
 
+myVariable++;
+// 1 - What type of error will be thrown on the line above: 
+ReferenceError
+const myString = 42;
+myString.substring(0);
+// 2 - What type of error will be thrown on the line above: 
+TypeError
+const myRandomNumber; = Math.random();
+// 3 - What type of error will be thrown on the line above: 
