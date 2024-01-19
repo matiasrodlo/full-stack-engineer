805d258557ff5ccb112793f92ff06543cbf4ec87
Matías Rodlo <m@hablaqui.com>
2024-01-19 14:34:13 -0300

    Update error.js

--- Full-Stack Engineer/Interactive/JS Sintax 2/error.js
+++ Full-Stack Engineer/Interactive/JS Sintax 2/error.js
@@ -48,3 +48,27 @@ console.log('isSumBigger(1, 3, 2) returns: ' + isSumBigger(1, 3, 2));
 
 // Should return false
 console.log('isSumBigger(1, 3, 5) returns: ' + isSumBigger(1, 3, 5));
+
+
+///////////////////////////////////////////////////////////
+
+function capitalizeASingleWord(word) {
+  //console.log(word)
+  if (word.match(' ')) {
+    console.log(word)
+    return null;
+  }
+  
+  let firstLetter = word.charAt(0);
+  const restOfWord = word.slice(1);
+  
+  firstLetter = firstLetter.toUpperCase();
+  
+  return firstLetter + restOfWord;
+}
+
+// Should return "Hey"
+console.log("capitalizeASingleWord('hey') returns: " + capitalizeASingleWord('hey')); 
+
+// Should return null
+console.log("capitalizeASingleWord('hey ho') returns: " + capitalizeASingleWord('hey ho'));
