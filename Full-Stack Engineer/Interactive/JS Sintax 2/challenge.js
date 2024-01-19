c466badfc59e1ebbf00fea33630106a5112aac1c
Matías Rodlo <m@hablaqui.com>
2024-01-19 18:55:59 -0300

    Update challenge.js

--- Full-Stack Engineer/Interactive/JS Sintax 2/challenge.js
+++ Full-Stack Engineer/Interactive/JS Sintax 2/challenge.js
@@ -148,3 +148,47 @@ function sortYears(years) {
 const yearsArray = [1990, 2005, 1980, 2015, 1975];
 console.log(sortYears(yearsArray));
 // Example output: [2015, 2005, 1990, 1980, 1975]
+
+////////////////////////////////////////////////////////////
+
+const justCoolStuff = (firstArray, secondArray) =>
+  firstArray.filter((item) => secondArray.includes(item));
+
+/*
+// As a function declaration AND using named function w/ a loop:
+function justCoolStuff(firstArray, secondArray) {
+      function isInSecondArray(item){
+            for(let i = 0; i<secondArray.length; i++){
+                  if (secondArray[i] === item){
+                        return true
+                  }
+            }
+            return false 
+      }
+      return firstArray.filter(isInSecondArray)
+}
+*/
+
+const coolStuff = [
+  "gameboys",
+  "skateboards",
+  "backwards hats",
+  "fruit-by-the-foot",
+  "pogs",
+  "my room",
+  "temporary tattoos",
+];
+
+const myStuff = [
+  "rules",
+  "fruit-by-the-foot",
+  "wedgies",
+  "sweaters",
+  "skateboards",
+  "family-night",
+  "my room",
+  "braces",
+  "the information superhighway",
+];
+
+console.log(justCoolStuff(myStuff, coolStuff));
