4edd2768043c02e721249fa1abe63ca41fb404a3
Matías Rodlo <m@hablaqui.com>
2024-01-19 14:09:56 -0300

    Update error.js

--- Full-Stack Engineer/Interactive/JS Sintax 2/error.js
+++ Full-Stack Engineer/Interactive/JS Sintax 2/error.js
@@ -30,3 +30,21 @@ myString.substring(0);
 TypeError
 const myRandomNumber; = Math.random();
 // 3 - What type of error will be thrown on the line above: 
+
+///////////////////////////////////////////////////////////
+
+function isSumBigger(number1, number2, total) {
+  const sum = number1 + number2;
+  
+  if (sum > total) {
+    return true;
+  } else {
+    return false;
+  }
+}
+
+// Should return true
+console.log('isSumBigger(1, 3, 2) returns: ' + isSumBigger(1, 3, 2));
+
+// Should return false
+console.log('isSumBigger(1, 3, 5) returns: ' + isSumBigger(1, 3, 5));
