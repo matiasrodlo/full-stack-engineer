9d7279134b56c39a365d162be0a5b7c45ba9e2f9
Matías Rodlo <m@hablaqui.com>
2024-01-20 13:47:50 -0300

    Update challenge.js

--- Full-Stack Engineer/Interactive/JS Sintax 2/challenge.js
+++ Full-Stack Engineer/Interactive/JS Sintax 2/challenge.js
@@ -318,3 +318,13 @@ function dogFactory(name, breed, weight) {
     },
   };
 }
+
+////////////////////////////////////////////////////////////
+
+function factorial(num) {
+  let result = 1;
+  for (let i = 1; i <= num; i++) {
+    result *= i;
+  }
+  return result;
+}
