2d8ec6575ee9bd6081e02cda1c553c19d8914dd7
Matías Rodlo <m@hablaqui.com>
2024-01-22 19:02:00 -0300

    dom style element

--- Full-Stack Engineer/Interactive/DOM/script.js
+++ Full-Stack Engineer/Interactive/DOM/script.js
@@ -50,3 +50,9 @@ document.getElementById('fourth').innerHTML = 'Professor Snape'
 document.getElementsByClassName('slytherin')[0].innerHTML = 'Salazar Slytherin';
 
 document.getElementsByTagName('li')[0].innerHTML = 'Dobby';
+
+///////////////////////////////////////////////////////////
+
+document.querySelector('body').style.backgroundColor = '#201F2E';
+
+document.querySelector('.heading').style.fontFamily = 'Roboto';
