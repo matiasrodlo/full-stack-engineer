beb250c89398601cea8ffac590aaabc8bfcfdb45
Matías Rodlo <m@hablaqui.com>
2024-01-23 15:32:24 -0300

    event type

--- Full-Stack Engineer/Interactive/DOM/Events/main.js
+++ Full-Stack Engineer/Interactive/DOM/Events/main.js
@@ -82,3 +82,25 @@ let sharePhoto = function (event) {
 };
 
 share.addEventListener("click", sharePhoto);
+
+///////////////////////////////////////////////////////////
+
+// This variable stores the "Pick a Color" button
+let button = document.getElementById("color-button");
+
+// This variable stores the "Mystery Color" button
+let mysteryButton = document.getElementById("next-button");
+
+// This random number function will create color codes for the randomColor variable
+function colorValue() {
+  return Math.floor(Math.random() * 256);
+}
+
+function colorChange(event) {
+  let randomColor =
+    "rgb(" + colorValue() + "," + colorValue() + "," + colorValue() + ")";
+  event.target.style.backgroundColor = randomColor;
+}
+
+button.addEventListener("click", colorChange);
+mysteryButton.addEventListener("wheel", colorChange);
