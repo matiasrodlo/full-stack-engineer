9228d6b714b1ee26769e4fcb8889506f2a0024e2
Matías Rodlo <m@hablaqui.com>
2024-01-26 18:25:15 -0300

    homework

---
+++ Full-Stack Engineer/Frontend/github2/homework.js
@@ -0,0 +1,31 @@
+// Mary Rose's JavaScript Homework
+
+// 1. Write an if/else statement
+var APPLE_QUANTITY = 5;
+
+// I'm a comment!
+if (APPLE_QUANTITY < 5) {
+  console.log("You have fewer than 5 apples.");
+} else {
+  console.log("You have 5 or more apples. Hurrah!");
+}
+
+// I'm a comment!
+// 2. Write a "for" loop
+for (i = 0; i < APPLE_QUANTITY; i++) {
+  console.log("WEEEEEEE! I'm looping!");
+}
+
+// I'm a comment!
+// 3. Write a function
+var sayHello = function () {
+  console.log("Hello!");
+};
+
+// I'm a comment!
+// 4. Create an object
+var mary = {
+  name: "Mary Rose",
+  codeNinja: true,
+  country: "UK",
+};
