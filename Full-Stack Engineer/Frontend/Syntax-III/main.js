61bee7a464b9b891443a90ac26f37d57ab6816ed
Matias Rodlo <m@hablaqui.com>
2024-02-09 16:32:51 -0300

    inheritance II

--- Full-Stack Engineer/Frontend/Syntax-III/main.js
+++ Full-Stack Engineer/Frontend/Syntax-III/main.js
@@ -28,3 +28,48 @@ const surgeonJackson = new Surgeon('Ruth Jackson', 'Orthopedics');
 console.log(surgeonRomero.name);
 console.log(surgeonRomero.takeVacationDays(3));
 console.log(surgeonRomero.remainingVacationDays());
+
+////////////////////////////////////////////////////////////////////////
+
+class HospitalEmployee {
+  constructor(name) {
+    this._name = name;
+    this._remainingVacationDays = 20;
+  }
+
+  get name() {
+    return this._name;
+  }
+
+  get remainingVacationDays() {
+    return this._remainingVacationDays;
+  }
+
+ takeVacationDays(days) {
+    if (days <= this._remainingVacationDays) {
+      this._remainingVacationDays -= days;
+      console.log(`${this._name} took ${days} vacation day(s). Remaining vacation days: ${this._remainingVacationDays}`);
+    } else {
+      console.log(`Not enough remaining vacation days for ${this._name}.`);
+    }
+  }
+}
+
+class Doctor extends HospitalEmployee {
+  constructor(name, insurance) {
+    super(name);
+    this._insurance = insurance;
+  }
+}
+
+class Nurse extends HospitalEmployee {
+  constructor(name, certifications) {
+    super(name);
+    this._certifications = certifications;
+  }
+
+  addCertification(certification) {
+    this._certifications.push(certification);
+    console.log(`${this._name} added certification: ${certification}`);
+  }
+}
