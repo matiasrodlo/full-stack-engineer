773878ed85b60a732d40258681994d8a5f6fcc8e
Matias Rodlo <m@hablaqui.com>
2024-02-10 15:49:44 -0300

    intro tdd

---
+++ Full-Stack Engineer/Frontend/testing/tdd-mocha.js
@@ -0,0 +1,10 @@
+const assert = require('assert');
+const Calculate =  require('../index.js')
+
+describe('Calculate', () =>{
+  describe('.sum', () =>{
+     it('returns the sum of an array of numbers', () =>{
+       
+     })
+  }) 
+})
