61e5e18a3b35629f85dea3e24fec2940a3c2b8ce
Matias Rodlo <m@hablaqui.com>
2024-02-10 17:34:02 -0300

    getting into red

--- Full-Stack Engineer/Frontend/testing/expressive-test.js
+++ Full-Stack Engineer/Frontend/testing/expressive-test.js
@@ -51,8 +51,8 @@ const assert = require('assert');
 describe('-', () => {
   it('returns the difference of two values', () => {
     const bigNum = 100;
-		const smallNum = 4;
-		const expected = 96;
+	const smallNum = 4;
+	const expected = 96;
     
     const result =  bigNum - smallNum;
 
--- Full-Stack Engineer/Frontend/testing/tdd-mocha.js
+++ Full-Stack Engineer/Frontend/testing/tdd-mocha.js
@@ -8,3 +8,17 @@ describe('Calculate', () =>{
      })
   }) 
 })
+
+// Getting Into The Red I
+
+const assert = require("assert");
+const Calculate = require("../index.js");
+
+describe("Calculate", () => {
+  describe(".sum", () => {
+    it("returns the sum of an array of numbers", () => {
+assert.strictEqual(Calculate.sum([1,2,3]), 6);
+    });
+  });
+});
+
