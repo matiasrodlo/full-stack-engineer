365da295d4da2fa74764e8d312db4545c4be9bf6
Matias Rodlo <m@hablaqui.com>
2024-02-10 14:06:23 -0300

    deepequal

---
+++ Full-Stack Engineer/Frontend/testing/expressive-test.js
@@ -0,0 +1,80 @@
+const assert = require('assert');
+
+describe('+', () => {
+  it('returns the sum of its arguments', () => {
+
+    assert.ok(3 + 4 === 7);
+    
+  });
+});
+
+
+// assert.ok
+const assert = require('assert');
+
+describe('-', () => {
+  it('returns the difference of two values', () => {
+    const bigNum = 100;
+		const smallNum = 4;
+		const expected = 96;
+    
+    const result =  bigNum - smallNum;
+
+    // Write assertion here
+    assert.ok(result === expected);
+
+  });
+});
+
+// assert.equal
+
+const assert = require('assert');
+
+describe('-', () => {
+  it('returns the difference of two values', () => {
+    const bigNum = 100;
+		const smallNum = 4;
+		const expected = 96;
+    
+    const result =  bigNum - smallNum;
+
+    // Write assertion here
+    assert.equal(result, expected);
+  });
+});
+
+
+// assert.strictEqual
+
+const assert = require('assert');
+
+describe('-', () => {
+  it('returns the difference of two values', () => {
+    const bigNum = 100;
+		const smallNum = 4;
+		const expected = 96;
+    
+    const result =  bigNum - smallNum;
+
+    // Write assertion here
+    assert.strictEqual(result, expected);
+  });
+});
+
+// assert.deepEqual
+
+const assert = require('assert');
+
+describe('+', () => {
+  it('returns the sum of two values', () => {
+    // Setup
+		let expected = {a: 3, b: 4, result: 7};
+		let sum = {a: 3, b: 4};
+
+    // Exercise
+		sum.result = sum.a + sum.b;
+
+    // Verify
+    assert.deepEqual(sum, expected);
+  });
+});
