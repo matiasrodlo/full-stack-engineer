088d0aa37e9b227f3ea77252cf68bbc5c2d6a855
Matias Rodlo <m@hablaqui.com>
2024-02-10 12:26:17 -0300

    test in 3 steps

--- Full-Stack Engineer/Frontend/testing/mocha.js
+++ Full-Stack Engineer/Frontend/testing/mocha.js
@@ -55,3 +55,41 @@ describe('Math', () => {
       });
     });
   });
+
+
+  // import assert here
+const assert = require('assert');
+
+describe('+', () => {
+  it('returns the sum of its arguments', () => {
+assert.ok(3 + 4 === 7);
+
+  });
+});
+
+// Setup, Exercise, and Verify
+
+
+const assert = require('assert');
+
+// Naive approach
+describe('.pop', () => {
+  it('returns the last element in the array [naive]', () => {
+    assert.ok(['padawan', 'knight'].pop() === 'knight'); 
+  });
+});
+
+// 3 phase approach
+describe('.pop', () => {
+  it('returns the last element in the array [3phase]', () => {
+    // Setup
+    const knightString = 'knight';
+    const jediPath = ['padawan', knightString];
+
+    // Exercise
+    const popped = jediPath.pop();
+
+    // Verify
+    assert.ok(popped === knightString);
+  });
+});
