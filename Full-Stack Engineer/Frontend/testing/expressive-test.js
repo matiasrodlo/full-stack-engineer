87a40255b10f862dc1ca7d35b02309d83370eb2f
Matias Rodlo <m@hablaqui.com>
2024-02-10 14:09:03 -0300

    deepequal 2

--- Full-Stack Engineer/Frontend/testing/expressive-test.js
+++ Full-Stack Engineer/Frontend/testing/expressive-test.js
@@ -78,3 +78,21 @@ describe('+', () => {
     assert.deepEqual(sum, expected);
   });
 });
+
+// assert.deepEqual II
+
+const assert = require('assert');
+
+describe('+', () => {
+  it('returns the sum of two values', () => {
+    // Setup
+		let expected = [3, 4, 7];
+		let sum = [3, 4];
+
+    // Exercise
+		sum.push(3 + 4);
+
+    // Verify
+    assert.deepEqual(sum, expected);
+  });
+});
