8aa717ca7513fd53f6d1862896f5b60a24094e5b
Matias Rodlo <m@hablaqui.com>
2024-02-14 18:25:27 -0300

    settimeout

--- Full-Stack Engineer/frontend/async-http/app.js
+++ Full-Stack Engineer/frontend/async-http/app.js
@@ -20,3 +20,19 @@ const inventory = {
   const orderPromise = orderSunglasses();
   
   console.log(orderPromise);                      
+
+// The Node setTimeout() Function
+
+  console.log("This is the first line of code in app.js.");
+// Keep the line above as the first line of code
+// Write your code here:
+
+
+function usingSTO() {
+  console.log('This is the async code in app.js.')
+}
+
+setTimeout(usingSTO, 3000)
+
+// Keep the line below as the last line of code:
+console.log("This is the last line of code in app.js.");
