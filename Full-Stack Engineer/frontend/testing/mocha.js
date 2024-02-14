d6dfb652452bb3d88a0d09ab8f9e00b4cd621e06
Matias Rodlo <m@hablaqui.com>
2024-02-14 12:26:02 -0300

    refactor I

--- Full-Stack Engineer/frontend/testing/mocha.js
+++ Full-Stack Engineer/frontend/testing/mocha.js
@@ -192,3 +192,21 @@ const Calculate = {
   }
 }
 module.exports = Calculate;
+
+// Refactor I
+
+const assert = require('assert');
+const Calculate =  require('../index.js')
+
+describe('Calculate', () => {
+  describe('.sum',() => {
+    it('returns the sum of an array of numbers', () => {
+      
+      const expectedResult = 6;
+      const inputArray = [1,2,3]
+      const result = Calculate.sum(inputArray)
+      
+      assert.strictEqual(result, expectedResult);
+    });
+  });
+});
