1a0b46a8a213f59e20d6d73fc2000cebc1783699
Matias Rodlo <m@hablaqui.com>
2024-02-14 13:09:12 -0300

    edge case

--- Full-Stack Engineer/frontend/testing/mocha.js
+++ Full-Stack Engineer/frontend/testing/mocha.js
@@ -263,3 +263,21 @@ const Calculate = {
 };
 
 module.exports = Calculate;
+
+// Edge Case
+
+const Calculate = {
+  sum(inputArray) {
+
+if (inputArray.length === 0) {
+  return 0;
+}
+
+    return inputArray.reduce((sum, value) => {
+      return sum + value;
+    })
+  }
+}
+
+module.exports = Calculate;
+
