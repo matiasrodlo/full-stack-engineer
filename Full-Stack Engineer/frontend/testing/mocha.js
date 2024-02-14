907126633ea7ae5f5dd0ac9220ea5fdc6841738a
Matias Rodlo <m@hablaqui.com>
2024-02-14 13:15:43 -0300

    result

--- Full-Stack Engineer/frontend/testing/mocha.js
+++ Full-Stack Engineer/frontend/testing/mocha.js
@@ -281,3 +281,54 @@ if (inputArray.length === 0) {
 
 module.exports = Calculate;
 
+// index.js (functionality) //
+
+const Calculate = {
+  sum(inputArray) {
+    if(inputArray.length === 0){
+      return 0
+    }
+    
+    return inputArray.reduce((sum, value) => {
+      return sum + value;
+    })
+  }
+}
+
+module.exports = Calculate;
+
+
+// index.js (functionality) //
+ 
+const assert = require('assert');
+const Calculate =  require('../index.js')
+
+describe('Calculate', () => {
+  describe('.sum',() => {
+    it('returns the sum of an array of numbers', () => {
+      const expectedResult = 6;
+      const inputArray = [1,2,3]
+      
+      const result = Calculate.sum(inputArray)
+      
+      assert.strictEqual(result, expectedResult);
+    });
+    
+    it('returns the sum of a four item list', ()=>{
+      const expectedResult = 22;
+      const inputArray = [4,6,5,7];
+      
+      const result = Calculate.sum(inputArray);
+      
+      assert.strictEqual(result, expectedResult)
+      
+    });
+    
+    it('returns zero when the array is empty', ()=>{
+       const expectedResult = 0;
+       const inputArray = [];
+       const result = Calculate.sum(inputArray);
+       assert.strictEqual(result, expectedResult)
+    });
+  });
+});
