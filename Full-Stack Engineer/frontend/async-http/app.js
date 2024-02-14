4c31035987c794d611a4660c2b8430eefdbfae84
Matias Rodlo <m@hablaqui.com>
2024-02-14 19:04:38 -0300

    catch with promise

--- Full-Stack Engineer/frontend/async-http/app.js
+++ Full-Stack Engineer/frontend/async-http/app.js
@@ -77,3 +77,22 @@ const inventory = {
   };
   
   module.exports = { checkInventory };
+
+  // Using catch() with Promises
+
+  const {checkInventory} = require('./library.js');
+
+const order = [['sunglasses', 1], ['bags', 2]];
+
+const handleSuccess = (resolvedValue) => {
+  console.log(resolvedValue);
+};
+
+const handleFailure = (rejectReason) => {
+  console.log(rejectReason);
+};
+
+// Write your code below:
+checkInventory(order)
+	.then(handleSuccess)
+	.catch(handleFailure);
