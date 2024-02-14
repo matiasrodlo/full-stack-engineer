39d304b03041e80e7f13c90f41f8169bba633fc4
Matias Rodlo <m@hablaqui.com>
2024-02-14 19:30:04 -0300

    commom mistakes

--- Full-Stack Engineer/frontend/async-http/app.js
+++ Full-Stack Engineer/frontend/async-http/app.js
@@ -206,3 +206,25 @@ const store = {
   }
   
   module.exports = {checkInventory, processPayment, shipOrder};
+ 
+// common mistakes
+
+const {checkInventory, processPayment, shipOrder} = require('./library.js');
+
+const order = {
+  items: [['sunglasses', 1], ['bags', 2]],
+  giftcardBalance: 79.82
+};
+
+// Refactor the code below:
+
+checkInventory(order)
+.then((resolvedValueArray) => {
+  return processPayment(resolvedValueArray);
+})
+.then((resolvedValueArray) => {
+  return shipOrder(resolvedValueArray);
+})
+.then((successMessage) => {
+  console.log(successMessage);
+});
