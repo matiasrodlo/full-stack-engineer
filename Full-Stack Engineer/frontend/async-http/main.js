27dba8d06c053073195e578a903a3d1c5c0d63e9
Matias Rodlo <m@hablaqui.com>
2024-02-20 13:13:13 -0300

    standard export

--- Full-Stack Engineer/frontend/async-http/main.js
+++ Full-Stack Engineer/frontend/async-http/main.js
@@ -13,3 +13,24 @@ setInterval(()=> {
   // call changeToFunkyColor() here
 changeToFunkyColor(header)
 }, 200);
+
+////////////////////////////////////
+
+function changeText(domElement, newText) {
+    domElement.innerHTML = newText;
+  }
+  
+  function changeToFunkyColor(domElement) {
+    const r = Math.random() * 255;
+    const g = Math.random() * 255;
+    const b = Math.random() * 255;
+  
+    domElement.style.color = `rgb(${r}, ${g}, ${b})`;
+  }
+  
+  // use default export syntax below here
+  
+  export default { 
+    changeText: changeText,       
+    changeToFunkyColor: changeToFunkyColor 
+  }
