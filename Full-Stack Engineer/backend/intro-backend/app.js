1d6647122927e5e6cfb2c4806d94cd747dfbac8d
Matias Rodlo <m@hablaqui.com>
2024-02-21 20:55:18 -0300

    intro to node

---
+++ Full-Stack Engineer/backend/intro-backend/app.js
@@ -0,0 +1,8 @@
+let noun1 = 'silly';
+let adjective = 'silly';
+let noun2 = 'silly';
+let verb = 'silly';
+let noun3 = 'silly';
+
+
+console.log(`The world's first ${noun1} was a very ${adjective} ${noun2} who loved to ${verb} while eating ${noun3} for every meal.`);
