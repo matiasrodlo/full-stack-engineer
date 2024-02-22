d50c66348af7acfc3f515ddb564293d6facf28a8
Matias Rodlo <m@hablaqui.com>
2024-02-21 21:03:23 -0300

    node console

--- Full-Stack Engineer/backend/intro-backend/app.js
+++ Full-Stack Engineer/backend/intro-backend/app.js
@@ -6,3 +6,12 @@ let noun3 = 'silly';
 
 
 console.log(`The world's first ${noun1} was a very ${adjective} ${noun2} who loved to ${verb} while eating ${noun3} for every meal.`);
+
+//The Console Module
+
+const petsArray = ['dog', 'cat', 'bird', 'monkey'];
+
+// Add console methods below!
+console.log(petsArray)
+console.table(petsArray)
+console.assert(petsArray.length > 5)
