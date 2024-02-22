df780cc843a683208713c138f9073665debd9f05
Matias Rodlo <m@hablaqui.com>
2024-02-21 21:16:44 -0300

    process module

--- Full-Stack Engineer/backend/intro-backend/app.js
+++ Full-Stack Engineer/backend/intro-backend/app.js
@@ -7,7 +7,9 @@ let noun3 = 'silly';
 
 console.log(`The world's first ${noun1} was a very ${adjective} ${noun2} who loved to ${verb} while eating ${noun3} for every meal.`);
 
-//The Console Module
+///////////////////////////////////////////////////////
+//The Console Module///////////////////////////////////
+///////////////////////////////////////////////////////
 
 const petsArray = ['dog', 'cat', 'bird', 'monkey'];
 
@@ -15,3 +17,23 @@ const petsArray = ['dog', 'cat', 'bird', 'monkey'];
 console.log(petsArray)
 console.table(petsArray)
 console.assert(petsArray.length > 5)
+
+///////////////////////////////////////////////////////
+//The Console Module///////////////////////////////////
+///////////////////////////////////////////////////////
+
+let initialMemory = process.memoryUsage().heapUsed;
+let word = process.argv[2];
+
+console.log(`Your word is ${word}`)
+
+// Create a new array
+let wordArray = [];
+
+// Loop 1000 times, pushing into the array each time 
+for (let i = 0; i < 1000; i++){
+  wordArray.push(`${word} count: ${i}`)
+}
+
+console.log(`Starting memory usage: ${initialMemory}. \nCurrent memory usage: ${process.memoryUsage().heapUsed}. \nAfter using the loop to add elements to the array, the process is using ${process.memoryUsage().heapUsed - initialMemory} more bytes of memory.`)
+
