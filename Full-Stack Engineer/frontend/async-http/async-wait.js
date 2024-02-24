22149a6bb9ccf9b689dcf289f84514f159c0ef89
Matías Rodlo <m@hablaqui.com>
2024-02-24 16:11:03 -0300

    handlingerrors

--- Full-Stack Engineer/frontend/async-http/async-wait.js
+++ Full-Stack Engineer/frontend/async-http/async-wait.js
@@ -124,3 +124,23 @@ async function makeBeans() {
 }
 
 makeBeans();
+
+///////////////////////////////////////////////
+//Handling Errors//////////////////////////////
+///////////////////////////////////////////////
+
+const cookBeanSouffle = require('./library.js');
+
+// Write your code below:
+
+async function hostDinnerParty() {
+    try {
+     let dinner = await cookBeanSouffle();
+     console.log(`${dinner} is served!`);  }
+  catch (error) {
+console.log(error);
+console.log('Ordering a pizza!');
+  }
+}
+
+hostDinnerParty()
