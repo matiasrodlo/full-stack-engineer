6668b64c6c276f0e5707aee9071a16dd092be71f
Matías Rodlo <m@hablaqui.com>
2024-02-24 14:37:36 -0300

    async await

--- Full-Stack Engineer/frontend/async-http/async-wait.js
+++ Full-Stack Engineer/frontend/async-http/async-wait.js
@@ -36,3 +36,38 @@ async function readFiles() {
 }
 
 readFiles();
+
+///////////////////////////////////////////////
+//The async Keyword///////////////////////////
+///////////////////////////////////////////////
+
+function withConstructor(num) {
+  return new Promise((resolve, reject) => {
+    if (num === 0) {
+      resolve("zero");
+    } else {
+      resolve("not zero");
+    }
+  });
+}
+
+withConstructor(0).then((resolveValue) => {
+  console.log(
+    ` withConstructor(0) returned a promise which resolved to: ${resolveValue}.`
+  );
+});
+
+// Write your code below:
+async function withAsync(num) {
+  if (num === 0) {
+    return "zero";
+  } else {
+    return "not zero";
+  }
+}
+
+withAsync(100).then((resolveValue) => {
+  console.log(
+    ` withAsync(100) returned a promise which resolved to: ${resolveValue}.`
+  );
+});
