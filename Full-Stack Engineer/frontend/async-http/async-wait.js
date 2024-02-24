d48102ae0828421855349f7620968fbe5cd57f1b
Matías Rodlo <m@hablaqui.com>
2024-02-24 16:17:14 -0300

    handling independent promises

--- Full-Stack Engineer/frontend/async-http/async-wait.js
+++ Full-Stack Engineer/frontend/async-http/async-wait.js
@@ -144,3 +144,27 @@ console.log('Ordering a pizza!');
 }
 
 hostDinnerParty()
+
+///////////////////////////////////////////////
+//Handling Independent Promises////////////////
+///////////////////////////////////////////////
+
+let {
+  cookBeans,
+  steamBroccoli,
+  cookRice,
+  bakeChicken,
+} = require("./library.js");
+
+// Write your code below:
+
+async function serveDinner() {
+  const vegetablePromise = steamBroccoli();
+  const starchPromise = cookRice();
+  const proteinPromise = bakeChicken();
+  const sidePromise = cookBeans();
+  console.log(
+    `Dinner is served. We're having ${await vegetablePromise}, ${await starchPromise}, ${await proteinPromise}, and ${await sidePromise}.`
+  );
+}
+serveDinner();
