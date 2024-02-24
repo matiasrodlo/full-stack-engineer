80dd5ad3332e82f2a2f02a97505bf616e9a680cd
Matías Rodlo <m@hablaqui.com>
2024-02-24 15:33:08 -0300

    await operator

--- Full-Stack Engineer/frontend/async-http/async-wait.js
+++ Full-Stack Engineer/frontend/async-http/async-wait.js
@@ -71,3 +71,26 @@ withAsync(100).then((resolveValue) => {
     ` withAsync(100) returned a promise which resolved to: ${resolveValue}.`
   );
 });
+
+
+///////////////////////////////////////////////
+//The await Operator///////////////////////////
+///////////////////////////////////////////////
+
+const brainstormDinner = require("./library.js");
+
+// Native promise version:
+function nativePromiseDinner() {
+  brainstormDinner().then((meal) => {
+    console.log(`I'm going to make ${meal} for dinner.`);
+  });
+}
+
+// async/await version:
+async function announceDinner() {
+  // Write your code below:
+  let meal = await brainstormDinner();
+  console.log(`I'm going to make ${meal} for dinner.`);
+}
+
+announceDinner();
