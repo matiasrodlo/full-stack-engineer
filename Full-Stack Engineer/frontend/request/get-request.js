d3e2e039833912a3f1470bf8286c6f165c4ba2eb
Matías Rodlo <m@hablaqui.com>
2024-02-24 20:07:19 -0300

    async post  request

--- Full-Stack Engineer/frontend/request/get-request.js
+++ Full-Stack Engineer/frontend/request/get-request.js
@@ -206,3 +206,50 @@ const displaySuggestions = (event) => {
 }
 
 submit.addEventListener('click', displaySuggestions);
+
+////////////////////////////////////////////////////
+//async POST Request///////////////////////////////
+///////////////////////////////////////////////////
+
+// information to reach API
+const apiKey = "<Your API Key>";
+const url = "https://api.rebrandly.com/v1/links";
+
+// Some page elements
+const inputField = document.querySelector("#input");
+const shortenButton = document.querySelector("#shorten");
+const responseField = document.querySelector("#responseField");
+
+// Asynchronous functions
+// Code goes here
+const shortenUrl = async () => {
+  const urlToShorten = inputField.value;
+  const data = JSON.stringify({ destination: urlToShorten });
+  try {
+    const response = await fetch(url, {
+      method: "POST",
+      body: data,
+      headers: {
+        "Content-type": "application/json",
+        apikey: apiKey,
+      },
+    });
+    if (response.ok) {
+      const jsonResponse = await response.json();
+      renderResponse(jsonResponse);
+    }
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+// Clear page and call Asynchronous functions
+const displayShortUrl = (event) => {
+  event.preventDefault();
+  while (responseField.firstChild) {
+    responseField.removeChild(responseField.firstChild);
+  }
+  shortenUrl();
+};
+
+shortenButton.addEventListener("click", displayShortUrl);
