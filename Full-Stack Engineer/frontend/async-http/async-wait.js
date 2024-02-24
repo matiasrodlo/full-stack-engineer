b6dc5c9e4ab13b54b2f2bebd92a79d67a9067cde
Matías Rodlo <m@hablaqui.com>
2024-02-24 15:46:03 -0300

    writing async func

--- Full-Stack Engineer/frontend/async-http/async-wait.js
+++ Full-Stack Engineer/frontend/async-http/async-wait.js
@@ -94,3 +94,18 @@ async function announceDinner() {
 }
 
 announceDinner();
+
+///////////////////////////////////////////////
+//Writing async Functions//////////////////////
+///////////////////////////////////////////////
+
+const shopForBeans = require('./library.js');
+
+async function getBeans() {
+  console.log(`1. Heading to the store to buy beans...`);
+  let value = await shopForBeans();
+  console.log(`3. Great! I'm making ${value} beans for dinner tonight!`);
+}
+
+getBeans();
+
