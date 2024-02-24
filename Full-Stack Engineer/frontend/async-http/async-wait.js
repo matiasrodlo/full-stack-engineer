7c3c252aca8f2261ec28c2f5f0e2b66d295c7429
Matías Rodlo <m@hablaqui.com>
2024-02-24 16:02:39 -0300

    handling promises

--- Full-Stack Engineer/frontend/async-http/async-wait.js
+++ Full-Stack Engineer/frontend/async-http/async-wait.js
@@ -109,3 +109,18 @@ async function getBeans() {
 
 getBeans();
 
+///////////////////////////////////////////////
+//Handling Dependent Promises//////////////////
+///////////////////////////////////////////////
+
+const {shopForBeans, soakTheBeans, cookTheBeans} = require('./library.js');
+
+// Write your code below:
+async function makeBeans() {
+  let type = await shopForBeans();
+  let isSoft = await soakTheBeans(type);
+  let dinner = await cookTheBeans(isSoft);
+  console.log(dinner);
+}
+
+makeBeans();
