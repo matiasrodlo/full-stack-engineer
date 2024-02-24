d96bbfae1dc58edd1a86ecca390fb1708d3740c6
Matías Rodlo <m@hablaqui.com>
2024-02-24 19:08:33 -0300

    making post request

--- Full-Stack Engineer/frontend/request/get-request.js
+++ Full-Stack Engineer/frontend/request/get-request.js
@@ -79,3 +79,42 @@ const displaySuggestions = (event) => {
 };
 
 submit.addEventListener('click', displaySuggestions);
+
+////////////////////////////////////////////////////
+//Making a POST Request////////////////////////////
+///////////////////////////////////////////////////
+
+// Information to reach API
+const apiKey = 'b189e41509fe4ebb94b8bd79b90078b3';
+const url = 'https://api.rebrandly.com/v1/links';
+
+// Some page elements
+const inputField = document.querySelector('#input');
+const shortenButton = document.querySelector('#shorten');
+const responseField = document.querySelector('#responseField');
+
+// Asynchronous functions
+const shortenUrl = () => {
+  const urlToShorten = inputField.value;
+  const data = JSON.stringify({destination: urlToShorten});
+  
+	fetch(url, {
+    method: 'POST',
+    headers: {
+      'Content-type': 'application/json',
+      'apikey': apiKey
+    },
+    body: data
+  })
+}
+
+// Clear page and call AJAX functions
+const displayShortUrl = (event) => {
+  event.preventDefault();
+  while(responseField.firstChild){
+    responseField.removeChild(responseField.firstChild);
+  }
+  shortenUrl();
+}
+
+shortenButton.addEventListener('click', displayShortUrl);
