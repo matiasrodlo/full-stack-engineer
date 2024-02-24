bf5cfa1d86a42dc79ebfe8c601983374a7582032
Matías Rodlo <m@hablaqui.com>
2024-02-24 18:50:31 -0300

    handling get request

--- Full-Stack Engineer/frontend/request/get-request.js
+++ Full-Stack Engineer/frontend/request/get-request.js
@@ -1,3 +1,7 @@
+////////////////////////////////////////////////////
+//GET Request//////////////////////////////////////
+///////////////////////////////////////////////////
+
 // Information to reach API
 
 const url = "https://api.datamuse.com/words?sl=";
@@ -34,3 +38,44 @@ const displaySuggestions = (event) => {
 };
 
 submit.addEventListener("click", displaySuggestions);
+
+////////////////////////////////////////////////////
+//Handling a GET Request////////////////////////////
+///////////////////////////////////////////////////
+
+// Information to reach API
+const url = 'https://api.datamuse.com/words?sl=';
+
+// Selects page elements
+const inputField = document.querySelector('#input');
+const submit = document.querySelector('#submit');
+const responseField = document.querySelector('#responseField');
+
+// Asynchronous function
+const getSuggestions = () => {
+  const wordQuery = inputField.value;
+  const endpoint = `${url}${wordQuery}`;
+  
+  fetch(endpoint, {cache: 'no-cache'}).then(response => {
+    if (response.ok) {
+      return response.json();
+    }
+    throw new Error('Request failed!');
+  }, networkError => {
+    console.log(networkError.message)
+  }).then(jsonResponse => {
+    // renderRawResponse(jsonResponse);
+    renderResponse(jsonResponse);
+  })
+}
+
+// Clears previous results and display results to webpage
+const displaySuggestions = (event) => {
+  event.preventDefault();
+  while(responseField.firstChild){
+    responseField.removeChild(responseField.firstChild);
+  }
+  getSuggestions();
+};
+
+submit.addEventListener('click', displaySuggestions);
