f0b8cc27c5b772448259485dc34bdbd12a090e35
Matías Rodlo <m@hablaqui.com>
2024-02-25 13:02:44 -0300

    primary key

--- Full-Stack Engineer/backend/designing-relational-bd/script.sql
+++ Full-Stack Engineer/backend/designing-relational-bd/script.sql
@@ -2,7 +2,9 @@
 
 SELECT * from profile;
 
+///////////////////////
 // Creating Your Tables
+///////////////////////
 
 CREATE TABLE book (
     title varchar(100),
@@ -26,9 +28,9 @@ CREATE TABLE book (
     email varchar(20)
   );
 
-////////////////////
-Querying Your Tables
-////////////////////
+////////////////////////
+// Querying Your Tables
+////////////////////////
 
   INSERT INTO book VALUES (
   'Postgres for Beginners',
@@ -42,3 +44,29 @@ Querying Your Tables
 SELECT * FROM book WHERE title = 'Postgres for Beginners';
 
 SELECT * FROM book WHERE isbn = '0-5980-6249-1';
+
+////////////////////////
+// Primary Key /////////
+////////////////////////
+
+CREATE TABLE book (
+  title varchar(100),
+  isbn varchar(50) PRIMARY KEY,
+  pages integer,
+  price money,
+  description varchar(256),
+  publisher varchar(100)
+);
+
+CREATE TABLE chapter (
+  id integer PRIMARY KEY,
+  number integer,
+  title varchar(50),
+  content varchar(1024)
+);
+
+CREATE TABLE author (
+  name varchar(50),
+  bio varchar(100),
+  email varchar(20) PRIMARY KEY
+);
