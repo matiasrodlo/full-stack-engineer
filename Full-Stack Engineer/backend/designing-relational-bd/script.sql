499c6e464be8c1d908bff3e40aac4eb45212d52d
Matías Rodlo <m@hablaqui.com>
2024-02-25 13:34:38 -0300

    composite primary key

--- Full-Stack Engineer/backend/designing-relational-bd/script.sql
+++ Full-Stack Engineer/backend/designing-relational-bd/script.sql
@@ -82,16 +82,35 @@ FROM
 WHERE
   table_name = 'book';
 
-  SELECT
+SELECT
   constraint_name, table_name, column_name
 FROM
   information_schema.key_column_usage
 WHERE
   table_name = 'chapter';
 
-    SELECT
+SELECT
   constraint_name, table_name, column_name
 FROM
   information_schema.key_column_usage
 WHERE
   table_name = 'author';
+
+////////////////////////
+//Composite Primary Key/
+////////////////////////
+
+  CREATE TABLE popular_books (
+  book_title varchar(100),
+  author_name varchar(50),
+  number_sold integer,
+  number_previewed integer,
+  PRIMARY KEY (book_title, author_name)
+);
+
+SELECT
+  constraint_name, table_name, column_name
+FROM
+  information_schema.key_column_usage
+WHERE
+  table_name = 'popular_books';
