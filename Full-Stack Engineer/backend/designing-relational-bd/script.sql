25a6ac4ddb5a6ab81dce8a0a0764756f8748c773
Matías Rodlo <m@hablaqui.com>
2024-02-25 14:18:23 -0300

    one to one relatioship

--- Full-Stack Engineer/backend/designing-relational-bd/script.sql
+++ Full-Stack Engineer/backend/designing-relational-bd/script.sql
@@ -153,3 +153,59 @@ SELECT book.title as book, chapter.title as chapters
 FROM book
 JOIN chapter
 ON book.isbn = chapter.book_isbn;
+
+
+/////////////////////////////
+//One-to-One Relationship////
+////////////////////////////
+
+CREATE TABLE book_details (
+  id integer PRIMARY KEY,
+  book_isbn varchar(50) REFERENCES book(isbn) UNIQUE,
+  rating decimal,
+  language varchar(10),
+  keywords text[],
+  date_published date
+);
+
+SELECT
+  constraint_name, table_name, column_name
+FROM
+  information_schema.key_column_usage
+WHERE
+  table_name = 'book_details';
+
+INSERT INTO book VALUES (
+  'Learn PostgreSQL',
+  '123457890',
+  100,
+  2.99,
+  'Great course',
+  'Codecademy'
+);
+
+INSERT INTO book_details VALUES (
+  1,
+  '123457890',
+  3.95,
+  'English',
+  '{sql, postgresql, database}',
+  '2020-05-20'
+);
+
+INSERT INTO book_details VALUES (
+  2,
+  '123457890',
+  3.95,
+  'French',
+  '{sql, postgresql, database}',
+  '2020-05-20'
+);
+
+SELECT
+    book.title, book.price, book_details.language, book_details.rating
+FROM book
+JOIN book_details
+ON book.isbn = book_details.book_isbn;
+
+
