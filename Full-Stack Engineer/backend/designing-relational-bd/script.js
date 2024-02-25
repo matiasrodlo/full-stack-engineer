c9f0d4de7a3aa4106a7e4c69138f553bf9a26abf
Matías Rodlo <m@hablaqui.com>
2024-02-24 21:59:06 -0300

    create your table

--- Full-Stack Engineer/backend/designing-relational-bd/script.js
+++ Full-Stack Engineer/backend/designing-relational-bd/script.js
@@ -1,1 +1,27 @@
+//Select a Table
+
 SELECT * from profile;
+
+// Creating Your Tables
+
+CREATE TABLE book (
+    title varchar(100),
+    isbn varchar(50),
+    pages integer,
+    price money, 
+    description varchar(256),
+    publisher varchar(100)
+  );
+  
+  CREATE TABLE chapter (
+    id integer,
+    number integer,
+    title varchar(50),
+    content varchar(1024)
+  );
+  
+  CREATE TABLE author (
+    name varchar(50),
+    bio varchar(100),
+    email varchar(20)
+  );
