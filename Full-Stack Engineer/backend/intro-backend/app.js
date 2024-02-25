6bd80274135991578023f2727d6f7cbe9a6ea11b
Matías Rodlo <m@hablaqui.com>
2024-02-24 21:30:29 -0300

    the event module

--- Full-Stack Engineer/backend/intro-backend/app.js
+++ Full-Stack Engineer/backend/intro-backend/app.js
@@ -136,3 +136,23 @@ fs.readFile('./file.txt', 'utf-8', readDataCallback);
 setImmediate(() => {
   console.log('Welcome to Node.js');
 });
+
+///////////////////////////////////////////////////////
+//The Events Module///////////////////////////////////
+//////////////////////////////////////////////////////
+
+// Here we require in the 'events' module and save a reference to it in an events variable
+let events = require('events');
+
+let listenerCallback = (data) => {
+    console.log(`Celebrate ${data}`);
+}
+
+// Here we create an instance of the EventEmitter class
+let myEmitter = new events.EventEmitter();
+
+// Here we subscribe to 'celebration' events and provide a callback function which will be passed the event's data
+myEmitter.on('celebration', listenerCallback);
+
+// Here we emit an event, we pass the event type, 'celebration', as the first argument, and the event data as the second
+myEmitter.emit('celebration', 'good times, come on!');
