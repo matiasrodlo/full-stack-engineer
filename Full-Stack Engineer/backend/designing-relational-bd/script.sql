331743218c7bbf2dece2d3b43f49cb05d9656b5d
Matías Rodlo <m@hablaqui.com>
2024-02-25 12:41:44 -0300

    script sql

--- Full-Stack Engineer/backend/designing-relational-bd/script.sql
+++ Full-Stack Engineer/backend/designing-relational-bd/script.sql
@@ -25,3 +25,20 @@ CREATE TABLE book (
     bio varchar(100),
     email varchar(20)
   );
+
+////////////////////
+Querying Your Tables
+////////////////////
+
+  INSERT INTO book VALUES (
+  'Postgres for Beginners',
+  '0-5980-6249-1',
+  25,
+  4.99,
+  'Learn Postgres the Easy Way',
+  'Codecademy Publishing'
+);
+
+SELECT * FROM book WHERE title = 'Postgres for Beginners';
+
+SELECT * FROM book WHERE isbn = '0-5980-6249-1';
