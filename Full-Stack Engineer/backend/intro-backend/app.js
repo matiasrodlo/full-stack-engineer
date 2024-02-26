19e60a581f18f22ea44562fe4c2c48aa805eb92f
Matías Rodlo <m@hablaqui.com>
2024-02-26 12:55:27 -0300

    the error module

--- Full-Stack Engineer/backend/intro-backend/app.js
+++ Full-Stack Engineer/backend/intro-backend/app.js
@@ -171,3 +171,20 @@ let playGame = (userInput) => {
 };
 
 process.stdin.on('data', playGame);
+
+///////////////////////////////////////////////////////
+//The Error Module////////////////////////////////////
+//////////////////////////////////////////////////////
+
+const api = require('./api.js');
+
+// Not an error-first callback
+let callbackFunc = (data) => {
+   console.log(`Something went right. Data: ${data}\n`);
+};
+  
+try {
+  api.naiveErrorProneAsyncFunction('problematic input', callbackFunc);
+} catch(err) {
+  console.log(`Something went wrong. ${err}\n`);
+}
