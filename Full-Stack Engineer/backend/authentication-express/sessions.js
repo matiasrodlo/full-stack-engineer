e8dfb28b9757e15fa39d3c1f27ff176bbef1ef46
Matías Rodlo <m@hablaqui.com>
2024-02-29 19:45:23 -0300

    cfg express-sessions

--- Full-Stack Engineer/backend/authentication-express/sessions.js
+++ Full-Stack Engineer/backend/authentication-express/sessions.js
@@ -13,3 +13,51 @@ const PORT = process.env.PORT || 4001;
 app.listen(PORT, () => {
   console.log(`Server is listening on port ${PORT}`);
 });
+
+////////////////////////////////////////////////////
+//express-session Configuration/////////////////////
+//////////////////////////////////////////////////
+
+const express = require("express");
+const app = express();
+const session = require("express-session");
+
+const PORT = process.env.PORT || 4001;
+
+// Create your session middleware below:
+app.use(
+  session({
+    secret: "D53gxl41G",
+    resave: false,
+    saveUninitialized: false,
+  })
+);
+
+app.listen(PORT, () => {
+  console.log(`Server is listening on port ${PORT}`);
+});
+
+////////////////////////////////////////////////////
+//Storing Session Data/////////////////////////////
+//////////////////////////////////////////////////
+
+const express = require("express");
+const app = express();
+const session = require("express-session");
+app.set('trust proxy', 1)
+const PORT = process.env.PORT || 4001;
+// Create your store below:
+const store = new session.MemoryStore();
+
+app.use(
+  session({
+    secret: "D53gxl41G",
+    resave: false,
+    saveUninitialized: false,
+    store
+  })
+);
+
+app.listen(PORT, () => {
+  console.log(`Server is listening on port ${PORT}`);
+});
