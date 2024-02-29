ea4809f0047e92d6ba6432694ac43c48de20ac9c
Matías Rodlo <m@hablaqui.com>
2024-02-29 19:59:38 -0300

    cookies

--- Full-Stack Engineer/backend/authentication-express/sessions.js
+++ Full-Stack Engineer/backend/authentication-express/sessions.js
@@ -61,3 +61,29 @@ app.use(
 app.listen(PORT, () => {
   console.log(`Server is listening on port ${PORT}`);
 });
+
+////////////////////////////////////////////////////
+//Cookies//////////////////////////////////////////
+//////////////////////////////////////////////////
+
+const express = require("express");
+const app = express();
+const session = require("express-session");
+app.set("trust proxy", 1);
+const PORT = process.env.PORT || 4001;
+const store = new session.MemoryStore();
+
+app.use(
+  session({
+    secret: "D53gxl41G",
+    // Add the cookie property below:
+    cookie: { maxAge: 172800000, secure: true, sameSite: "none" },
+    resave: false,
+    saveUninitialized: false,
+    store
+  })
+);
+
+app.listen(PORT, () => {
+  console.log(`Server is listening on port ${PORT}`);
+});
