ae4a754bbdf9bbf9a306feca5669d28ddb12b9cb
Matías Rodlo <m@hablaqui.com>
2024-02-29 15:20:09 -0300

    first route

--- Full-Stack Engineer/backend/intro-backend/express-routes.js
+++ Full-Stack Engineer/backend/intro-backend/express-routes.js
@@ -1,4 +1,7 @@
-// Import the express library here
+///////////////////////////////////////////////////////
+//Setup a server//////////////////////////////////////
+//////////////////////////////////////////////////////
+
 const express = require('express');
 const app = express();
 
@@ -7,4 +10,23 @@ const PORT = process.env.PORT || 4001;
 // Invoke the app's `.listen()` method below:
 app.listen(PORT, () => {
   console.log(`Server is listening on port ${PORT}`);
+});
+
+///////////////////////////////////////////////////////
+//Writing Your First Route////////////////////////////
+//////////////////////////////////////////////////////
+
+const express = require('express');
+const app = express();
+
+const PORT = process.env.PORT || 4001;
+// Use static server to serve the Express Yourself Website
+app.use(express.static('public'));
+
+// Open a call to `app.get()` below:
+app.get('/expressions', (req, res, next) => {
+console.log('hello')});
+
+app.listen(PORT, () => {
+  console.log(`Listening on port ${PORT}`);
 });
