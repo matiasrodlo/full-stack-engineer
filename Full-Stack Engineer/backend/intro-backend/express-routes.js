98a1526afa8990fa16d7efc197f3a896cef45a61
Matías Rodlo <m@hablaqui.com>
2024-02-29 16:06:48 -0300

    getting a single expression

--- Full-Stack Engineer/backend/intro-backend/express-routes.js
+++ Full-Stack Engineer/backend/intro-backend/express-routes.js
@@ -56,3 +56,33 @@ app.get('/expressions', (req, res, next) => {
 app.listen(PORT, () => {
   console.log(`Listening on port ${PORT}`);
 });
+
+///////////////////////////////////////////////////////
+//Getting A Single Expression//////////////////////////
+//////////////////////////////////////////////////////
+
+const express = require('express');
+const app = express();
+
+// Serves Express Yourself website
+app.use(express.static('public'));
+
+const { getElementById, seedElements } = require('./utils');
+
+const expressions = [];
+seedElements(expressions, 'expressions');
+
+const PORT = process.env.PORT || 4001;
+
+app.get('/expressions', (req, res, next) => {
+  res.send(expressions);
+});
+
+app.get('/expressions/:id', (req, res, next) => {
+  const foundExpression = getElementById(req.params.id, expressions);
+  res.send(foundExpression);
+});
+
+app.listen(PORT, () => {
+  console.log(`Listening on port ${PORT}`);
+});
