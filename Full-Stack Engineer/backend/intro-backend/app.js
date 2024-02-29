eb2eb27951fcf6cb2f0b62a319040d0482b0c4ed
Matías Rodlo <m@hablaqui.com>
2024-02-29 07:44:39 -0300

    writable streams

--- Full-Stack Engineer/backend/intro-backend/app.js
+++ Full-Stack Engineer/backend/intro-backend/app.js
@@ -252,3 +252,23 @@ const printData = (data) => {
 };
 
 myInterface.on('line', printData);
+
+///////////////////////////////////////////////////////
+//Writeable Streams///////////////////////////////////
+//////////////////////////////////////////////////////
+
+const readline = require('readline');
+const fs = require('fs');
+
+const myInterface = readline.createInterface({
+  input: fs.createReadStream('shoppingList.txt')
+});
+
+
+const fileStream = fs.createWriteStream('shoppingResults.txt');
+
+let transformData = (line) => {
+ fileStream.write(`They were out of: ${line}\n`); 
+};
+
+myInterface.on('line', transformData);
