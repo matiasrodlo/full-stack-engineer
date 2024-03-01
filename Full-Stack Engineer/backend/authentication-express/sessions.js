2e0dc9ed59122c7acd00d49c0ed9b6335148738c
Matías Rodlo <m@hablaqui.com>
2024-03-01 15:12:03 -0300

    serializing

--- Full-Stack Engineer/backend/authentication-express/sessions.js
+++ Full-Stack Engineer/backend/authentication-express/sessions.js
@@ -311,3 +311,65 @@ app.get("/", (req, res) => {
 app.listen(PORT, () => {
   console.log(`Server is listening on port: ${PORT}`);
 });
+
+////////////////////////////////////////////////////
+//Serializing and Deserializing Users///////////////
+////////////////////////////////////////////////////
+
+const express = require("express");
+const app = express();
+const passport = require("passport");
+const LocalStrategy = require("passport-local").Strategy;
+const session = require("express-session");
+const PORT = process.env.PORT || 5000;
+
+app.use(
+  session({
+    secret: "secret-key",
+    resave: false,
+    saveUninitialized: false,
+  })
+);
+
+app.use(passport.initialize());
+app.use(passport.session());
+
+// Complete the serializeUser function below:
+passport.serializeUser((user, done) => {
+  done(null, user.id);
+  // Look up user id in database. 
+
+});
+
+// Complete the deserializeUser function below:
+passport.deserializeUser((id, done) => {
+  db.users.findById(id, function (err, user) {
+    if (err) return done(err); 
+    done(null, user);
+  });
+});
+
+passport.use(
+  new LocalStrategy(function (username, password, done) {
+    db.users.findByUsername(username, (err, user) => {
+      if (err) {
+        return done(err);
+      }
+      if (!user) {
+        return done(null, false);
+      }
+      if (user.password != password) {
+        return done(null, false);
+      }
+      return done(null, user);
+    });
+  })
+);
+
+app.get("/", (req, res) => {
+  res.send("Hello from the homepage!");
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is listening on port: ${PORT}`);
+});
