3cbdcd011280d9bbb2439a387926f48d7fa147da
Matías Rodlo <m@hablaqui.com>
2024-03-01 19:29:27 -0300

    getuserfromclient

--- Full-Stack Engineer/backend/authentication-express/oauth2.js
+++ Full-Stack Engineer/backend/authentication-express/oauth2.js
@@ -133,3 +133,39 @@ module.exports = {
 module.exports = {
   getClient: getClient
 }
+
+/////////////////////////////////////////////////////
+//getUserFromClient//////////////////////////////////
+////////////////////////////////////////////////////
+
+let db = require('./db.js');
+
+const getClient = (clientId, clientSecret) => {
+  let confidentialClients = db.confidentialClients.filter((client) => {
+    return client.clientId === clientId && client.clientSecret === clientSecret
+  });
+  return confidentialClients[0];
+}
+
+const saveToken = (token, client, user) => {
+  token.client = {
+    id: client.clientId
+  }
+  token.user = {
+    username: user.username
+  }
+  db.tokens.push(token);
+  return token;
+}
+
+
+// Write getUserFromClient() function here
+const getUserFromClient = (client) => {
+  return {}
+}
+// Export getUserFromClient() function here
+module.exports = {
+  getClient: getClient,
+  saveToken: saveToken,
+  getUserFromClient: getUserFromClient
+}
