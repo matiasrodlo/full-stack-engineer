49c9cdfbeba8a646e1e7a1ab21533e8e446b41d4
Matías Rodlo <m@hablaqui.com>
2024-03-01 19:11:58 -0300

    getClient

--- Full-Stack Engineer/backend/authentication-express/oauth2.js
+++ Full-Stack Engineer/backend/authentication-express/oauth2.js
@@ -81,3 +81,20 @@ module.exports = {
   
   }
   
+/////////////////////////////////////////////////////
+//getClient()////////////////////////////////////////
+////////////////////////////////////////////////////
+
+  let db = require("./db.js");
+
+// Write getClient() function here
+const getClient = (clientId, clientSecret) => {
+  let confidentialClients = db.confidentialClients.filter((client) => {
+    return client.clientId === clientId && client.clientSecret === clientSecret;
+  });
+  return confidentialClients[0];
+};
+
+module.exports = {
+  getClient: getClient
+}
