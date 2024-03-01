dbbf6a71a9010e0184b0ba1bc113187d775ab03a
Matías Rodlo <m@hablaqui.com>
2024-03-01 19:41:54 -0300

    token handler

--- Full-Stack Engineer/backend/authentication-express/oauth2.js
+++ Full-Stack Engineer/backend/authentication-express/oauth2.js
@@ -169,3 +169,56 @@ module.exports = {
   saveToken: saveToken,
   getUserFromClient: getUserFromClient
 }
+
+////////////////////////////////////////////////////
+//Obtaining Token Handler///////////////////////////
+////////////////////////////////////////////////////
+
+const express = require("express");
+const path = require("path");
+const OAuth2Server = require("oauth2-server");
+
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+const PORT = 4001;
+
+const oauth = new OAuth2Server({
+  model: require("./model"),
+  allowBearerTokensInQueryString: true,
+});
+
+// Write obtainToken() here
+
+const obtainToken = (req, res) => {
+  let request = new OAuth2Server.Request(req);
+  let response = new OAuth2Server.Response(res);
+
+  return oauth
+    .token(request, response)
+    .then((token) => {
+      res.json(token);
+    })
+    .catch((err) => {
+      res.json(err);
+    });
+};
+
+// Write '/auth' route here
+app.all('/auth', obtainToken);
+
+// Do not change below this line
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "public/home.html"));
+});
+app.get("/login", (req, res) => {
+  res.sendFile(path.join(__dirname, "public/login.html"));
+});
+
+app.get("/secret", (req, res) => {
+  res.send("Welcome to the secret area.");
+});
+
+app.listen(PORT, () => console.log(`Listening on port ${PORT}`));
