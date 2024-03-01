2e9d3b957304b9dc77b36c32255cf2263e92e6de
Matías Rodlo <m@hablaqui.com>
2024-03-01 18:45:53 -0300

    oauth2

---
+++ Full-Stack Engineer/backend/authentication-express/oauth2.js
@@ -0,0 +1,28 @@
+/////////////////////////////////////////////////////
+//Oauth2-server/////////////////////////////
+////////////////////////////////////////////////////
+
+const express = require('express');
+const path = require('path');
+// Import oauth2-server here
+const OAuth2Server = require('oauth2-server');
+
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
+
+const PORT = 4001;
+
+app.get('/', (req, res)=>{
+    res.sendFile(path.join(__dirname, 'public/home.html'));
+})
+app.get('/login', (req, res)=>{
+    res.sendFile(path.join(__dirname, 'public/login.html'));
+})
+
+app.get('/secret', (req, res)=>{
+    res.send('Welcome to the secret area.');
+})
+
+app.listen(PORT, ()=>console.log(`Listening on port ${PORT}`));
