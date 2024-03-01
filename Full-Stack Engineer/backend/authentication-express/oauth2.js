face7b4f2b5a6dcef2477173b7072308f1dca098
Matías Rodlo <m@hablaqui.com>
2024-03-01 19:48:47 -0300

    getaccesstoken

--- Full-Stack Engineer/backend/authentication-express/oauth2.js
+++ Full-Stack Engineer/backend/authentication-express/oauth2.js
@@ -222,3 +222,48 @@ app.get("/secret", (req, res) => {
 });
 
 app.listen(PORT, () => console.log(`Listening on port ${PORT}`));
+
+////////////////////////////////////////////////////
+//getAccessToken////////////////////////////////////
+////////////////////////////////////////////////////
+
+let db = require('./db.js');
+
+const getClient = (clientId, clientSecret) => {
+  let confidentialClients = db.confidentialClients.filter((client) => {
+    return client.clientId === clientId && client.clientSecret === clientSecret
+  });
+  return confidentialClients[0];
+}
+
+const saveToken = (token, client, user) => {
+  token.client = {
+    id: client.clientId
+  }
+  token.user = {
+    username: user.username
+  }
+  db.tokens.push(token);
+  return token;
+}
+
+const getUserFromClient = (client) => {
+  return {}
+}
+
+// Write getAccessToken() function here
+
+const getAccessToken = (accessToken) => {
+ let tokens = db.tokens.filter((savedToken)=>{
+   return savedToken.accessToken === accessToken;
+ })
+ return tokens[0];
+}
+
+// Export getAccessToken() function here
+module.exports = {
+  getClient: getClient,
+  saveTokens: saveTokens,
+  getUserFromClient: getUserFromClient,
+  getAccessToken: getAccessToken
+}
