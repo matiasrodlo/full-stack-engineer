d03e1017c33b0b3638277f5029c344d60c1a6a3d
Matías Rodlo <m@hablaqui.com>
2024-03-01 19:20:22 -0300

    saveToken

--- Full-Stack Engineer/backend/authentication-express/oauth2.js
+++ Full-Stack Engineer/backend/authentication-express/oauth2.js
@@ -98,3 +98,38 @@ const getClient = (clientId, clientSecret) => {
 module.exports = {
   getClient: getClient
 }
+
+/////////////////////////////////////////////////////
+//saveToken()////////////////////////////////////////
+////////////////////////////////////////////////////
+
+let db = require('./db.js');
+
+// Write getClient() function here
+const getClient = (clientId, clientSecret) => {
+  let confidentialClients = db.confidentialClients.filter((client) => {
+    return client.clientId === clientId && client.clientSecret === clientSecret
+  });
+  return confidentialClients[0];
+}
+
+// Write saveToken() function here
+const saveToken = (token, client, user) => {
+  token.client = {
+    id: client.clientId
+  }
+  token.user = {
+    username: user.username
+  }
+  db.tokens.push(token);
+  return token;
+}
+
+module.exports = {
+  saveToken: saveToken
+}
+
+// Export saveToken() function here
+module.exports = {
+  getClient: getClient
+}
