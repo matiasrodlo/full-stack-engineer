9678c485cd7eabe48e3fb7b3f68531f9e1fc1441
Matías Rodlo <m@hablaqui.com>
2024-03-01 18:52:41 -0300

    creating an oauth

--- Full-Stack Engineer/backend/authentication-express/oauth2.js
+++ Full-Stack Engineer/backend/authentication-express/oauth2.js
@@ -26,3 +26,40 @@ app.get('/secret', (req, res)=>{
 })
 
 app.listen(PORT, ()=>console.log(`Listening on port ${PORT}`));
+
+/////////////////////////////////////////////////////
+//Creating an OAuth/////////////////////////////////
+////////////////////////////////////////////////////
+
+
+const express = require('express');
+const path = require('path');
+const OAuth2Server = require('oauth2-server');
+
+const app = express();
+
+// Create oauth instance here
+
+const oauth = new OAuth2Server({
+  model: require('./model.js'),
+  allowBearerTokensInQueryString: true,
+  accessTokenLifetime: 60 * 60
+})
+
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
+
+const PORT = 4001;
+
+app.get('/', (req, res)=>{
+    res.sendFile(path.join(__dirname, 'public/home.html'));
+})
+app.get('/login', (req, res)=>{
+    res.sendFile(path.join(__dirname, 'public/login.html'));
+})
+
+app.get('/secret', (req, res)=>{
+    res.send('Welcome to the secret area.');
+})
+
+app.listen(PORT, ()=>console.log(`Listening on port ${PORT}`));
