e2b69735357d326f24a17fec63ced59bacdeca7b
Matías Rodlo <m@hablaqui.com>
2024-03-03 12:23:18 -0300

    creating an expression

--- Full-Stack Engineer/backend/authentication-express/main.js
+++ Full-Stack Engineer/backend/authentication-express/main.js
@@ -33,9 +33,9 @@ var images = new Map([
     }
   }
 
-  //////////////////////////////////////////////////////////////////////////
-  //Using Queries///////////////////////////////////////////////////////////
-  //////////////////////////////////////////////////////////////////////////
+//////////////////////////////////////////////////////////////////////////
+//Using Queries///////////////////////////////////////////////////////////
+//////////////////////////////////////////////////////////////////////////
 
   const express = require('express');
 const app = express();
@@ -67,6 +67,57 @@ app.get('/expressions/:id', (req, res, next) => {
 app.put('/expressions/:id', (req, res, next) => {
   
 });
+
+app.listen(PORT, () => {
+  console.log(`Listening on port ${PORT}`);
+});
+
+//////////////////////////////////////////////////////////////////////////
+//Using Queries///////////////////////////////////////////////////////////
+//////////////////////////////////////////////////////////////////////////
+
+const express = require('express');
+const app = express();
+
+// Serves Express Yourself website
+app.use(express.static('public'));
+
+const { getElementById, getIndexById, updateElement,
+  seedElements, createElement } = require('./utils');
+
+const expressions = [];
+seedElements(expressions, 'expressions');
+
+const PORT = process.env.PORT || 4001;
+
+app.get('/expressions', (req, res, next) => {
+  res.send(expressions);
+});
+
+app.get('/expressions/:id', (req, res, next) => {
+  const foundExpression = getElementById(req.params.id, expressions);
+  if (foundExpression) {
+    res.send(foundExpression);
+  } else {
+    res.status(404).send();
+  }
+});
+
+app.put('/expressions/:id', (req, res, next) => {
+  const expressionIndex = getIndexById(req.params.id, expressions);
+  if (expressionIndex !== -1) {
+    updateElement(req.params.id, req.query, expressions);
+    res.send(expressions[expressionIndex]);
+  } else {
+    res.status(404).send();
+  }
+});
+
+// Add your POST handler below:
+const newElement = createElement('emoji', {name: 'example', emoji: ':)'});
+if (newElement) {
+  elements.push(newElement);
+}
 
 app.listen(PORT, () => {
   console.log(`Listening on port ${PORT}`);
