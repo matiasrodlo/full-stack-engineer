b0ac8e70db5a0806590e2ee090c2c0edda4ebfc6
Matías Rodlo <m@hablaqui.com>
2024-03-13 15:03:30 -0300

    code challenge 3

--- full-stack/backend/backend-express/routes-challenge.js
+++ full-stack/backend/backend-express/routes-challenge.js
@@ -151,3 +151,88 @@ soupsRouter.put('/soups', (req, res, next) => {
     res.status(400).send();
   }
 });
+
+///////////////////////////////////////////////////////////////////////////
+
+const express = require('express');
+const app = express();
+
+const PORT = process.env.PORT || 4001;
+
+const puddingFlavors = ['chocolate', 'banana', 'butterscotch', 'pistachio'];
+
+const findPuddingIndex = (name) => {
+  return puddingFlavors.indexOf(name);
+}
+
+const deletePuddingAtIndex = (index) => {
+  puddingFlavors.splice(index, 1);
+}
+
+// Your code here!
+app.delete('/puddings/:flavor', (req, res, next) => {
+  const findPuddingIndex = getIndexById(req.params.id, puddingFlavors);
+  if (findPuddingIndex !== -1) {
+    animals.splice(findPuddingIndex, 1);
+    res.status(204).send();
+  } else {
+    res.status(404).send();
+  }
+});
+
+
+app.listen(PORT, () => {
+  console.log(`Server is listening on port ${PORT}`);
+});
+
+////////////////////////////////////////////////////////////////////////////
+
+const express = require('express');
+const app = express();
+
+const PORT = process.env.PORT || 4001;
+
+app.listen(PORT, () => {
+  console.log(`Server is listening on port ${PORT}`);
+});
+
+const pastas = ['agnolotti', 'cavatelli', 'gemelli', 'tortellini'];
+
+app.get('/pastas', (req, res, next) => {
+  res.send(pastas);
+});
+
+const sauceRouter = express.Router();
+// Add your code here:
+app.use('/sauces', sauceRouter);
+
+// Get all animals
+app.get('/animals', (req, res, next) => {
+  res.send(animals);
+});
+
+const sauces = ['carbonara', 'primavera', 'bolognese', 'puttanesca', 'fra diavolo']
+
+sauceRouter.get('/', (req, res, next) => {
+  res.send(sauces);
+});
+
+//////////////////////////////////////////////////////////////////////////////
+
+const express = require('express');
+const app = express();
+
+const PORT = process.env.PORT || 4001;
+
+app.listen(PORT, () => {
+  console.log(`Server is listening on port ${PORT}`);
+});
+
+const mountains = ['denali', 'olympus', 'kilimanjaro', 'matterhorn'];
+const mountainRanges = ['alps', 'andes', 'himalayas', 'rockies'];
+
+const mountainsRouter = require('expressions.js');
+app.use('/mountains', mountainsRouter);
+
+const mountainRangesRouter = require('./expressions.js');
+app.use('/mountain-ranges', mountainRangesRouter);
