d195c423cc512879843e730436c18be538f4b894
Matías Rodlo <m@hablaqui.com>
2024-03-13 13:36:29 -0300

    refactoring routes

--- full-stack/backend/backend-express/app.js
+++ full-stack/backend/backend-express/app.js
@@ -231,3 +231,74 @@ app.delete('/animals/:id', (req, res, next) => {
 app.listen(PORT, () => {
   console.log(`Server is listening on ${PORT}`);
 });
+
+///////////////////////////////////////////////////////////////////////////
+//Refactoring Expressions Routes//////////////////////////////////////////
+//////////////////////////////////////////////////////////////////////////
+const express = require('express');
+const app = express();
+
+const { getElementById, getIndexById, updateElement,
+  seedElements, createElement } = require('./utils');
+
+const PORT = process.env.PORT || 4001;
+// Use static server to serve the Express Yourself Website
+app.use(express.static('public'));
+
+let animals = [];
+seedElements(animals, 'animals');
+
+const expressionsRouter = require('./expressions.js');
+app.use('/expressions', expressionsRouter);
+
+// Get all animals
+app.get('/animals', (req, res, next) => {
+  res.send(animals);
+});
+
+// Get a single animal
+app.get('/animals/:id', (req, res, next) => {
+  const animal = getElementById(req.params.id, animals);
+  if (animal) {
+    res.send(animal);
+  } else {
+    res.status(404).send();
+  }
+});
+
+// Create an animal
+app.post('/animals', (req, res, next) => {
+  const receivedAnimal = createElement('animals', req.query);
+  if (receivedAnimal) {
+    animals.push(receivedAnimal);
+    res.status(201).send(receivedAnimal);
+  } else {
+    res.status(400).send();
+  }
+});
+
+// Update an animal
+app.put('/animals/:id', (req, res, next) => {
+  const animalIndex = getIndexById(req.params.id, animals);
+  if (animalIndex !== -1) {
+    updateElement(req.params.id, req.query, animals);
+    res.send(animals[animalIndex]);
+  } else {
+    res.status(404).send();
+  }
+});
+
+// Delete a single animal
+app.delete('/animals/:id', (req, res, next) => {
+  const animalIndex = getIndexById(req.params.id, animals);
+  if (animalIndex !== -1) {
+    animals.splice(animalIndex, 1);
+    res.status(204).send();
+  } else {
+    res.status(404).send();
+  }
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is listening on ${PORT}`);
+});
