086363a56da33fe2560e367a0309020ba7efb3cc
Matías Rodlo <m@hablaqui.com>
2024-03-14 19:08:18 -0300

    testing asset 3

--- full-stack/backend/backend-testing/testing.js
+++ full-stack/backend/backend-testing/testing.js
@@ -22,3 +22,29 @@ describe("User visits root", () => {
     });
   });
 });
+
+///////////////////////////////////////////////////////////////////////////////
+//Feature Test II: Assert//////////////////////////////////////////////////////
+///////////////////////////////////////////////////////////////////////////////
+
+const { assert } = require("chai");
+
+describe("User visits root", () => {
+  describe("without existing messages", () => {
+    it("starts blank", () => {
+      browser.url("/");
+
+      assert.equal(browser.getText("#messages"), "");
+    });
+  });
+
+  describe("posting a message", () => {
+    it("saves the message with the author information", () => {
+      const message =
+        "feature tests often hit every level of the TDD Testing Pyramid";
+      const author = "username";
+    });
+  });
+});
+
+
