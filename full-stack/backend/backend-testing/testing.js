06b6e068d0a981b8dfc3934352e4652230f5d74b
Matías Rodlo <m@hablaqui.com>
2024-03-14 16:46:54 -0300

    testing js

---
+++ full-stack/backend/backend-testing/testing.js
@@ -0,0 +1,9 @@
+const {assert} = require('chai');
+
+describe('User visits root', () => {
+  describe('without existing messages', () => {
+    it('starts blank', () => {
+      browser.url('/');
+    });  
+  });
+});
