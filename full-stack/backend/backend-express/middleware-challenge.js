908008606377f921b087fc541e3b60b27d72dfa2
Matías Rodlo <m@hablaqui.com>
2024-03-14 12:55:55 -0300

    middleware challenge 5

--- full-stack/backend/backend-express/middleware-challenge.js
+++ full-stack/backend/backend-express/middleware-challenge.js
@@ -76,7 +76,6 @@ app.get('/appetizers', (req, res, next) => {
 //EXPRESS MIDDLEWARE CODE CHALLENGES 4 ////////////////////////////////////
 //////////////////////////////////////////////////////////////////////////
 
-
 const express = require('express');
 const app = express();
 
@@ -100,3 +99,23 @@ const errorHandler = (err, req, res, next) => {
 }
 
 app.use(errorHandler);
+
+///////////////////////////////////////////////////////////////////////////
+//EXPRESS MIDDLEWARE CODE CHALLENGES 5 ////////////////////////////////////
+//////////////////////////////////////////////////////////////////////////
+
+const express = require('express');
+const app = express();
+// Add your code below:
+const morgan = require('morgan');
+app.use(morgan('short'));
+
+// Add your code above
+
+app.get('/say-hi', (req, res, next) => {
+  res.send('Hi!');
+});
+
+app.get('/say-bye', (req, res, next) => {
+  res.send('Bye!');
+});
