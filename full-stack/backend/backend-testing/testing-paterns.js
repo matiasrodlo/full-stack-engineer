b17cd14de13ab8405fcb3b17b258e23d66e85a96
Matías Rodlo <m@hablaqui.com>
2024-03-15 13:07:48 -0300

    api error

--- full-stack/backend/backend-testing/testing-paterns.js
+++ full-stack/backend/backend-testing/testing-paterns.js
@@ -68,7 +68,7 @@ router.get('/:username', (req, res) => {
 module.exports = router;
 
 ////////////////////////////////////////////////////////////////////////////
-//Refactoring: Route Parameters/////////////////////////////////////////////
+//Refactoring: Handlers/////////////////////////////////////////////
 ////////////////////////////////////////////////////////////////////////////
 
 const express = require('express');
@@ -81,3 +81,54 @@ router.get('/:username', (req, res) => {
 });
 
 module.exports = router;
+
+////////////////////////////////////////////////////////////////////////////
+//API Error ////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////////////////
+
+const {assert} = require('chai');
+const request = require('supertest');
+const {jsdom} = require('jsdom');
+
+const app = require('../../app');
+
+const parseTextFromHTML = (htmlAsString, selector) => {
+  const selectedElement = jsdom(htmlAsString).querySelector(selector);
+  if (selectedElement !== null) {
+    return selectedElement.textContent;
+  } else {
+    throw new Error(`No element with selector ${selector} found in HTML string`);
+  }
+};
+
+describe('/messages', () => {
+  
+  describe('POST', () => {
+    describe('when the Message is valid', () => {
+      it('redirects to the index', async () => {
+        const author = 'Inquisitive User';
+        const message = 'Why Test?';
+
+        const response = await request(app)
+          .post('/messages')
+          .type('form')
+          .send({author, message});
+        assert.equal(response.status, 302);
+        assert.equal(response.headers.location, '/');
+      });
+    });
+
+    describe('when the author is blank', () => {
+      it('renders an error message', async () => {
+        const message = 'Server Testing';
+
+        const response = await request(app)
+          .post('/messages')
+          .send({message});
+        assert.equal(response.status, 400);
+        assert.equal(JSON.parse(response.text).message,  'Every message requires an author');
+        });
+    });
+
+  });
+});
