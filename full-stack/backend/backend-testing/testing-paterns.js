de7bada23693477243e37d92deda5e9c91a4ebb8
Matías Rodlo <m@hablaqui.com>
2024-03-15 12:48:07 -0300

    refactoring orutes

--- full-stack/backend/backend-testing/testing-paterns.js
+++ full-stack/backend/backend-testing/testing-paterns.js
@@ -52,3 +52,17 @@ describe("root page", () => {
     });
   });
 });
+
+////////////////////////////////////////////////////////////////////////////
+//Refactoring: Route Parameters/////////////////////////////////////////////
+////////////////////////////////////////////////////////////////////////////
+
+const express = require('express');
+const router = express.Router();
+
+
+router.get('/:username', (req, res) => {
+    res.send('<h1 id="welcome-message">Welcome ' + req.params.username + '!</h1>');
+});
+
+module.exports = router;
