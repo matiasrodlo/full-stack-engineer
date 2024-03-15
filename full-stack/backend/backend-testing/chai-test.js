f6817dc12f9d3a9119e860a03b01919349cd8358
Matías Rodlo <m@hablaqui.com>
2024-03-14 21:17:11 -0300

    async

--- full-stack/backend/backend-testing/chai-test.js
+++ full-stack/backend/backend-testing/chai-test.js
@@ -49,3 +49,21 @@ describe("HTML tests", () => {
     });
   });
 });
+
+//////////////////////////////////////////////////////////////////////////////
+//Async / Await///////////////////////////////////////////////////////////////
+//////////////////////////////////////////////////////////////////////////////
+
+
+const request = require('supertest');
+
+const app = require('../../app');
+
+describe('the homepage',  () => {
+  it('returns the correct content', async () => {
+    const response = await request(app)
+      .get('/')
+      .send();
+      console.log( response.text);
+    });
+});
