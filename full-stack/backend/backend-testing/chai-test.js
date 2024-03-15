c2ab944a12e65c7e7de57933519fa209526f1749
Matías Rodlo <m@hablaqui.com>
2024-03-14 21:11:38 -0300

    html response

--- full-stack/backend/backend-testing/chai-test.js
+++ full-stack/backend/backend-testing/chai-test.js
@@ -19,3 +19,33 @@ describe('Array', () => {
     });
   });
 });
+
+//////////////////////////////////////////////////////////////////////////////
+//Testing HTML Responses//////////////////////////////////////////////////////
+//////////////////////////////////////////////////////////////////////////////
+
+const { assert } = require("chai");
+const { jsdom } = require("jsdom");
+
+const parseTextFromHTML = (htmlAsString, selector) => {
+  const selectedElement = jsdom(htmlAsString).querySelector(selector);
+  if (selectedElement !== null) {
+    return selectedElement.textContent;
+  } else {
+    throw new Error(
+      `No element with selector ${selector} found in HTML string`
+    );
+  }
+};
+
+describe("HTML tests", () => {
+  describe("#bar", () => {
+    it('should include the string "Hello"', () => {
+      // setup
+      const foo =
+        '<html><div id="bar">Hello</div><div id="buzz">Hello</div><html>';
+      //asserts
+      assert.include(parseTextFromHTML(foo, "#bar"), "Hello");
+    });
+  });
+});
