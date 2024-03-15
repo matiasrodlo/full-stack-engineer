807be296068aa125bc24dadda119b0621d3bca42
Matías Rodlo <m@hablaqui.com>
2024-03-15 12:59:02 -0300

    habldersbars refactroing

--- full-stack/backend/backend-testing/testing-paterns.js
+++ full-stack/backend/backend-testing/testing-paterns.js
@@ -66,3 +66,18 @@ router.get('/:username', (req, res) => {
 });
 
 module.exports = router;
+
+////////////////////////////////////////////////////////////////////////////
+//Refactoring: Route Parameters/////////////////////////////////////////////
+////////////////////////////////////////////////////////////////////////////
+
+const express = require('express');
+const router = express.Router();
+
+
+router.get('/:username', (req, res) => {
+    const username = req.params.username;
+    res.render('profile', {username});
+});
+
+module.exports = router;
