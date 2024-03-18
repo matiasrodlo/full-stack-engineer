6a37b8c509c5cb8ea5b0d39b95dbf7f8cc22f0ab
Matías Rodlo <m@hablaqui.com>
2024-03-18 16:27:02 -0300

    focus my trigger

--- full-stack/backend/design-relational-bd/database-triggers/script.sql
+++ full-stack/backend/design-relational-bd/database-triggers/script.sql
@@ -52,3 +52,29 @@ UPDATE customers
 SET years_old = years_old + 1;
 
 SELECT * FROM customers;
+
+-- Focus trigger    
+
+CREATE TRIGGER update_trigger_high
+  BEFORE UPDATE ON clients
+  FOR EACH ROW
+  WHEN (NEW.total_spent >= 1000)
+  EXECUTE PROCEDURE set_high_spender();
+
+CREATE TRIGGER update_trigger_low
+  BEFORE UPDATE ON clients
+  FOR EACH ROW
+  WHEN (NEW.total_spent < 1000)
+  EXECUTE PROCEDURE set_low_spender();
+
+SELECT * FROM clients ORDER BY client_id;
+
+UPDATE clients
+SET total_spent = 5000
+WHERE last_name = 'Campbell';
+
+UPDATE clients
+SET total_spent = 100
+WHERE last_name = 'Lewis';
+
+SELECT * FROM clients ORDER BY client_id;
