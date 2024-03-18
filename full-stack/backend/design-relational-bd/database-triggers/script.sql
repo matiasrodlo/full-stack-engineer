c210c9f17c8b2831117bf13eb8eb7f0e554b2550
Matías Rodlo <m@hablaqui.com>
2024-03-18 17:01:44 -0300

    hings to considerer

--- full-stack/backend/design-relational-bd/database-triggers/script.sql
+++ full-stack/backend/design-relational-bd/database-triggers/script.sql
@@ -78,3 +78,27 @@ SET total_spent = 100
 WHERE last_name = 'Lewis';
 
 SELECT * FROM clients ORDER BY client_id;
+
+-- Things to consider
+
+CREATE TRIGGER update_alpha
+  BEFORE UPDATE ON orders
+  FOR EACH ROW
+  EXECUTE PROCEDURE update_first();
+
+CREATE TRIGGER update_bravo
+  BEFORE UPDATE ON orders
+  FOR EACH ROW
+  EXECUTE PROCEDURE update_second();
+
+SELECT *
+FROM orders
+ORDER BY order_id;
+
+UPDATE orders
+SET quantity = 5
+WHERE order_id = 1234;
+
+SELECT *
+FROM orders
+ORDER BY order_id;
