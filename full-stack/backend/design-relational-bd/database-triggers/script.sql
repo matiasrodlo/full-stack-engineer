e84b4842a5fbfd405cab8bff5eb72e39e1e3cace
Matías Rodlo <m@hablaqui.com>
2024-03-18 16:08:08 -0300

    trigger activated

--- full-stack/backend/design-relational-bd/database-triggers/script.sql
+++ full-stack/backend/design-relational-bd/database-triggers/script.sql
@@ -19,3 +19,22 @@ INSERT INTO customers (first_name, last_name)
 VALUES ('John', 'Doe');
 
 SELECT * FROM customers;
+
+-- When is a Trigger Activated?
+
+SELECT *
+FROM customers
+ORDER BY customer_id;
+
+CREATE TRIGGER after_trigger
+AFTER UPDATE ON customers
+FOR EACH ROW
+EXECUTE PROCEDURE log_customers_change();
+
+UPDATE customers
+SET years_old = years_old + 10
+WHERE customer_id = 1;
+
+SELECT * FROM customers ORDER BY customer_id;
+
+SELECT * FROM customers_log;
