c282062ef709d8cb2aece67197bc68adff0bcc23
Matías Rodlo <m@hablaqui.com>
2024-03-18 21:34:27 -0300

    more check contrains

--- full-stack/backend/advance-postgres/contraints.sql
+++ full-stack/backend/advance-postgres/contraints.sql
@@ -48,3 +48,12 @@ ALTER COLUMN organization SET NOT NULL;
 
 ALTER TABLE speakers
 ADD CHECK (years_in_role < 100);
+
+-- Check Constraints Continued
+
+
+ALTER TABLE speakers 
+ADD CHECK (years_in_role < 100 AND years_in_role > 0);
+
+ALTER TABLE attendees 
+ADD CHECK (standard_tickets_reserved + vip_tickets_reserved = total_tickets_reserved);
