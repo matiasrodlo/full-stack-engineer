04fc2d3050dbadb26a596749967048266d486cfb
Matías Rodlo <m@hablaqui.com>
2024-03-20 11:35:39 -0300

    groups and inheritance

--- full-stack/backend/advance-postgres/security.sql
+++ full-stack/backend/advance-postgres/security.sql
@@ -61,3 +61,18 @@ SELECT
 FROM information_schema.table_privileges 
 WHERE table_schema  = 'census'
 AND grantee = 'writer';
+
+-- Groups and Inheritance
+
+CREATE ROLE pgdba WITH SUPERUSER CREATEDB NOLOGIN; 
+
+CREATE ROLE david WITH LOGIN IN ROLE pgdba, employees; 
+
+SELECT rolname, rolsuper 
+FROM pg_catalog.pg_roles 
+WHERE rolname = 'david';
+
+SET ROLE david;
+
+SELECT * 
+FROM cc_user.company_startdates;
