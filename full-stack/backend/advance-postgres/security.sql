20aebcb6977bf650ec2e8be8cb02c6e9bd320ca1
Matías Rodlo <m@hablaqui.com>
2024-03-19 21:39:00 -0300

    creating a modifying bd

--- full-stack/backend/advance-postgres/security.sql
+++ full-stack/backend/advance-postgres/security.sql
@@ -12,3 +12,18 @@ FROM information_schema.table_privileges
 WHERE grantee = 'analyst';
 
 SELECT * FROM event_log WHERE id = 1;
+
+-- Creating and Modifying Database Roles
+
+CREATE ROLE analyst WITH LOGIN NOSUPERUSER;
+
+CREATE ROLE analyst_mgmt WITH LOGIN CREATEROLE;
+
+ALTER ROLE analyst_mgmt WITH LOGIN CREATEDB;
+
+SET ROLE analyst_mgmt;
+
+CREATE ROLE wilson WITH LOGIN;
+
+SELECT * FROM pg_catalog.pg_roles 
+ORDER BY rolname;
