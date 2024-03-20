b09ae66a172f9450f2a1d56fdcbd31a3782cc2e1
Matías Rodlo <m@hablaqui.com>
2024-03-20 14:37:46 -0300

    multicolumn iondexes

--- full-stack/backend/advance-postgres/index.sql
+++ full-stack/backend/advance-postgres/index.sql
@@ -18,3 +18,21 @@ WHERE last_name = 'Jones';
 -- How to Build an Index
 
 CREATE INDEX customers_city_idx ON customers(city);
+
+-- Index Filtering
+
+CREATE INDEX customers_last_name_idx ON customers(last_name);
+
+--Multicolumn Indexes
+
+EXPLAIN ANALYZE SELECT *
+FROM customers
+WHERE last_name= 'Jones'
+     AND first_name= 'David';
+
+CREATE INDEX customers_last_name_first_name_idx ON customers(last_name, first_name);
+
+EXPLAIN ANALYZE SELECT *
+FROM customers
+WHERE last_name= 'Jones'
+     AND first_name= 'David';
