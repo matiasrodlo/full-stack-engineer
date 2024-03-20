ab43e1a25f5018f872b50647ea8375262f1f4f90
Matías Rodlo <m@hablaqui.com>
2024-03-20 19:47:11 -0300

    combining indexes

--- full-stack/backend/advance-postgres/medium-index.sql
+++ full-stack/backend/advance-postgres/medium-index.sql
@@ -66,3 +66,17 @@ CREATE INDEX customers_last_name_first_name_email_address_idx ON customers (last
 EXPLAIN ANALYZE SELECT first_name, last_name, email_address
 FROM customers
 WHERE last_name = 'Smith';
+
+-- Combining Indexes
+
+EXPLAIN ANALYZE SELECT *
+FROM customers
+WHERE last_name = 'Jones'
+  AND first_name = 'Steve';
+
+CREATE INDEX customers_last_name_first_name_idx ON customers (last_name, first_name);
+
+EXPLAIN ANALYZE SELECT *
+FROM customers
+WHERE last_name = 'Jones'
+  AND first_name = 'Steve';
