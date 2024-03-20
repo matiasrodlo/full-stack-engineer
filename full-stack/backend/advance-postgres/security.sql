2c5ce94d322db4177470f4ce81233257a0eff5fa
Matías Rodlo <m@hablaqui.com>
2024-03-19 21:27:28 -0300

    superuser permissions

--- full-stack/backend/advance-postgres/security.sql
+++ full-stack/backend/advance-postgres/security.sql
@@ -1,1 +1,14 @@
 SELECT current_user;
+
+-- Investigating Superuser Permissions
+
+SELECT rolname
+FROM pg_catalog.pg_roles;
+
+SET ROLE analyst;
+
+SELECT grantee, table_name, privilege_type 
+FROM information_schema.table_privileges 
+WHERE grantee = 'analyst';
+
+SELECT * FROM event_log WHERE id = 1;
