f6993b042915bf102280bc2a03f7c90e0f3a268c
Matías Rodlo <m@hablaqui.com>
2024-03-20 19:00:36 -0300

    primarykey

--- full-stack/backend/advance-postgres/medium-index.sql
+++ full-stack/backend/advance-postgres/medium-index.sql
@@ -15,3 +15,27 @@ CREATE INDEX customers_years_old_teen_idx ON customers (years_old) WHERE years_o
 EXPLAIN ANALYZE SELECT *
 FROM customers
 WHERE years_old BETWEEN 13 AND 19;
+
+--Order By
+
+CREATE INDEX customers_state_name_email_address_idx ON customers(state_name, email_address);
+
+EXPLAIN ANALYZE SELECT state_name, email_address
+FROM customers
+WHERE state_name = 'California' OR state_name = 'Ohio'
+ORDER BY state_name DESC, email_address;
+
+CREATE INDEX customers_state_name_email_address_ordered_idx ON customers(state_name DESC, email_address ASC);
+
+EXPLAIN ANALYZE SELECT state_name, email_address
+FROM customers
+WHERE state_name = 'California' OR state_name = 'Ohio'
+ORDER BY state_name DESC, email_address
+
+-- primary key
+
+ALTER TABLE customers ADD PRIMARY KEY (customer_id);
+
+SELECT *
+FROM pg_Indexes
+WHERE tablename = 'customers';
