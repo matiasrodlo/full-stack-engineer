e77b2392e7221605a117793db1b504cb267c97d5
Matías Rodlo <m@hablaqui.com>
2024-03-20 14:15:33 -0300

    impact of indexes

--- full-stack/backend/advance-postgres/index.sql
+++ full-stack/backend/advance-postgres/index.sql
@@ -4,3 +4,13 @@ FROM customers;
 SELECT *
 FROM pg_Indexes
 WHERE tablename = 'customers';
+
+-- Impact of Indexes
+
+EXPLAIN ANALYZE SELECT *
+FROM customers
+WHERE first_name = 'David';
+
+EXPLAIN ANALYZE SELECT *
+FROM customers
+WHERE last_name = 'Jones';
