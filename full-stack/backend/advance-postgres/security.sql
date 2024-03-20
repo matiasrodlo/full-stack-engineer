cd9325e99f1629abd47fa2166cfadb96ca08e0d1
Matías Rodlo <m@hablaqui.com>
2024-03-19 22:06:01 -0300

    modifying default permissions

--- full-stack/backend/advance-postgres/security.sql
+++ full-stack/backend/advance-postgres/security.sql
@@ -40,3 +40,24 @@ REVOKE DELETE ON marketing.prospects FROM analyst;
 SET ROLE analyst;
 
 DELETE FROM marketing.prospects WHERE id = 2;
+
+-- Modifying Default Permissions
+
+GRANT SELECT, DELETE, UPDATE, INSERT ON census.economic_survey TO writer;
+
+ALTER DEFAULT PRIVILEGES IN SCHEMA census
+GRANT SELECT, DELETE, UPDATE, INSERT ON TABLES TO writer;
+
+CREATE TABLE census.housing_survey (
+    area_id int primary key, 
+    median_rent int
+);
+
+SELECT
+    grantee, 
+    table_schema,
+    table_name,
+    privilege_type
+FROM information_schema.table_privileges 
+WHERE table_schema  = 'census'
+AND grantee = 'writer';
