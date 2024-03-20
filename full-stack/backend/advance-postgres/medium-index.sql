cd0853b4613fa0e2a9adb65f8cc2fe24641188a6
Matías Rodlo <m@hablaqui.com>
2024-03-20 19:18:30 -0300

    non clustered index

--- full-stack/backend/advance-postgres/medium-index.sql
+++ full-stack/backend/advance-postgres/medium-index.sql
@@ -45,3 +45,12 @@ WHERE tablename = 'customers';
 CLUSTER customers USING customers_last_name_idx;
 
 CLUSTER customers
+
+-- non clustered index
+
+CREATE INDEX customers_state_name_idx ON customers(state_name);
+
+SELECT last_name, state_name
+FROM customers
+WHERE state_name = 'Texas'
+ORDER BY last_name;
