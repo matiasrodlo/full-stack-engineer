f3536a0be0ce9ae1face7c4818a3ce9f3d2ac34b
Matías Rodlo <m@hablaqui.com>
2024-03-29 19:11:45 -0300

    trunca te

--- full-stack/backend/advance-postgres/maintenance.sql
+++ full-stack/backend/advance-postgres/maintenance.sql
@@ -75,3 +75,12 @@ VACUUM FULL mock.orders;
 SELECT pg_size_pretty(
     pg_total_relation_size('mock.orders')
 ) as total_size;
+
+-- Truncate
+
+TRUNCATE mock.current_day_logins;
+
+SELECT 
+    pg_size_pretty(pg_table_size('mock.current_day_logins')) as tbl_size, 
+    pg_size_pretty(pg_indexes_size('mock.current_day_logins')) as idx_size,
+    pg_size_pretty(pg_total_relation_size('mock.current_day_logins')) as total_size;
