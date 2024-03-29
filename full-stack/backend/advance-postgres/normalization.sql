f0fb4ccaae390e74a7a3289bc334008a51c1262c
Matías Rodlo <m@hablaqui.com>
2024-03-29 13:16:01 -0300

    creating vs modifiying

--- full-stack/backend/advance-postgres/normalization.sql
+++ full-stack/backend/advance-postgres/normalization.sql
@@ -82,3 +82,31 @@ SELECT *
 FROM students_majors
 ORDER BY student_id
 LIMIT 10;
+
+-- Creating Versus Modifying a Database Schema
+
+CREATE TABLE advisors (
+  id INTEGER PRIMARY KEY,
+  email VARCHAR(100) UNIQUE,
+  name VARCHAR(100),
+  department VARCHAR(100)
+);
+
+CREATE TABLE students (
+  id INTEGER PRIMARY KEY,
+  name VARCHAR(100),
+  year VARCHAR(50),
+  email VARCHAR(100) UNIQUE,
+  advisor_id INTEGER REFERENCES advisors(id)
+);
+
+CREATE TABLE majors (
+  id INTEGER PRIMARY KEY,
+  major VARCHAR(100),
+  credits_reqd INTEGER
+);
+
+CREATE TABLE students_majors (
+  student_id INTEGER REFERENCES students(id),
+  major_id INTEGER REFERENCES majors(id)
+);
