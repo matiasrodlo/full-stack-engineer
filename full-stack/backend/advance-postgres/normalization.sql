51395eb38f0a8e8c11e2707ae06143a74d86a3da
Matías Rodlo <m@hablaqui.com>
2024-03-28 21:45:07 -0300

    Update normalization.sql

--- full-stack/backend/advance-postgres/normalization.sql
+++ full-stack/backend/advance-postgres/normalization.sql
@@ -2,6 +2,8 @@ SELECT *
 FROM college
 LIMIT 10;
 
+-- Duplicated Data
+
 SELECT advisor_name, advisor_department, advisor_email
 FROM college
 WHERE advisor_name = 'Brunson';
@@ -9,3 +11,13 @@ WHERE advisor_name = 'Brunson';
 SELECT major_1, major_1_credits_reqd
 FROM college
 WHERE major_1 = 'Computer Science';
+
+-- Data Update Challenges
+
+SELECT advisor_name,advisor_department, advisor_email 
+FROM college 
+WHERE advisor_name='Sommer';
+
+UPDATE college
+SET advisor_email = 'sophie@college.edu'
+WHERE advisor_name = 'Sommer' AND advisor_department = 'Statistics';
