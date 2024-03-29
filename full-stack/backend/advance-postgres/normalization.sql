8adb154fe99576fc270fe4c6a2fc9e0778ae38db
Matías Rodlo <m@hablaqui.com>
2024-03-28 21:36:59 -0300

    Update normalization.sql

--- full-stack/backend/advance-postgres/normalization.sql
+++ full-stack/backend/advance-postgres/normalization.sql
@@ -1,3 +1,11 @@
 SELECT *
 FROM college
 LIMIT 10;
+
+SELECT advisor_name, advisor_department, advisor_email
+FROM college
+WHERE advisor_name = 'Brunson';
+
+SELECT major_1, major_1_credits_reqd
+FROM college
+WHERE major_1 = 'Computer Science';
