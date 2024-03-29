6582a1a5486230b85e6320d2502dfdb024643370
Matías Rodlo <m@hablaqui.com>
2024-03-29 17:05:00 -0300

    object size

--- full-stack/backend/advance-postgres/maintenance.sql
+++ full-stack/backend/advance-postgres/maintenance.sql
@@ -1,1 +1,15 @@
 SELECT * from mock.time_series LIMIT 10;
+
+-- Understanding Object Size
+
+SELECT pg_size_pretty(
+  pg_table_size('mock.house_prices')
+);
+
+SELECT pg_size_pretty( 
+    pg_indexes_size('mock.house_prices')
+);
+
+SELECT pg_size_pretty(pg_total_relation_size('mock.house_prices_pk'));
+
+SELECT pg_size_pretty(pg_total_relation_size('mock.house_prices'));
