529f4e7c6470cd84d223e6195250fcdaeeaed6f5
Matías Rodlo <m@hablaqui.com>
2024-03-28 22:00:09 -0300

    Update normalization.sql

--- full-stack/backend/advance-postgres/normalization.sql
+++ full-stack/backend/advance-postgres/normalization.sql
@@ -30,3 +30,18 @@ VALUES ('-1', 'Stern', 'Biology', 'stern111@college.edu');
 SELECT *
 FROM college
 WHERE student_id = '-1'
+
+-- Search and Sort Efficiency
+
+WITH majors AS(
+SELECT major_1 as major 
+FROM college
+UNION ALL
+SELECT major_2 as major
+FROM college
+WHERE major_2 IS NOT NULL
+)
+SELECT major, count(*)
+FROM majors
+GROUP BY major
+ORDER BY count DESC;
