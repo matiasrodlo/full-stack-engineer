72ec765974358f6d9d194ee45cdb7001acb25019
Matías Rodlo <m@hablaqui.com>
2024-03-29 19:00:21 -0300

    vacuum full

--- full-stack/backend/advance-postgres/maintenance.sql
+++ full-stack/backend/advance-postgres/maintenance.sql
@@ -63,3 +63,15 @@ ANALYZE mock.stock_prices;
 SELECT relname, n_dead_tup 
 FROM pg_stat_all_tables 
 WHERE relname = mock.stock_prices;
+
+-- Vacuum Full
+
+select relname, n_live_tup, n_dead_tup , last_vacuum
+from pg_catalog.pg_stat_all_tables
+where relname = 'orders';
+
+VACUUM FULL mock.orders;
+
+SELECT pg_size_pretty(
+    pg_total_relation_size('mock.orders')
+) as total_size;
