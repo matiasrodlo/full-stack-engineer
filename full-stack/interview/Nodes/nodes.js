abe46a327ab482a1669ea7e007d9decdc4c2214a
Matías Rodlo <m@hablaqui.com>
2024-03-30 20:40:06 -0300

    set next node

--- full-stack/interview/Nodes/nodes.js
+++ full-stack/interview/Nodes/nodes.js
@@ -12,4 +12,25 @@ class Node {
   console.log(firstNode.next);
   
   module.exports = Node;
+
+  // Set Next Node
+
+  class Node {
+    constructor(data) {
+      this.data = data;
+      this.next = null;
+    }
+    setNextNode(node) {
+      this.next = node;
+    }
+  }
+  
+  const firstNode = new Node("I am an instance of a Node!");
+  const secondNode = new Node('I am the next Node!');
+  
+  firstNode.setNextNode(secondNode);
+  console.log(firstNode);
+  
+  module.exports = Node;
+  
   
