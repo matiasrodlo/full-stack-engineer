70481e5b7db97241635459dc1a94e828f43eea6d
Matías Rodlo <m@hablaqui.com>
2024-03-30 20:51:04 -0300

    set next node validation

--- full-stack/interview/Nodes/nodes.js
+++ full-stack/interview/Nodes/nodes.js
@@ -33,4 +33,26 @@ class Node {
   
   module.exports = Node;
   
+  // Set Next Node Validation
+
+  class Node {
+    constructor(data) {
+      this.data = data;
+      this.next = null;
+    }
+  
+    setNextNode(node) {
+      if (node instanceof Node || node === null) {
+        this.next = node;
+      } else {
+        throw new Error('Next node must be a member of the Node class.');
+      }
+    }
+  }
+  
+  const firstNode = new Node('I am an instance of a Node!');
+  firstNode.setNextNode('This is a string, not a Node.');
+  
+  module.exports = Node;
+  
   
