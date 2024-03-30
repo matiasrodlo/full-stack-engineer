1756456bb093740075c55160ea87eee2bd9bcc26
Matías Rodlo <m@hablaqui.com>
2024-03-30 20:23:44 -0300

    intro to nodes

---
+++ full-stack/interview/Nodes/nodes.js
@@ -0,0 +1,15 @@
+// Introduction
+
+class Node {
+    constructor(data) {
+      this.data = data;
+      this.next = null;
+    }
+  }
+  
+  const firstNode = new Node('2');
+  console.log(firstNode.data);
+  console.log(firstNode.next);
+  
+  module.exports = Node;
+  
