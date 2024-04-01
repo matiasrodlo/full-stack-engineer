bfd5a3f04f08f00357fe82ad6dbcf0d08609dd7a
Matías Rodlo <m@hablaqui.com>
2024-04-01 19:38:03 -0300

    node class and constructor

--- full-stack/interview/Nodes/nodes.js
+++ full-stack/interview/Nodes/nodes.js
@@ -331,3 +331,28 @@ const findMiddle = linkedList => {
   // At this point, the slow pointer is in the middle
   return slow;
 };
+
+// Node Class and Constructor
+
+const Node = require("./Node");
+
+class DoublyLinkedList {
+  // Create your constructor below:
+  constructor() {
+    this.head = null;
+    this.tail = null;
+  }
+
+  printList() {
+    let currentNode = this.head;
+    let output = "<head> ";
+    while (currentNode !== null) {
+      output += currentNode.data + " ";
+      currentNode = currentNode.getNextNode();
+    }
+    output += "<tail>";
+    console.log(output);
+  }
+}
+
+module.exports = DoublyLinkedList;
