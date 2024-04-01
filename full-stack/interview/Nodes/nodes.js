feb69bb4ee609e2b474231be8049919a107fef21
Matías Rodlo <m@hablaqui.com>
2024-04-01 18:59:48 -0300

    pointers at different sppeds

--- full-stack/interview/Nodes/nodes.js
+++ full-stack/interview/Nodes/nodes.js
@@ -310,3 +310,24 @@ const nthLastNode = (linkedList, n) => {
   // Pointer1 is now at the nth last node
   return pointer1;
 };
+ 
+// Pointers at Different Speeds
+
+const findMiddle = linkedList => {
+  let fast = linkedList.head;
+  let slow = linkedList.head;
+
+  // As long as the end of the list is not reached
+  while (fast !== null) {
+    // Move the fast pointer at least one step
+    fast = fast.getNextNode();
+    // If it isn't at the end of the list
+    if (fast !== null) {
+      // Move both pointers forward once
+      fast = fast.getNextNode();
+      slow = slow.getNextNode();
+    }
+  }
+  // At this point, the slow pointer is in the middle
+  return slow;
+};
