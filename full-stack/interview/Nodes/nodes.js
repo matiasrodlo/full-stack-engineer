2db7907f33cacc04c32b91cd5f995521b2535488
Matías Rodlo <m@hablaqui.com>
2024-04-01 20:02:31 -0300

    add to tail

--- full-stack/interview/Nodes/nodes.js
+++ full-stack/interview/Nodes/nodes.js
@@ -394,3 +394,54 @@ class DoublyLinkedList {
 }
 
 module.exports = DoublyLinkedList;
+
+// Add To Tail
+
+const Node = require('./Node');
+
+class DoublyLinkedList {
+  constructor() {
+    this.head = null;
+    this.tail = null;
+  }
+  
+  addToHead(data) {
+    const newHead = new Node(data);
+    const currentHead = this.head;
+    if (currentHead) {
+      currentHead.setPreviousNode(newHead);
+      newHead.setNextNode(currentHead);
+    }
+    this.head = newHead;
+    if (!this.tail) {
+      this.tail = newHead;
+    }
+  }
+  
+  // Create your .addToTail() method below:
+  addToTail(data) {
+    const newTail = new Node(data);
+    const currentTail = this.tail;
+    if (currentTail) {
+      currentTail.setNextNode(newTail);
+      newTail.setPreviousNode(currentTail);
+    }
+    this.tail = newTail;
+    if (!this.head) {
+      this.head = newTail;
+    }
+  }
+  
+  printList() {
+    let currentNode = this.head;
+    let output = '<head> ';
+    while (currentNode !== null) {
+      output += currentNode.data + ' ';
+      currentNode = currentNode.getNextNode();
+    }
+    output += '<tail>';
+    console.log(output);
+  }
+}
+
+module.exports = DoublyLinkedList;
