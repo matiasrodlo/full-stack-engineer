a8d0d2e0b22100bcdf58cb69cd9f445447c8040e
Matías Rodlo <m@hablaqui.com>
2024-04-01 20:13:51 -0300

    remove head

--- full-stack/interview/Nodes/nodes.js
+++ full-stack/interview/Nodes/nodes.js
@@ -445,3 +445,61 @@ class DoublyLinkedList {
 }
 
 module.exports = DoublyLinkedList;
+
+// Remove Head
+
+const Node = require("./Node");
+
+class DoublyLinkedList {
+  constructor() {
+    this.head = null;
+    this.tail = null;
+  }
+
+  addToHead(data) {
+    const newHead = new Node(data);
+    const currentHead = this.head;
+    if (currentHead) {
+      currentHead.setPreviousNode(newHead);
+      newHead.setNextNode(currentHead);
+    }
+    this.head = newHead;
+    if (!this.tail) {
+      this.tail = newHead;
+    }
+  }
+
+  addToTail(data) {
+    const newTail = new Node(data);
+    const currentTail = this.tail;
+    if (currentTail) {
+      currentTail.setNextNode(newTail);
+      newTail.setPreviousNode(currentTail);
+    }
+    this.tail = newTail;
+    if (!this.head) {
+      this.head = newTail;
+    }
+  }
+
+  // Create your .removeHead() method below:
+  removeHead() {
+    const removedHead = this.head;
+    if (!removedHead) {
+      return;
+    }
+  }
+
+  printList() {
+    let currentNode = this.head;
+    let output = "<head> ";
+    while (currentNode !== null) {
+      output += currentNode.data + " ";
+      currentNode = currentNode.getNextNode();
+    }
+    output += "<tail>";
+    console.log(output);
+  }
+}
+
+module.exports = DoublyLinkedList;
