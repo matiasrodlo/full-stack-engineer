81e286ede90fe92833b82a69b491d26b635584f3
Matías Rodlo <m@hablaqui.com>
2024-04-01 19:47:16 -0300

    add to head

--- full-stack/interview/Nodes/nodes.js
+++ full-stack/interview/Nodes/nodes.js
@@ -356,3 +356,41 @@ class DoublyLinkedList {
 }
 
 module.exports = DoublyLinkedList;
+
+// Add To Head
+
+const Node = require("./Node");
+
+class DoublyLinkedList {
+  constructor() {
+    this.head = null;
+    this.tail = null;
+  }
+
+  // Create your .addToHead() method below:
+  addToHead(data) {
+    const newHead = new Node(data);
+    const currentHead = this.head;
+        if (currentHead) {
+      currentHead.setPreviousNode(newHead);
+      newHead.setNextNode(currentHead);
+    }
+    this.head = newHead;
+    if (!this.tail) {
+      this.tail = newHead;
+    }
+  }
+
+  printList() {
+    let currentNode = this.head;
+    let output = "<head> ";
+    while (currentNode !== null) {
+      output += currentNode.data + " ";
+      currentNode = currentNode.getNextNode();
+    }
+    output += "<tail>";
+    console.log(output);
+  }
+}
+
+module.exports = DoublyLinkedList;
