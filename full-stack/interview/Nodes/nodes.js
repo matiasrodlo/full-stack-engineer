d4aba5ab8b4b955df11e189a47c7892bd1b839a0
Matías Rodlo <m@hablaqui.com>
2024-04-01 13:28:48 -0300

    using the linked list

--- full-stack/interview/Nodes/nodes.js
+++ full-stack/interview/Nodes/nodes.js
@@ -268,3 +268,21 @@ class LinkedList {
 }
 
 module.exports = LinkedList;
+
+// Using the Linked List
+
+const LinkedList = require('./LinkedList');
+
+const seasons = new LinkedList();
+seasons.printList();
+
+seasons.addToHead('summer')
+seasons.addToHead('spring')
+seasons.printList()
+
+seasons.addToTail('fall')
+seasons.addToTail('winter')
+seasons.printList()
+
+seasons.removeHead()
+seasons.printList()
