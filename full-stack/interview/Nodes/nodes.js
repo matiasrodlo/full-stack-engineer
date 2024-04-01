9a65ba35a59645526b64c24426fa5335f196ea97
Matías Rodlo <m@hablaqui.com>
2024-04-01 12:55:47 -0300

    adding to tail

--- full-stack/interview/Nodes/nodes.js
+++ full-stack/interview/Nodes/nodes.js
@@ -137,4 +137,37 @@ class LinkedList {
 
 module.exports = LinkedList;
 
+// Adding to Tail
+
+const Node = require('./Node');
+
+class LinkedList {
   
+  constructor() {
+    this.head = null;
+  }
+
+  addToHead(data) {
+    const newHead = new Node(data);
+    const currentHead = this.head;
+    this.head = newHead;
+    if (currentHead) {
+      this.head.setNextNode(currentHead);
+    }
+  }
+  
+  addToTail(data) {
+    let tail = this.head;
+    if (!tail) {
+      this.head = new Node(data);
+    } else {
+      while (tail.getNextNode() !== null) {
+        tail = tail.getNextNode();
+      }
+      tail.setNextNode(new Node(data));
+    }
+  }
+  
+}
+
+module.exports = LinkedList;
