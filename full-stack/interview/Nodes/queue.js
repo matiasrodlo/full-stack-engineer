bd9295a34382a1fe9cd81f11bd49dfec55868452
Matías Rodlo <m@hablaqui.com>
2024-04-04 14:51:29 -0300

    enqueue

--- full-stack/interview/Nodes/queue.js
+++ full-stack/interview/Nodes/queue.js
@@ -35,3 +35,41 @@ console.log(`restaurantOrder queue has ${restaurantOrder.size} orders.\n`);
 restaurantOrder.enqueue("apple pie");
 restaurantOrder.enqueue("roast chicken");
 restaurantOrder.enqueue("quinoa salad");
+
+// enqueue
+
+const LinkedList = require('./LinkedList');
+
+class Queue {
+  constructor() {
+    this.queue = new LinkedList();
+    this.size = 0;
+  }
+
+  enqueue(data) {
+    this.queue.addToTail(data);
+    this.size++;
+    console.log(`Added ${data} to queue! Queue size is now ${this.size}.`);
+  }
+
+  dequeue() {
+    const data = this.queue.removeHead();
+    this.size--;
+    console.log(`Removed ${data} from queue! Queue size is now ${this.size}.`);
+    return data;
+  }
+}
+
+module.exports = Queue;
+
+const restaurantOrder = new Queue();
+restaurantOrder.enqueue('apple pie');
+restaurantOrder.enqueue('roast chicken');
+restaurantOrder.enqueue('quinoa salad');
+console.log('\nFood preparing...\n')
+restaurantOrder.dequeue();
+restaurantOrder.dequeue();
+restaurantOrder.dequeue();
+console.log('All orders ready!')
+
+
