58d5ae88777c136464fb2290fee6b5cb35fbe358
Matías Rodlo <m@hablaqui.com>
2024-04-04 12:52:28 -0300

    remove tail

--- full-stack/interview/Nodes/nodes.js
+++ full-stack/interview/Nodes/nodes.js
@@ -503,3 +503,84 @@ class DoublyLinkedList {
 }
 
 module.exports = DoublyLinkedList;
+
+// Remove Tail
+
+const Node = require('./Node');
+
+class DoublyLinkedList {
+  constructor() {
+    this.head = null;
+    this.tail = null;
+  }
+  
+  addToHead(data) {
+    const newHead = new Node(data);
+    const currentHead = this.head;
+    if (currentHead) {
+      currentHead.setPreviousNode(newHead);
+      newHead.setNextNode(currentHead);
+    }
+    this.head = newHead;
+    if (!this.tail) {
+      this.tail = newHead;
+    }
+  }
+  
+  addToTail(data) {
+    const newTail = new Node(data);
+    const currentTail = this.tail;
+    if (currentTail) {
+      currentTail.setNextNode(newTail);
+      newTail.setPreviousNode(currentTail);
+    }
+    this.tail = newTail;
+    if (!this.head) {
+      this.head = newTail;
+    }
+  }
+  
+  removeHead() {
+    const removedHead = this.head;
+    if (!removedHead) {
+      return;
+    }
+    this.head = removedHead.getNextNode();
+    if (this.head) {
+      this.head.setPreviousNode(null);
+    }
+    if (removedHead === this.tail) {
+      this.removeTail();
+    }
+    return removedHead.data;
+  }
+  
+  // Create your .removeTail() method below:
+  removeTail() {
+    const removedTail = this.tail;
+    if (!removedTail) {
+      return;
+    }
+    this.tail = removedTail.getPreviousNode();
+    if (this.tail) {
+      this.tail.setNextNode(null);
+    }
+    if (removedTail === this.head) {
+      this.removeHead();
+    }
+    return removedTail.data;
+  }
+  
+  printList() {
+    let currentNode = this.head;
+    let output = '<head> ';
+    while (currentNode !== null) {
+      output += currentNode.data + ' ';
+      currentNode = currentNode.getNextNode();
+    }
+    output += '<tail>';
+    console.log(output);
+  }
+}
+
+module.exports = DoublyLinkedList;
