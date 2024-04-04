373579c6111b5ddad494d8446a7ec290acc321fc
Matías Rodlo <m@hablaqui.com>
2024-04-04 15:13:46 -0300

    avoiding underflow

--- full-stack/interview/Nodes/queue.js
+++ full-stack/interview/Nodes/queue.js
@@ -107,3 +107,47 @@ class Queue {
 }
 
 module.exports = Queue;
+
+// adding underflow
+
+const LinkedList = require("./LinkedList");
+
+class Queue {
+  constructor(maxSize = Infinity) {
+    this.queue = new LinkedList();
+    this.maxSize = maxSize;
+    this.size = 0;
+  }
+
+  isEmpty() {
+    return this.size === 0;
+  }
+
+  hasRoom() {
+    return this.size < this.maxSize;
+  }
+
+  enqueue(data) {
+    this.queue.addToTail(data);
+    this.size++;
+    console.log(`Added ${data} to queue! Queue size is now ${this.size}.`);
+  }
+
+  dequeue() {
+    if (!this.isEmpty()) {
+      const data = this.queue.removeHead();
+      this.size--;
+      console.log(`Removed ${data} from queue! Queue size is now ${this.size}.`);
+      return data;
+    } else {
+      throw new Error("Queue is empty!");
+    }
+  }
+}
+
+module.exports = Queue;
+
+const boundedQueue = new Queue(3);
+boundedQueue.enqueue(1);
+boundedQueue.enqueue(2);
+boundedQueue.enqueue(3);
