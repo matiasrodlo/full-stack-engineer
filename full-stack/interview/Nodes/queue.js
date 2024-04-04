aaa0a92a9866f6cddf1367659e559768eb66e18b
Matías Rodlo <m@hablaqui.com>
2024-04-04 14:39:46 -0300

    queue

---
+++ full-stack/interview/Nodes/queue.js
@@ -0,0 +1,13 @@
+const LinkedList = require('./LinkedList');
+
+class Queue {
+  constructor() {
+    this.queue = new LinkedList();
+    this.size = 0;
+  }
+}
+
+module.exports = Queue;
+
+const restaurantOrders = new Queue();
+console.log(`restaurantOrders has ${restaurantOrders.size} nodes`)
