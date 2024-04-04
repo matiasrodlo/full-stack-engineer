c01876f9db91143635796ff9ecb214d02e411e4c
Matías Rodlo <m@hablaqui.com>
2024-04-04 18:36:39 -0300

    size i

--- full-stack/interview/Nodes/stacks.js
+++ full-stack/interview/Nodes/stacks.js
@@ -36,3 +36,39 @@ pop() {
 }
 
 module.exports = Stack;
+
+// Size I
+
+const LinkedList = require('./LinkedList');
+
+class Stack {
+  constructor(maxSize = Infinity) {
+    this.stack = new LinkedList();
+    this.size = 0;
+    this.maxSize = maxSize;
+  }
+
+  push(value) {
+    this.stack.addToHead(value);
+  }
+
+  pop() {
+    if (this.size > 0) {
+      const value = this.stack.removeHead();
+      this.size --;
+      return value;      
+    } else {
+      console.log('Stack is empty');
+    }
+  }
+
+  peek() {
+    if (this.size > 0) {
+      return this.stack.head.data;      
+    } else {
+      return null;
+    }
+  }
+}
+
+module.exports = Stack;
