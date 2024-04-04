5f2da29d9c4baf644d75ee4b1e5bc5afad79d2d4
Matías Rodlo <m@hablaqui.com>
2024-04-04 14:45:53 -0300

    queue method

--- full-stack/interview/Nodes/queue.js
+++ full-stack/interview/Nodes/queue.js
@@ -11,3 +11,27 @@ module.exports = Queue;
 
 const restaurantOrders = new Queue();
 console.log(`restaurantOrders has ${restaurantOrders.size} nodes`)
+
+// queue method
+
+const LinkedList = require("./LinkedList");
+
+class Queue {
+  constructor() {
+    this.queue = new LinkedList();
+    this.size = 0;
+  }
+  enqueue(data) {
+    this.queue.addToTail(data);
+    this.size++;
+    console.log(`Added ${data}! Queue size is now ${this.size}.`);
+  }
+}
+
+module.exports = Queue;
+
+const restaurantOrder = new Queue();
+console.log(`restaurantOrder queue has ${restaurantOrder.size} orders.\n`);
+restaurantOrder.enqueue("apple pie");
+restaurantOrder.enqueue("roast chicken");
+restaurantOrder.enqueue("quinoa salad");
