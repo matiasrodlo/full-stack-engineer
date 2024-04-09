8173513c254203e86a8f3b3fc7ba57e9b3d71f4a
Matías Rodlo <m@hablaqui.com>
2024-04-09 19:41:11 -0400

    hash second part

--- full-stack/interview/Nodes/hashmap.js
+++ full-stack/interview/Nodes/hashmap.js
@@ -21,3 +21,20 @@ class HashMap {
   
   module.exports = HashMap;
   
+  
+  // Second part
+ 
+  class HashMap {
+  constructor(size = 0) {
+    this.hashmap = new Array(size).fill(null);
+  }
+  hash(key) {
+    let hashCode = 0;
+    for (let i = 0; i < key.length; i++) {
+      hashCode += hashCode + key.charCodeAt(i);
+    }
+    return hashCode;
+  }
+}
+
+module.exports = HashMap;
