8c535c2fd98741b5371ad68c0d57566c87a9770c
Matías Rodlo <m@hablaqui.com>
2024-04-09 20:25:34 -0400

    collisions

--- full-stack/interview/Nodes/hashmap.js
+++ full-stack/interview/Nodes/hashmap.js
@@ -122,3 +122,39 @@ class HashMap {
   glossary.assign('semordnilap', 'Words that form different words when reversed');
   console.log(glossary.retrieve('semordnilap'));
   
+// Collisions
+
+const LinkedList = require('./LinkedList');
+const Node = require('./Node');
+class HashMap {
+  constructor(size = 0) {
+    this.hashmap = new Array(size);
+  }
+
+  hash(key) {
+    let hashCode = 0;
+    for (let i = 0; i < key.length; i++) {
+      hashCode += hashCode + key.charCodeAt(i);
+    }
+    return hashCode % this.hashmap.length;
+  }
+
+  assign(key, value) {
+    const arrayIndex = this.hash(key);
+    this.hashmap[arrayIndex] = value;
+  }
+  
+  retrieve(key) {
+    const arrayIndex = this.hash(key);
+    return this.hashmap[arrayIndex];
+  }
+}
+
+module.exports = HashMap;
+
+const parkInventory = new HashMap(2);
+parkInventory.assign('reed', 'marsh plant');
+parkInventory.assign('deer', 'forest animal');
+
+console.log(parkInventory.retrieve('reed'));
+console.log(parkInventory.retrieve('deer'));
