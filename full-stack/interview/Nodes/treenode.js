09bb43044408980b912626e477479c206e9b49e5
Matías Rodlo <m@hablaqui.com>
2024-04-10 19:12:20 -0400

    tree traversal

--- full-stack/interview/Nodes/treenode.js
+++ full-stack/interview/Nodes/treenode.js
@@ -93,3 +93,70 @@ for (let i = 0; i < 3; i++) {
 // pretty-print the tree
 tree.print();
 
+// Depth-first Tree Traversal
+
+class TreeNode {
+    constructor(data) {
+      this.data = data;
+      this.children = [];
+    }
+  
+    addChild(child) {
+      if (child instanceof TreeNode) {
+        this.children.push(child);
+      } else {
+        this.children.push(new TreeNode(child));
+      }
+    }
+    
+    removeChild(childToRemove) {
+      const length = this.children.length;
+      this.children = this.children.filter(child => {
+        return childToRemove instanceof TreeNode
+        ? child !== childToRemove
+        : child.data !== childToRemove;
+      });
+  
+      if (length === this.children.length) {
+        this.children.forEach(child => child.removeChild(childToRemove));
+      }
+    }
+  
+    print(level = 0) {
+      let result = '';
+      for (let i = 0; i < level; i++) {
+        result += '-- ';
+      }
+      console.log(`${result}${this.data}`);
+      this.children.forEach(child => child.print(level + 1));
+    }
+  
+    depthFirstTraversal() {
+      console.log(this.data);
+      this.children.forEach(child => child.depthFirstTraversal());
+    }
+  };
+  
+  module.exports = TreeNode;
+
+  // script js
+
+  const TreeNode = require('./TreeNode');
+const tree = new TreeNode(15);
+const randomize = () => Math.floor(Math.random() * 20);
+
+// add first-level children
+for (let i = 0; i < 3; i++) {
+  tree.addChild(randomize());
+}
+
+// add second-level children
+for (let i = 0; i < 3; i++) {
+  for (let j = 0; j < 2; j++) {
+    tree.children[i].addChild(randomize());
+  }
+}
+
+// pretty-print the tree
+tree.print();
+tree.depthFirstTraversal();
