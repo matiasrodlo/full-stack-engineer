b1b137e7ec330f2e383a4869837902e9d57a4615
Matías Rodlo <m@hablaqui.com>
2024-04-09 20:00:48 -0400

    compression

--- full-stack/interview/Nodes/hashmap.js
+++ full-stack/interview/Nodes/hashmap.js
@@ -38,3 +38,27 @@ class HashMap {
 }
 
 module.exports = HashMap;
+
+// comnpression
+
+class HashMap {
+    constructor(size = 0) {
+      this.hashmap = new Array(size)
+        .fill(null);
+    }
+  
+    hash(key) {
+      let hashCode = 0;
+      for (let i = 0; i < key.length; i++) {
+        hashCode += hashCode + key.charCodeAt(i);
+      }
+      return hashCode % this.hashmap.length;
+    }
+  }
+  
+  module.exports = HashMap;
+  
+  const myHashMap = new HashMap(3);
+  console.log(myHashMap.hash('id'));
+  console.log(myHashMap.hash('id'));
+  
