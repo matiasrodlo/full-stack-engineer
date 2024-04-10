9d60b873a29e412b477fb3975103dc93fe3ae9f2
Matías Rodlo <m@hablaqui.com>
2024-04-09 20:21:53 -0400

    retrueve

--- full-stack/interview/Nodes/hashmap.js
+++ full-stack/interview/Nodes/hashmap.js
@@ -89,3 +89,36 @@ class HashMap {
   
   module.exports = HashMap;
   
+  // retrieve
+
+  class HashMap {
+    constructor(size = 0) {
+      this.hashmap = new Array(size)
+        .fill(null);
+    }
+  
+    hash(key) {
+      let hashCode = 0;
+      for (let i = 0; i < key.length; i++) {
+        hashCode += hashCode + key.charCodeAt(i);
+      }
+      return hashCode % this.hashmap.length;
+    }
+  
+    assign(key, value) {
+      const arrayIndex = this.hash(key);
+      this.hashmap[arrayIndex] = value;
+    }
+    
+    retrieve(key) {
+      const arrayIndex = this.hash(key);
+      return this.hashmap[arrayIndex];
+    }
+  }
+  
+  module.exports = HashMap;
+  
+  const glossary = new HashMap(3);
+  glossary.assign('semordnilap', 'Words that form different words when reversed');
+  console.log(glossary.retrieve('semordnilap'));
+  
