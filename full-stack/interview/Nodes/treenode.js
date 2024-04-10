d55c76a6b75e62d48357364db857cca085dc349e
Matías Rodlo <m@hablaqui.com>
2024-04-10 19:02:22 -0400

    script.js print

--- full-stack/interview/Nodes/treenode.js
+++ full-stack/interview/Nodes/treenode.js
@@ -62,3 +62,34 @@ class TreeNode {
   };
   
   module.exports = TreeNode;
+
+  // script.js print()
+
+  const TreeNode = require('./TreeNode');
+const tree = new TreeNode(1);
+const randomize = () => Math.floor(Math.random() * 20);
+
+// add first-level children
+for (let i = 0; i < 3; i++) {
+  tree.addChild(randomize());
+}
+
+// add second-level children
+for (let i = 0; i < 3; i++) {
+  for (let j = 0; j < 2; j++) {
+    tree.children[i].addChild(randomize());
+  }
+}
+
+// add third-level children
+for (let i = 0; i < 3; i++) {
+  for (let j = 0; j < 2; j++) {
+    for (let k = 0; k < 2; k++) {
+      tree.children[i].children[j].addChild(randomize());
+    }
+  }
+}
+
+// pretty-print the tree
+tree.print();
+
