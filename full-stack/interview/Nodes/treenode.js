9eab3c1e97fe184e5bf367bce92b44410887a72e
Matías Rodlo <m@hablaqui.com>
2024-04-10 19:00:51 -0400

    removing a child

--- full-stack/interview/Nodes/treenode.js
+++ full-stack/interview/Nodes/treenode.js
@@ -29,3 +29,36 @@ class TreeNode {
   };
   
   module.exports = TreeNode;
+
+// Removing a Child
+
+class TreeNode {
+    constructor(data) {
+      this.data = data;
+      this.children = [];
+    }
+  
+    addChild(child) {
+      if (child instanceof TreeNode) {
+        this.children.push(child);
+      } else {
+        this.children.push(new TreeNode(child));
+      }
+    }
+    removeChild(childToRemove) {
+      const length = this.children.length;
+      this.children = this.children.filter(child => {
+        if (childToRemove instanceof TreeNode) {
+          return childToRemove !== child;
+        } else {
+          return child.data !== childToRemove;
+        }
+      });
+      
+      if (length === this.children.length) {
+        this.children.forEach(child => child.removeChild(childToRemove));
+      }
+    }
+  };
+  
+  module.exports = TreeNode;
