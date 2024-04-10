b2e7c2068fc46498f04b66487bb4b013d2fa0e2b
Matías Rodlo <m@hablaqui.com>
2024-04-09 20:09:09 -0400

    assign

--- full-stack/interview/Nodes/hashmap.js
+++ full-stack/interview/Nodes/hashmap.js
@@ -62,3 +62,30 @@ class HashMap {
   console.log(myHashMap.hash('id'));
   console.log(myHashMap.hash('id'));
   
+  // Assign
+
+  class HashMap {
+    constructor(size = 0) {
+      this.hashmap = new Array(size).fill(null);
+    }
+  
+    hash(key) {
+      let hashCode = 0;
+      for (let i = 0; i < key.length; i++) {
+        hashCode += hashCode + key.charCodeAt(i);
+      }
+      return hashCode % this.hashmap.length;
+    }
+  
+    assign(key, value) {
+      const arrayIndex = this.hash(key);
+      this.hashmap[arrayIndex] = value;
+    }
+  }
+  
+  const employees = new HashMap(3);
+  employees.assign('34-567', 'Mara');
+  console.log(employees.hashmap);
+  
+  module.exports = HashMap;
+  
