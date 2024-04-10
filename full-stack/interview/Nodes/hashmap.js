1a0a0352fcdf6f61d93f14fcbb89b15146afb352
Matías Rodlo <m@hablaqui.com>
2024-04-09 20:37:43 -0400

    collision assigning

--- full-stack/interview/Nodes/hashmap.js
+++ full-stack/interview/Nodes/hashmap.js
@@ -158,3 +158,33 @@ parkInventory.assign('deer', 'forest animal');
 
 console.log(parkInventory.retrieve('reed'));
 console.log(parkInventory.retrieve('deer'));
+
+// collisions assigning
+
+const LinkedList = require('./LinkedList');
+const Node = require('./Node');
+class HashMap {
+  constructor(size = 0) {
+    this.hashmap = new Array(size)
+      .fill(null)
+      .map(() => new LinkedList());
+  }
+
+  hash(key) {
+    let hashCode = 0;
+    for (let i = 0; i < key.length; i++) {
+      hashCode += hashCode + key.charCodeAt(i);
+    }
+    return hashCode % this.hashmap.length;
+  }
+
+  assign(key, value) {
+    const arrayIndex = this.hash(key);
+    const linkedList = this.hashmap[arrayIndex];
+    if (linkedList.head === null) {
+      linkedList.addToHead({ key, value });
+      return;
+    }
+  }
+
+module.exports = HashMap;
