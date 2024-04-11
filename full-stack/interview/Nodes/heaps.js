449b50216c97b333e97397dbee1dccd720824e73
Matías Rodlo <m@hablaqui.com>
2024-04-10 21:24:11 -0400

    parent and child elements

--- full-stack/interview/Nodes/heaps.js
+++ full-stack/interview/Nodes/heaps.js
@@ -68,3 +68,57 @@ minHeap.add(42)
 
 // display content of minHeap
 console.log('Content of heap', minHeap.heap);
+
+// Parent and Child Elements
+
+const { MinHeap, getParent, getLeft, getRight } = require('./MinHeap');
+
+// instantiate MinHeap and assign to minHeap
+const minHeap = new MinHeap();
+
+// sample content of minHeap
+minHeap.heap = [ null, 10, 13, 21, 61, 22, 23, 99 ];
+
+// display content of minHeap
+console.log(minHeap.heap);
+
+// display the current value, its parent value, left child value and right child value
+// replace null with the correct way to access the values of the parent, left child and right child
+const current = 3;
+const currentValue = minHeap.heap[current];
+console.log(`Current value of ${current} is ${currentValue}`);
+console.log(`Parent value of ${currentValue} is ${minHeap.heap[getParent(current)]}`);
+console.log(`Left child value of ${currentValue} is ${minHeap.heap[getLeft(current)]}`);
+console.log(`Right child value of ${currentValue} is ${minHeap.heap[getRight(current)]}`);
+
+// MinHeap.js
+
+class MinHeap {
+    constructor() {
+      this.heap = [ null ];
+      this.size = 0;
+    }
+  
+    add(value) {
+      this.heap.push(value);
+      this.size++;
+      this.bubbleUp();
+      console.log(this.heap);
+    }
+  
+    bubbleUp() {
+      console.log('Bubble Up');
+    }
+  }
+  
+  const getParent = current => Math.floor((current / 2));
+  const getLeft = current => current * 2;
+  const getRight = current => current * 2 + 1;
+  
+  module.exports = { 
+    MinHeap,
+    getParent,
+    getLeft,
+    getRight
+  };
+  
