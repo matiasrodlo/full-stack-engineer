f69b511d3b3b145fd3c8ce2b20e3deb617043ed8
Matías Rodlo <m@hablaqui.com>
2024-04-12 20:38:53 -0400

    adding vertices

--- full-stack/interview/Nodes/graphs.js
+++ full-stack/interview/Nodes/graphs.js
@@ -48,3 +48,33 @@ class Edge {
   }
   
   module.exports = Edge;
+
+  // Adding Vertices in graph.js
+
+  const Edge = require('./Edge.js');
+  const Vertex = require('./Vertex.js');
+  
+  class Graph {
+    constructor() {
+      this.vertices = [];
+    }
+  
+    print() {
+      this.vertices.forEach(vertex => vertex.print());
+    }
+  
+    addVertex(data) {
+      const newVertex = new Vertex(data);
+      this.vertices.push(newVertex)
+  
+      return newVertex;
+    }
+  }
+  
+    const trainNetwork = new Graph();
+    const atlantaStation = trainNetwork.addVertex('Atlanta');
+    const newYorkStation = trainNetwork.addVertex('New York');
+    
+    trainNetwork.print();
+  
+  module.exports = Graph;
