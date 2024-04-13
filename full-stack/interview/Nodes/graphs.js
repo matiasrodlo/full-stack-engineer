b17ed89d4643b5335d2d8bd394514cb7092555dc
Matías Rodlo <m@hablaqui.com>
2024-04-12 21:29:37 -0400

    directed graphs

--- full-stack/interview/Nodes/graphs.js
+++ full-stack/interview/Nodes/graphs.js
@@ -232,3 +232,69 @@ trainNetwork.addEdge(atlantaStation, newYorkStation, 800);
 trainNetwork.print();
 
 module.exports = Graph;
+
+// Directed Graphs
+
+const Edge = require('./Edge.js');
+const Vertex = require('./Vertex.js');
+
+class Graph {
+  constructor(isWeighted = false, isDirected = false) {
+    this.vertices = [];
+    this.isWeighted = isWeighted;
+    this.isDirected = isDirected;
+  }
+
+  addVertex(data) {
+    const newVertex = new Vertex(data);
+    this.vertices.push(newVertex);
+
+    return newVertex;
+  }
+
+  removeVertex(vertex) {
+    this.vertices = this.vertices.filter(v => v !== vertex);
+  }
+
+  addEdge(vertexOne, vertexTwo, weight) {
+    const edgeWeight = this.isWeighted ? weight : null;
+
+    if (vertexOne instanceof Vertex && vertexTwo instanceof Vertex) {
+      vertexOne.addEdge(vertexTwo, edgeWeight);
+
+      if (!this.isDirected) {
+        vertexTwo.addEdge(vertexOne, edgeWeight);
+      }
+    } else {
+      throw new Error('Expected Vertex arguments.');
+    }
+  }
+
+  removeEdge(vertexOne, vertexTwo) {
+    if (vertexOne instanceof Vertex && vertexTwo instanceof Vertex) {
+      vertexOne.removeEdge(vertexTwo);
+
+      if (!this.isDirected) {
+        vertexTwo.removeEdge(vertexOne);
+      }
+    } else {
+      throw new Error('Expected Vertex arguments.');
+    }
+  }
+
+  print() {
+    const vertexList = this.vertices || [];
+    vertexList.forEach(vertex => vertex.print());
+  }
+}
+
+const trainNetwork = new Graph(false, true);
+const atlantaStation = trainNetwork.addVertex('Atlanta');
+const newYorkStation = trainNetwork.addVertex('New York');
+trainNetwork.addEdge(atlantaStation, newYorkStation);
+
+trainNetwork.removeEdge(atlantaStation, newYorkStation);
+
+trainNetwork.print();
+
+module.exports = Graph;
