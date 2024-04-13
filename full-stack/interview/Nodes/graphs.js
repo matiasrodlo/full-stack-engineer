ea25e1a3ded6932818cfc87c22485e1ba696904d
Matías Rodlo <m@hablaqui.com>
2024-04-12 21:08:34 -0400

    removing vertex connections

--- full-stack/interview/Nodes/graphs.js
+++ full-stack/interview/Nodes/graphs.js
@@ -143,3 +143,36 @@ class Vertex {
 }
 
 module.exports = Vertex;
+
+// removing vertex connections
+
+const Edge = require('./Edge.js');
+
+class Vertex {
+  constructor(data) {
+    this.data = data;
+    this.edges = [];
+  }
+
+  addEdge(vertex) {
+    if (vertex instanceof Vertex) {
+      this.edges.push(new Edge(this, vertex));
+    } else {
+      throw new Error('Edge start and end must both be Vertex');
+    }
+  }
+
+  removeEdge(vertex) {
+    this.edges = this.edges.filter(edge => edge.end !== vertex);
+  }
+
+  print() {
+    const edgeList = this.edges.map(edge =>
+        edge.weight !== null ? `${edge.end.data} (${edge.weight})` : edge.end.data);
+
+    const output = `${this.data} --> ${edgeList.join(', ')}`;
+    console.log(output);
+  }
+}
+
+module.exports = Vertex;
