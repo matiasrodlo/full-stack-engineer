2a0cade541a0b6e81b6135e9db7e1fe210bfac9b
Matías Rodlo <m@hablaqui.com>
2024-04-12 20:50:23 -0400

    removingvertices

--- full-stack/interview/Nodes/graphs.js
+++ full-stack/interview/Nodes/graphs.js
@@ -78,3 +78,39 @@ class Edge {
     trainNetwork.print();
   
   module.exports = Graph;
+
+  // removing vertices in graph
+
+  const Edge = require('./Edge.js');
+const Vertex = require('./Vertex.js');
+
+class Graph {
+  constructor() {
+    this.vertices = [];
+  }
+
+  addVertex(data) {
+    const newVertex = new Vertex(data);
+    this.vertices.push(newVertex);
+
+    return newVertex;
+  }
+  
+  removeVertex(vertex) {
+    this.vertices = this.vertices.filter(v => v !== vertex);
+  }
+
+  print() {
+    this.vertices.forEach(vertex => vertex.print());
+  }
+}
+
+const trainNetwork = new Graph();
+const atlantaStation = trainNetwork.addVertex('Atlanta');
+const newYorkStation = trainNetwork.addVertex('New York');
+
+trainNetwork.removeVertex(atlantaStation);
+
+trainNetwork.print();
+
+module.exports = Graph;
