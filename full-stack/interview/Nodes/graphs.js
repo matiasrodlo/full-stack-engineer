0f96ba069b5fb84fa4a9706d327c83a462482e23
Matías Rodlo <m@hablaqui.com>
2024-04-12 21:23:34 -0400

    directed graphs

--- full-stack/interview/Nodes/graphs.js
+++ full-stack/interview/Nodes/graphs.js
@@ -176,3 +176,59 @@ class Vertex {
 }
 
 module.exports = Vertex;
+
+// Weighted graphs
+
+const Edge = require('./Edge.js');
+const Vertex = require('./Vertex.js');
+
+class Graph {
+  constructor(isWeighted = false) {
+    this.vertices = [];
+    this.isWeighted = isWeighted;
+  }
+
+  addVertex(data) {
+    const newVertex = new Vertex(data);
+    this.vertices.push(newVertex);
+
+    return newVertex;
+  }
+
+  removeVertex(vertex) {
+    this.vertices = this.vertices.filter(v => v !== vertex);
+  }
+
+  addEdge(vertexOne, vertexTwo, weight) {
+    const edgeWeight = this.isWeighted ? weight : null;
+
+    if (vertexOne instanceof Vertex && vertexTwo instanceof Vertex) {
+      vertexOne.addEdge(vertexTwo, edgeWeight);
+      vertexTwo.addEdge(vertexOne, edgeWeight);
+    } else {
+      throw new Error('Expected Vertex arguments.');
+    }
+  }
+
+  removeEdge(vertexOne, vertexTwo) {
+    if (vertexOne instanceof Vertex && vertexTwo instanceof Vertex) {
+      vertexOne.removeEdge(vertexTwo);
+      vertexTwo.removeEdge(vertexOne);
+    } else {
+      throw new Error('Expected Vertex arguments.');
+    }
+  }
+
+  print() {
+    this.vertices.forEach(vertex => vertex.print());
+  }
+}
+
+const trainNetwork = new Graph(true);
+const atlantaStation = trainNetwork.addVertex('Atlanta');
+const newYorkStation = trainNetwork.addVertex('New York');
+trainNetwork.addEdge(atlantaStation, newYorkStation, 800);
+
+trainNetwork.print();
+
+module.exports = Graph;
