38d93c29afa404adbdf4da1aab07df4903640b91
Matías Rodlo <m@hablaqui.com>
2024-04-15 15:18:27 -0400

    base case

--- full-stack/interview/algorithms/iterative-traversal.js
+++ full-stack/interview/algorithms/iterative-traversal.js
@@ -11,3 +11,82 @@ myList.addToHead('Node 4');
 // Add code below
 const foundNode = myList.findNodeIteratively('Node 3');
 console.log(foundNode);
+
+// base case
+
+const Node = require('./Node');
+
+class LinkedList {
+  constructor() {
+    this.head = null;
+  }
+
+  addToHead(data) {
+    const nextNode = new Node(data);
+    const currentHead = this.head;
+    this.head = nextNode;
+    if (currentHead) {
+      this.head.setNextNode(currentHead);
+    }
+  }
+
+  addToTail(data) {
+    let lastNode = this.head;
+    if (!lastNode) {
+      this.head = new Node(data);
+    } else {
+      let temp = this.head;
+      while (temp.getNextNode() !== null) {
+        temp = temp.getNextNode();
+      }
+      temp.setNextNode(new Node(data));
+    }
+  }
+
+  removeHead() {
+    const removedHead = this.head;
+    if (!removedHead) {
+      return;
+    }
+    if (removedHead.next) {
+      this.head = removedHead.next;
+    }
+    return removedHead.data;
+  }
+
+  printList() {
+    let currentNode = this.head;
+    let output = '<head> ';
+    while (currentNode !== null) {
+      output += currentNode.data + ' ';
+      currentNode = currentNode.next;
+    }
+    output = output.concat("<tail>");
+    console.log(output);
+  }
+
+  findNodeIteratively(data) {
+    let currentNode = this.head;
+    while (currentNode !== null) {
+      if (currentNode.data === data) {
+        return currentNode;
+      }
+      currentNode = currentNode.next;
+    }
+    return null;
+  }
+
+  findNodeRecursively(data, currentNode = this.head) {
+    // Add base cases below
+    if (currentNode === null) {
+      return null;
+    } else if (currentNode.data === data) {
+      return currentNode;
+    } 
+    
+    
+  }
+
+}
+
+module.exports = LinkedList;
