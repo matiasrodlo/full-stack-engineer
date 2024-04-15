a83f3f62ae4bf70e128d6971de9611d4e8573020
Matías Rodlo <m@hablaqui.com>
2024-04-15 14:22:56 -0400

    base case

--- full-stack/interview/algorithms/recursion.js
+++ full-stack/interview/algorithms/recursion.js
@@ -35,3 +35,24 @@ const iterativeFactorial = (n) => {
     recursiveFactorial,
   };
   
+  // Base Case
+
+  const recursiveFactorial = (n) => {
+    // Add a condition below
+    if (n == 0) {
+      return 1
+    }
+    
+    if (n > 0){
+      console.log(`Execution context: ${n}`);
+      
+      return recursiveFactorial(n - 1) * n;
+    }
+  }
+  
+  const recursiveSolution = recursiveFactorial(5);
+  console.log(recursiveSolution);
+  
+  module.exports = {
+    recursiveFactorial
+  };
