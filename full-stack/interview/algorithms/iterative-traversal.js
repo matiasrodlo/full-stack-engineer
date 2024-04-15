da27f2e95b77ddba61ea0c491161ecdeebc52d73
Matías Rodlo <m@hablaqui.com>
2024-04-15 15:04:15 -0400

    iteratucve traversal

---
+++ full-stack/interview/algorithms/iterative-traversal.js
@@ -0,0 +1,13 @@
+const Node = require('./Node');
+const LinkedList = require('./LinkedList');
+
+const myList = new LinkedList();
+
+myList.addToHead('Node 1');
+myList.addToHead('Node 2');
+myList.addToHead('Node 3');
+myList.addToHead('Node 4');
+
+// Add code below
+const foundNode = myList.findNodeIteratively('Node 3');
+console.log(foundNode);
