f4ad12ffe19f4a845330ddbffe4cdbec2fcf947d
Matías Rodlo <m@hablaqui.com>
2024-04-15 20:39:28 -0400

    bubble sort compare

--- full-stack/interview/algorithms/bubble-sort.js
+++ full-stack/interview/algorithms/bubble-sort.js
@@ -24,3 +24,26 @@ const swap = (arr, indexOne, indexTwo) => {
   
   module.exports = swap;
   
+//Compare
+
+const swap = require('./swap');
+
+const bubbleSort = input => {
+  let swapping = true;
+  
+  while (swapping) {
+    swapping = false;
+    for (let i = 0; i < input.length - 1; i++) {
+      if (input[i] > input[i + 1]) {
+        console.log(`Swapping pair ${input[i]}, ${input[i+1]} in [${input}]`);
+        swap(input, i, i + 1);
+        swapping = true;
+      }
+    }
+  }
+  return input;
+};
+
+module.exports = bubbleSort;
+
+console.log(bubbleSort([3, 2, 1]));
