3ce90dafb8dfcb28e1ab2c598c7b1efe5bdc3de6
Matías Rodlo <m@hablaqui.com>
2024-04-16 14:35:40 -0400

    checking the middle index

--- full-stack/interview/algorithms/binary-search.js
+++ full-stack/interview/algorithms/binary-search.js
@@ -1,12 +1,15 @@
 const binarySearch = (arr, target) => {
-    // Add left and right variables below
     let left = 0;
     let right = arr.length;
     
-    // Add indexToCheck calculation below
     const indexToCheck = Math.floor((left + right) / 2);
-    
-    return indexToCheck;
+    // 1. Create a constant called checking
+    const checking = arr[indexToCheck]
+    // 2. Create a conditional below
+  if (checking == target) {
+    return indexToCheck
+  }
+    return null;
   }
   
   const searchable = [1, 2, 7, 8, 22, 28, 41, 58, 67, 71, 94];
