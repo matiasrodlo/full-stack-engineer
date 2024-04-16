e16b003b1c136e6242e404540b758592c0cd8958
Matías Rodlo <m@hablaqui.com>
2024-04-16 19:17:43 -0400

    BFT all layers

--- full-stack/interview/algorithms/graph-traversal.js
+++ full-stack/interview/algorithms/graph-traversal.js
@@ -68,3 +68,28 @@ const breadthFirstTraversal = (start) => {
 };
 
 breadthFirstTraversal(testGraph.vertices[0]);
+
+// Breadth-First Traversal (All layers)
+
+const testGraph = require('./testGraph.js');
+const Queue = require('./Queue.js');
+
+const breadthFirstTraversal = (start) => {
+  const visitedVertices = [start];
+  const visitQueue = new Queue();
+  visitQueue.enqueue(start);
+  while (!visitQueue.isEmpty()) {
+    const current = visitQueue.dequeue();
+    console.log(current.data);
+    current.edges.forEach((edge) => {
+      const neighbor = edge.end;
+      if (!visitedVertices.includes(neighbor)) {
+        visitedVertices.push(neighbor);
+        visitQueue.enqueue(neighbor);
+      }
+    })
+  }
+  
+};
+
+breadthFirstTraversal(testGraph.vertices[0]);
