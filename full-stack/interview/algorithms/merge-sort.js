40c55b95f135ca85bfbcac95672cb41b9c4dfa18
Matías Rodlo <m@hablaqui.com>
2024-04-15 21:58:10 -0400

    review

--- full-stack/interview/algorithms/merge-sort.js
+++ full-stack/interview/algorithms/merge-sort.js
@@ -65,3 +65,34 @@ const mergeSort = (startArray) => {
   module.exports = {
     mergeSort
   };
+
+// Merging
+
+  const mergeSort = (startArray) => {
+    const length = startArray.length;
+    if (length === 1) {
+      return startArray;
+    }
+    
+    const mid = Math.floor(length / 2);
+    const leftArray = startArray.slice(0, mid);
+    const rightArray = startArray.slice(mid, length);
+  
+    return merge(mergeSort(leftArray), mergeSort(rightArray))
+  }
+  
+  const merge = (leftArray, rightArray) => {
+    const sortedArray = [];
+    while(leftArray.length > 0 && rightArray.length > 0) { if(leftArray[0] < rightArray[0])
+  
+    }
+  )
+  }
+  
+  const inputArr = [3, 5, 2, 90, 4, 7];
+  
+  console.log(mergeSort(inputArr));
+  
+  module.exports = {
+    mergeSort
+  };
