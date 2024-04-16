0d36b4b2f3e3b004817876a06b4aa8052b633587
Matías Rodlo <m@hablaqui.com>
2024-04-16 19:06:08 -0400

    DF traversal

--- full-stack/interview/algorithms/graph-traversal.js
+++ full-stack/interview/algorithms/graph-traversal.js
@@ -13,3 +13,22 @@ const depthFirstTraversal = (start, visitedVertices = [start]) => {
 };
 
 depthFirstTraversal(testGraph.vertices[0]);
+
+// Depth-First Traversal (All paths)
+
+const testGraph = require('./testGraph.js');
+
+const depthFirstTraversal = (start, visitedVertices = [start]) => {
+  console.log(start.data);
+
+  start.edges.forEach(edge => {
+    const neighbor = edge.end;
+
+    if (!visitedVertices.includes(neighbor)) {
+      visitedVertices.push(neighbor);
+      depthFirstTraversal(neighbor, visitedVertices);
+    }
+  });
+};
+
+depthFirstTraversal(testGraph.vertices[0]);
