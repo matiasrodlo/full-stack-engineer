7489aca6fd2f5f7e36977d7136a2633af2aa593d
Matías Rodlo <m@hablaqui.com>
2024-04-16 19:09:45 -0400

    DFT Callbacks

--- full-stack/interview/algorithms/graph-traversal.js
+++ full-stack/interview/algorithms/graph-traversal.js
@@ -32,3 +32,22 @@ const depthFirstTraversal = (start, visitedVertices = [start]) => {
 };
 
 depthFirstTraversal(testGraph.vertices[0]);
+
+// Depth-First Traversal (Callbacks)
+
+const testGraph = require('./testGraph.js');
+
+const depthFirstTraversal = (start, callback, visitedVertices = [start]) => {
+  callback(start);
+
+  start.edges.forEach((edge) => {
+    const neighbor = edge.end;
+
+    if (!visitedVertices.includes(neighbor)) {
+      visitedVertices.push(neighbor);
+      depthFirstTraversal(neighbor, callback, visitedVertices);
+    }
+  });
+};
+
+depthFirstTraversal(testGraph.vertices[0], (vertex) => { console.log(vertex.data) });
