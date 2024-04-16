4e5c3040318941b20232d18169310bf28b8b35aa
Matías Rodlo <m@hablaqui.com>
2024-04-16 14:59:38 -0400

    iterative checking

--- full-stack/interview/algorithms/binary-search.js
+++ full-stack/interview/algorithms/binary-search.js
@@ -18,3 +18,34 @@ const binarySearch = (arr, target) => {
   console.log(binarySearch(searchable, target));
   
   module.exports = {binarySearch};
+
+  // Iterative Checking
+
+  const binarySearch = (arr, target) => {
+    let left = 0;
+    let right = arr.length;
+    
+    while (right > left) {
+      const indexToCheck = Math.floor((left + right) / 2);
+      const checking = arr[indexToCheck];
+      console.log(`indexToCheck equals: ${indexToCheck}`)
+    
+      if (checking === target) {
+        return indexToCheck;
+      } else if (target > checking) {
+        left = indexToCheck + 1;
+      } else {
+        right = indexToCheck;
+      }
+    }
+    return null;
+  }
+  
+  const searchable = [1, 2, 7, 8, 22, 28, 41, 58, 67, 71, 94];
+  const target = 2;
+  
+  const targetIndex = binarySearch(searchable, target);
+  
+  console.log(`The target index is ${targetIndex}.`);
+  
+  module.exports = {binarySearch};
