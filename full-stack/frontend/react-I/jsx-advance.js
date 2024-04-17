087b5cf93bcaa84443643097010693ac62c76522
Matías Rodlo <m@hablaqui.com>
2024-04-17 13:44:50 -0400

    self closing tags

--- full-stack/frontend/react-I/jsx-advance.js
+++ full-stack/frontend/react-I/jsx-advance.js
@@ -10,3 +10,19 @@ I AM A BIG DIV
 </div>
 
 root.render(myDiv)
+
+// Self-Closing Tags
+
+const profile = (
+    <div>
+      <h1>John Smith</h1>
+      <img src="images/john.png"/>
+      <article>
+        My name is John Smith.
+        <br/>
+        I am a software developer.
+        <br/>
+        I specialize in creating React applications.
+      </article>
+    </div>
+  );
