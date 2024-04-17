4ab35fce3eb72a83f31da77cddfa11135a9894f7
Matías Rodlo <m@hablaqui.com>
2024-04-17 00:52:51 -0400

    destructuring obj

--- full-stack/frontend/react-I/destructure.js
+++ full-stack/frontend/react-I/destructure.js
@@ -1,6 +1,15 @@
+// Destructuring Arrays
+
 let colors = ['blue','red','purple'];
 
-// Destructure here
 let [color1, color2, color3] = colors
 
 console.log(color1, color2, color3);
+
+//Destructuring Objects
+
+let planets = { x: 'Saturn', y: 'Mars', z: 'Neptune' };
+
+let {x, y, z} = planets
+
+console.log(x, y, z);
