6a0df23eed8f75b95c907f95c9bfdb63fd3b4615
Matías Rodlo <m@hablaqui.com>
2024-04-16 21:35:54 -0400

    shortest path to a target vertex

--- full-stack/interview/algorithms/dijkstra.js
+++ full-stack/interview/algorithms/dijkstra.js
@@ -95,3 +95,31 @@ const results = dijkstras(testGraph, testGraph.vertices[0]);
 console.log(results);
 
 module.exports = dijkstras;
+
+// Shortest Path to a Target Vertex
+
+const testGraph = require('./testGraph.js');
+const dijkstras = require('./dijkstras.js');
+
+const shortestPathBetween = (graph, startingVertex, targetVertex) => {
+	const { distances, previous } = dijkstras(graph, startingVertex);
+	
+	const distance = distances[targetVertex.data];
+	const path = [];
+
+	let vertex = targetVertex;
+	while(vertex) {
+		path.unshift(vertex);
+		vertex = previous[vertex.data];
+	}
+
+	return { distance, path };
+};
+
+// Retrieve shortest path between vertices A and G
+const a = testGraph.getVertexByValue('A');
+const g = testGraph.getVertexByValue('G');
+const results = shortestPathBetween(testGraph, a, g);
+console.log(results);
+
+module.exports = shortestPathBetween;
