5b1d0d6a9c2112b338701c8df8930e69fd468dc6
Matías Rodlo <m@hablaqui.com>
2024-04-16 20:45:37 -0400

    vertex neighbors

--- full-stack/interview/algorithms/dijkstra.js
+++ full-stack/interview/algorithms/dijkstra.js
@@ -18,3 +18,37 @@ const results = dijkstras(testGraph, testGraph.vertices[0]);
 console.log(results);
 
 module.exports = dijkstras;
+
+// Evaluate Paths to Starting Vertex’s Neighbors
+
+const testGraph = require('./testGraph.js');
+
+const dijkstras = (graph, startingVertex) => {
+	const distances = {};
+	const previous = {};
+
+	graph.vertices.forEach((vertex) => {
+		distances[vertex.data] = Infinity;
+		previous[vertex.data] = null;
+	});
+
+	distances[startingVertex.data] = 0;
+
+  const vertex = startingVertex;
+
+	vertex.edges.forEach((edge) => {
+		const alternate = edge.weight + distances[vertex.data];
+		const neighborValue = edge.end.data;
+
+		if (alternate < distances[neighborValue]) {
+			distances[neighborValue] = alternate;
+			previous[neighborValue] = vertex;
+		}
+	})
+	return { distances, previous };
+};
+
+const results = dijkstras(testGraph, testGraph.vertices[0]);
+console.log(results);
+
+module.exports = dijkstras;
