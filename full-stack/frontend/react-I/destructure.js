756f930ac7f30215cc8ea4779560f526ac4771c9
Matías Rodlo <m@hablaqui.com>
2024-04-17 01:28:29 -0400

    destructuring func parameters

--- full-stack/frontend/react-I/destructure.js
+++ full-stack/frontend/react-I/destructure.js
@@ -13,3 +13,19 @@ let planets = { x: 'Saturn', y: 'Mars', z: 'Neptune' };
 let {x, y, z} = planets
 
 console.log(x, y, z);
+
+// Destructuring Function Parameters
+
+let prairieRose = {
+    name: 'Prairie Rose',
+    scientificName: 'Rosa arkansana',
+    kingdom:	'Plantae',
+    genus:	'Rosa',
+    use: 'ornamental'
+  };
+  
+  const printPlantInfo = ({name, scientificName, kingdom}) => {
+    console.log(`The ${name}, or ${scientificName}, is in the kingdom ${kingdom}.`)
+  };
+  
+  printPlantInfo(prairieRose);
