b871bd5b0a6ee404911fbde73092c88b57643584
Matías Rodlo <m@hablaqui.com>
2024-04-17 13:04:39 -0400

    jsx outer elements

--- full-stack/frontend/react-I/intro-jsx.jsx
+++ full-stack/frontend/react-I/intro-jsx.jsx
@@ -25,3 +25,16 @@ const myDiv = (
     </div>
   );
   
+// JSX Outer Elements
+
+const blog = (
+    <div>
+      <img src="pics/192940u73.jpg" />
+      <h1>Welcome to Dan's Blog!</h1>
+      <article>
+        Wow I had the tastiest sandwich today. I <strong>literally</strong> almost
+        freaked out.
+      </article>
+    </div>
+  );
+  
