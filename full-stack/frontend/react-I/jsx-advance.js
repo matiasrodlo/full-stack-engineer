0c9e7be6efc34fd8a7b56231452d6c539da53ebd
Matías Rodlo <m@hablaqui.com>
2024-04-17 14:21:28 -0400

    variable attributes ion jsx

--- full-stack/frontend/react-I/jsx-advance.js
+++ full-stack/frontend/react-I/jsx-advance.js
@@ -70,3 +70,16 @@ const root = createRoot(container);
 const theBestString = 'This text was accessed through a javascript variable';
 
 root.render(<h1>{theBestString}</h1>);
+
+//Variable Attributes in JSX
+
+import React from 'react';
+import { createRoot } from 'react-dom/client';
+
+const container = document.getElementById('app');
+const root = createRoot(container);
+const goose = 'https://content.codecademy.com/courses/React/react_photo-goose.jpg';
+
+// Declare new variable here:
+const gooseImg = <img src={goose}/>
+root.render(gooseImg)
