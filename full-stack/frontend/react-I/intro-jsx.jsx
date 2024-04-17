18e843684460a79fde7386a2dde6cb22655d3a7c
Matías Rodlo <m@hablaqui.com>
2024-04-17 13:23:32 -0400

    render explained

--- full-stack/frontend/react-I/intro-jsx.jsx
+++ full-stack/frontend/react-I/intro-jsx.jsx
@@ -43,3 +43,12 @@ const blog = (
 const container = document.getElementById('app');
 const root = createRoot(container);
 root.render(<h1>Hello world</h1>);
+
+// Render Explained
+
+import React from 'react';
+import { createRoot } from 'react-dom/client';
+
+const container = document.getElementById('container');
+const root = createRoot(container);
+root.render(<h1>Hello world</h1>);
