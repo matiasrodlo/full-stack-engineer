8d2de6473a4ba5a41d20b38cde239d2b5ec1a9e3
Matías Rodlo <m@hablaqui.com>
2024-04-17 14:10:11 -0400

    pi ion jsx

--- full-stack/frontend/react-I/jsx-advance.js
+++ full-stack/frontend/react-I/jsx-advance.js
@@ -47,3 +47,14 @@ const root = createRoot(container);
 // Write code here:
 root.render(<h1>{2 + 3}</h1>
 );
+
+// 20 digits of PI
+
+import React from "react";
+import { createRoot } from "react-dom/client";
+
+const container = document.getElementById("app");
+const root = createRoot(container);
+// Write code here:
+const math = (<h1> 2 + 3 = {2 + 3} </h1>);
+root.render(math)
