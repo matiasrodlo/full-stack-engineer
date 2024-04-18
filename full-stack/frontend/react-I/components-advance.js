4bffdcce543afbff8cf520655b14aeade35957f0
Matías Rodlo <m@hablaqui.com>
2024-04-18 16:49:18 -0400

    putting logic in a fucntion ckmponent

--- full-stack/frontend/react-I/components-advance.js
+++ full-stack/frontend/react-I/components-advance.js
@@ -39,3 +39,32 @@ function Owl() {
 }
 
 export default Owl;
+
+// puting logic
+
+import React from "react";
+
+const friends = [
+  {
+    title: "Yummmmmmm",
+    src: "https://content.codecademy.com/courses/React/react_photo-monkeyweirdo.jpg",
+  },
+  {
+    title: "Hey Guys! Wait Up!",
+    src: "https://content.codecademy.com/courses/React/react_photo-earnestfrog.jpg",
+  },
+  {
+    title: "Yikes",
+    src: "https://content.codecademy.com/courses/React/react_photo-alpaca.jpg",
+  },
+];
+
+// New function component starts here:
+function Friend() {
+  let friend = friends[0];
+  return <div> <h1> {friend.title}</h1>
+  <img src={friend.src} />
+   </div>;
+}
+
+export default Friend;
