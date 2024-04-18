bed630ef4cae1fa47304ff4f46b066d3b7946e3d
Matías Rodlo <m@hablaqui.com>
2024-04-17 21:06:08 -0400

    conditionals &&

--- full-stack/frontend/react-I/jsx-advance.js
+++ full-stack/frontend/react-I/jsx-advance.js
@@ -156,3 +156,27 @@ const pics = {
 const img = <img src={pics[ coinToss() === 'heads' ? 'kitty' : 'doggy']} />;
 
 root.render(img);
+
+// JSX Conditionals: &&
+
+import React from "react";
+import { createRoot } from "react-dom/client";
+
+const container = document.getElementById("app");
+const root = createRoot(container);
+// judgmental will be true half the time.
+const judgmental = Math.random() < 0.5;
+
+const favoriteFoods = (
+  <div>
+    <h1>My Favorite Foods</h1>
+    <ul>
+      <li>Sushi Burrito</li>
+      <li>Rhubarb Pie</li>
+      {!judgmental && <li>Nacho Cheez Straight Out The Jar</li>}
+      <li>Broiled Grapefruit</li>
+    </ul>
+  </div>
+);
+
+root.render(favoriteFoods);
