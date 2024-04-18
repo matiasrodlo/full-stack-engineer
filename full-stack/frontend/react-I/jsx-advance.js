e5b89ba72e179f08b0f361ad0551e55d0e5b01f1
Matías Rodlo <m@hablaqui.com>
2024-04-17 21:18:40 -0400

    map in jsx

--- full-stack/frontend/react-I/jsx-advance.js
+++ full-stack/frontend/react-I/jsx-advance.js
@@ -180,3 +180,20 @@ const favoriteFoods = (
 );
 
 root.render(favoriteFoods);
+
+// .map in JSX
+
+import React from 'react';
+import { createRoot } from 'react-dom/client';
+
+const container = document.getElementById('app');
+const root = createRoot(container);
+const people = ['Rowe', 'Prevost', 'Gare'];
+
+const peopleList = people.map((person,i) =>
+<li>{person}</li>
+  
+);
+
+// root.render goes here:
+root.render(<ul>{peopleList}</ul>)
