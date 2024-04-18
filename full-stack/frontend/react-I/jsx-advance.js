838d05f8c8d77d1a729c4d110bf8d35a1e9ba7dd
Matías Rodlo <m@hablaqui.com>
2024-04-17 21:30:22 -0400

    keys

--- full-stack/frontend/react-I/jsx-advance.js
+++ full-stack/frontend/react-I/jsx-advance.js
@@ -197,3 +197,20 @@ const peopleList = people.map((person,i) =>
 
 // root.render goes here:
 root.render(<ul>{peopleList}</ul>)
+
+//keys
+
+import React from 'react';
+import { createRoot } from 'react-dom/client';
+
+const container = document.getElementById('app');
+const root = createRoot(container);
+const people = ['Rowe', 'Prevost', 'Gare'];
+
+const peopleList = people.map((person,i) =>
+  // expression goes here:
+  <li key={'person_'+ i}>{person}</li>
+);
+
+// root.render goes here:
+root.render(<ul>{peopleList}</ul>);
