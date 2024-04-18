50ad5e8e0a6faeacdc033e971b70b0d3140da7b7
Matías Rodlo <m@hablaqui.com>
2024-04-17 20:44:47 -0400

    event listeners in sjx

--- full-stack/frontend/react-I/jsx-advance.js
+++ full-stack/frontend/react-I/jsx-advance.js
@@ -83,3 +83,25 @@ const goose = 'https://content.codecademy.com/courses/React/react_photo-goose.jp
 // Declare new variable here:
 const gooseImg = <img src={goose}/>
 root.render(gooseImg)
+
+// Event Listeners in JSX
+
+import React from 'react';
+import { createRoot } from 'react-dom/client';
+
+const container = document.getElementById('app');
+const root = createRoot(container);
+function makeDoggy(e) {
+  // Call this extremely useful function on an <img>.
+  // The <img> will become a picture of a doggy.
+  e.target.setAttribute('src', 'https://content.codecademy.com/courses/React/react_photo-puppy.jpeg');
+  e.target.setAttribute('alt', 'doggy');
+}
+
+const kitty = (
+	<img 
+		src="https://content.codecademy.com/courses/React/react_photo-kitty.jpg" 
+		alt="kitty" onClick={makeDoggy} />
+);
+
+root.render(kitty);
