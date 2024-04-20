44969b41ee38f1caa0ae5ca44909adef65d7343a
Matías Rodlo <m@hablaqui.com>
2024-04-20 13:05:07 -0400

    render a component

--- full-stack/frontend/react-II/components-renders.js
+++ full-stack/frontend/react-II/components-renders.js
@@ -71,3 +71,19 @@ function App() {
 }
 
 export default App;
+
+// render the componnets props
+
+import React from 'react';
+
+function Product(props) {
+  return (
+    <div>
+      <h1>{props.name}</h1>
+      <h2>{props.price}</h2>
+      <h3>{props.rating}</h3>
+    </div>       
+  );
+}
+
+export default Product;
