f1afbd0a654d0aae27a5b03378f3d487961eb67d
Matías Rodlo <m@hablaqui.com>
2024-04-20 14:02:24 -0400

    event handler as a prop

--- full-stack/frontend/react-II/components-renders.js
+++ full-stack/frontend/react-II/components-renders.js
@@ -152,3 +152,17 @@ function Talker() {
 }
 
 export default Talker;
+
+// Event hanlder as a prop
+
+import React from 'react';
+
+function Button(props) {
+  return (
+    <button onClick={props.talk}>
+      Click me!
+    </button>
+  );
+}
+
+export default Button;
