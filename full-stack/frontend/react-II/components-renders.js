3058d5e31b46c03f2a6ac071717f72e3fb7cd2be
Matías Rodlo <m@hablaqui.com>
2024-04-19 22:05:40 -0400

    access a component props

--- full-stack/frontend/react-II/components-renders.js
+++ full-stack/frontend/react-II/components-renders.js
@@ -29,3 +29,32 @@ function HelloWorld() {
 }
 
 export default HelloWorld;
+
+// Access a Component's props
+
+import React from 'react';
+
+function PropsDisplayer(props) {
+  const stringProps = JSON.stringify(props);
+  return (
+    <div>
+      <h1>CHECK OUT MY PROPS OBJECT</h1>
+      <h2>{stringProps}</h2>
+    </div>
+  );
+}
+
+export default PropsDisplayer;
+
+// app.js
+
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import PropsDisplayer from './PropsDisplayer';
+
+function App() {
+  return <PropsDisplayer />;
+}
+
+export default App;
