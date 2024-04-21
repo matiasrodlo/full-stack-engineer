ceca609be29695fff7168505d773641ec23140c2
Matías Rodlo <m@hablaqui.com>
2024-04-20 21:48:38 -0400

    initialize color

--- full-stack/frontend/react-II/hooks.js
+++ full-stack/frontend/react-II/hooks.js
@@ -33,3 +33,29 @@ export default function ColorPicker() {
     </div>
   );
 }
+
+// Initialize State
+
+import React, { useState } from 'react';
+
+const colorNames = ['Aquamarine', 'BlueViolet', 'Chartreuse', 'CornflowerBlue', 'Thistle', 'SpringGreen', 'SaddleBrown', 'PapayaWhip', 'MistyRose'];
+
+export default function ColorPicker() {
+  const [color = 'Tomato', setColor] = useState();
+
+ const divStyle = {backgroundColor: color};
+
+  return (
+    <div style={divStyle}>
+      <p>Selected color: {color}</p>
+      {colorNames.map((colorName)=>(
+        <button 
+          onClick={() => setColor(colorName)} 
+          key={colorName}>
+       	     {colorName}
+      	</button>
+      ))}
+    </div>
+  );
+}
+
