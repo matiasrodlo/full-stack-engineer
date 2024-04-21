172c292c2a592906f3ce8825fe439a6e3804e349
Matías Rodlo <m@hablaqui.com>
2024-04-21 13:31:32 -0400

    rules of reducers

--- full-stack/frontend/redux/core-concepts.js
+++ full-stack/frontend/redux/core-concepts.js
@@ -53,3 +53,72 @@ const reducer = (state = initialState, action) => {
   const removeAll = {
     type: 'songs/removeAll'
   }
+
+  // rules of reducers 1
+
+  // Reducer violates rule 1: 
+// They should only calculate the new state value based on the state and action arguments.
+ 
+const globalSong = 'We are the World';
+
+export const playlistReducer = (state = [], action) => {
+ switch (action.type) {
+   case 'songs/addGlobalSong': {
+     return [...state, action.payload];
+   }
+   default:
+     return state;
+ }
+}
+ 
+// Example call to reducer
+const state = [ 'Take Five', 'Claire de Lune', 'Respect' ];
+const addAction = { type: 'songs/addGlobalSong', payload: 'We are the World' };
+const newState = playlistReducer(state, addAction);
+
+// rules of reducers 2
+
+// Reducer violates rule 2: 
+// They are not allowed to modify the existing state. 
+// Instead, they must copy the existing state and make changes to the copied values.
+
+export const todoReducer = (state = [], action) => {
+    switch (action.type) {
+      case 'todos/addTodo': {
+        return [...state, action.payload];
+      }
+      case 'todos/removeAll': {
+        return [];
+      }
+      default: {
+        return state;
+      }
+    }
+   }
+   
+   // Example call to reducer
+   const state = [ 'Print trail map', 'Pack snacks', 'Summit the mountain' ];
+   const addTodoAction = { type: 'todos/addTodo', payload: 'Descend' };
+   const newState = todoReducer(state, addTodoAction);
+
+   // rules of reducers 3
+
+    // Reducer violates rule 3:
+ // They must not do any asynchronous logic or have other “side effects”.
+
+const initialState = [0, 1, 2];
+
+export const reducer = (state = initialState, action) => {
+ switch (action.type) {
+   case 'numbers/addRandom': {
+     return [...state, action.payload];
+   }
+   default: {
+     return state;
+   }
+ }
+}
+ 
+// Example call to reducer
+const randomAction = { type: 'numbers/addRandom', payload: Math.random() };
+const newState = reducer(undefined, randomAction);
