158677e3ce0b979950e0a0548f7475a47d76c970
Matías Rodlo <m@hablaqui.com>
2024-04-21 17:43:05 -0400

    connect redux store to ui

--- full-stack/frontend/redux/redux-api.js
+++ full-stack/frontend/redux/redux-api.js
@@ -92,3 +92,29 @@ store.subscribe(printCountStatus);
 store.dispatch(decrement()); // store.getState() === -1
 store.dispatch(increment()); // store.getState() === 0
 store.dispatch(increment()); // store.getState() === 1
+
+// Connect the Redux Store to a UI
+ 
+import React from "react";
+import { increment, decrement } from "./store";
+
+function App({ state, dispatch}) {
+  // Dispatch increment action
+  const incrementerClicked = () => {
+    dispatch(increment());
+  }
+  // Dispatch decrement action
+  const decrementerClicked = () => {
+    dispatch(decrement());
+  }
+
+  return(
+    <main>
+      <p id='counter'>{state}</p>
+      <button id='incrementer' onClick={incrementerClicked}>+</button>
+      <button id='decrementer' onClick={decrementerClicked}>-</button>
+    </main>
+  )
+}
+
+export default App;
