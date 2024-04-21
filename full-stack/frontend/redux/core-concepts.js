9b1a1ceb305a2e379b2e325c230244abf4d082c0
Matías Rodlo <m@hablaqui.com>
2024-04-21 13:10:43 -0400

    actions

--- full-stack/frontend/redux/core-concepts.js
+++ full-stack/frontend/redux/core-concepts.js
@@ -1,3 +1,21 @@
 // state
 
 const state = [ 'Take Five', 'Clair de Lune', 'Respect' ];
+
+// actions
+
+const state = [ 'Take Five', 'Claire de Lune', 'Respect' ];
+
+const addNewSong = {
+  type: 'songs/addSong',
+  payload: 'Halo'
+}
+
+const removeSong = {
+  type: 'songs/removeSong',
+  payload: 'Take Five'
+}
+
+const removeAll = {
+  type: 'songs/removeAll'
+}
