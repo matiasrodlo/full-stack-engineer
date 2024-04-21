a5a40c9465a5e6b852084409356cbfb139ffe28f
Matías Rodlo <m@hablaqui.com>
2024-04-21 13:17:34 -0400

    reducers

--- full-stack/frontend/redux/core-concepts.js
+++ full-stack/frontend/redux/core-concepts.js
@@ -19,3 +19,37 @@ const removeSong = {
 const removeAll = {
   type: 'songs/removeAll'
 }
+
+// reducers
+
+// Define reducer here
+const reducer = (state = initialState, action) => {
+    switch (action.type) {
+      case 'songs/addSong': {
+        return [...state, action.payload]
+      }
+      case 'songs/removeSong': {
+        return state.filter(song => song !== action.payload);
+      }
+      default: {
+        return state;
+      }
+    }
+  }
+  
+  
+  const initialState = [ 'Take Five', 'Claire de Lune', 'Respect' ];
+  
+  const addNewSong = {
+    type: 'songs/addSong',
+    payload: 'Halo'
+  };
+  
+  const removeSong = {
+    type: 'songs/removeSong',
+    payload: 'Take Five'
+  };
+  
+  const removeAll = {
+    type: 'songs/removeAll'
+  }
