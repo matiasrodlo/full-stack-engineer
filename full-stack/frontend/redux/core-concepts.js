51bd4a48146bdd2fa2c0008e4052caaea27da509
Matías Rodlo <m@hablaqui.com>
2024-04-21 13:39:42 -0400

    inmutables updates

--- full-stack/frontend/redux/core-concepts.js
+++ full-stack/frontend/redux/core-concepts.js
@@ -122,3 +122,41 @@ export const reducer = (state = initialState, action) => {
 // Example call to reducer
 const randomAction = { type: 'numbers/addRandom', payload: Math.random() };
 const newState = reducer(undefined, randomAction);
+
+// inmutable updates
+
+import React from 'react';
+
+//Modify mutable function
+export const removeItemAtIndex = (list, index) => {
+  return [
+    ...list.slice(0, index),
+    ...list.slice(index + 1, list.length)
+  ];
+};
+
+//Modify impure function
+export const generateUniqueId = (timestamp, random) => {
+  return timestamp + random;
+};
+
+const App = () => {
+  //Make your function calls here
+  const result = removeItemAtIndex(['a', 'b', 'c', 'd'], 1);
+
+  const timestamp = Date.now();
+  const random = Math.floor(Math.random() * 1000);
+  const uniqueId = generateUniqueId(timestamp, random);
+  
+  //Don't touch the content below this!
+  return (
+    <div>
+      <h1>Remove Item at Index</h1>
+      <p>Output: {result.join(', ')}</p>
+      <h1>Unique ID:</h1>
+      <p>Output: {uniqueId}</p>
+    </div>
+  );
+};
+
+export default App;
