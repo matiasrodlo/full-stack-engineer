17fbc1a2dc9fb4c4bb5a6ac8f40f060197adcdde
Matías Rodlo <m@hablaqui.com>
2024-04-21 00:49:57 -0400

    state setter outside jsx

--- full-stack/frontend/react-II/hooks.js
+++ full-stack/frontend/react-II/hooks.js
@@ -59,3 +59,29 @@ export default function ColorPicker() {
   );
 }
 
+// Use State Setter Outside of JSX
+
+import React, { useState } from "react";
+
+const validPhoneNumber = /^\d{1,10}$/;
+
+export default function PhoneNumber() {
+  const [phone, setPhone] = useState('');
+  
+   const handleChange = ({ target })=> {
+     const newPhone = target.value;
+     const isValid = validPhoneNumber.test(newPhone);
+     if (isValid) {
+       setPhone(newPhone);
+     }
+
+    };
+
+  return (
+    <div className='phone'>
+      <label for='phone-input'>Phone: </label>
+      <input value={phone} onChange={handleChange} id='phone-input' />
+    </div>
+  );
+}
+
