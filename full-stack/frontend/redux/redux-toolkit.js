a7b885da5f197f6c387dd035b016e3618957d6d4
Matías Rodlo <m@hablaqui.com>
2024-04-22 13:07:02 -0400

    returned objects and reducers

--- full-stack/frontend/redux/redux-toolkit.js
+++ full-stack/frontend/redux/redux-toolkit.js
@@ -95,3 +95,37 @@ export const selectFilteredFavoriteRecipes = (state) => {
 
 /*Begin your code here*/
 export const {addRecipe, removeRecipe} = favoriteRecipesSlice.actions;
+
+// Returned Objects and Reducers
+
+import { createSlice } from '@reduxjs/toolkit';
+import { selectSearchTerm } from './searchTermSlice.js';
+
+export const favoriteRecipesSlice = createSlice({
+  name: "favoriteRecipes",
+  initialState: [],
+  reducers: {
+    addRecipe: (state, action) => {
+      state.push(action.payload);
+    },
+    removeRecipe: (state, action) => {
+      return state.filter(recipe => recipe.id !== action.payload.id)
+    },
+  },
+});
+
+export const selectFavoriteRecipes = (state) => state.favoriteRecipes;
+
+export const selectFilteredFavoriteRecipes = (state) => {
+  const favoriteRecipes = selectFavoriteRecipes(state);
+  const searchTerm = selectSearchTerm(state);
+
+  return favoriteRecipes.filter((recipe) =>
+    recipe.name.toLowerCase().includes(searchTerm.toLowerCase())
+  );
+};
+
+export const {addRecipe, removeRecipe} = favoriteRecipesSlice.actions;
+
+// Begin writing code here.
+export default favoriteRecipesSlice.reducer
