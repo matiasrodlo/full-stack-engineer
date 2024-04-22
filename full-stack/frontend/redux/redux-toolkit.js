d05f496b39b44c4233a378d93981e38089588c55
Matías Rodlo <m@hablaqui.com>
2024-04-22 13:14:42 -0400

    cfgStore

--- full-stack/frontend/redux/redux-toolkit.js
+++ full-stack/frontend/redux/redux-toolkit.js
@@ -129,3 +129,19 @@ export const {addRecipe, removeRecipe} = favoriteRecipesSlice.actions;
 
 // Begin writing code here.
 export default favoriteRecipesSlice.reducer
+
+// converting the store to use cfgStore
+
+import { createStore, combineReducers } from 'redux';
+import { configureStore } from "@reduxjs/toolkit";
+import favoriteRecipesReducer from "../features/favoriteRecipes/favoriteRecipesSlice";
+import { searchTermReducer } from '../features/searchTerm/searchTermSlice.js';
+import { allRecipesReducer } from '../features/allRecipes/allRecipesSlice.js';
+
+export const store = configureStore({
+  reducer: {
+    favoriteRecipes: favoriteRecipesReducer,
+    searchTerm: searchTermReducer,
+    allRecipes: allRecipesReducer
+  },
+});
