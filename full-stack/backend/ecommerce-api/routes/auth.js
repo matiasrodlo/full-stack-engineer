66da3f68b46c282b81eeb11ef3f16ec29b278bab
Matías Rodlo <m@hablaqui.com>
2024-05-06 23:14:05 -0400

    CRUD with JWT

--- full-stack/backend/ecommerce-api/routes/auth.js
+++ full-stack/backend/ecommerce-api/routes/auth.js
@@ -24,12 +24,19 @@ router.post("/register", async (req, res) => {
 router.post("/login", async (req, res) => {
   try {
     const user = await User.findOne({username: req.body.username });
-    !user && res.status(401).json("Wrong credentials!")
+    !user && res.status(401).json("Wrong credentials!");
     const hashedPassword = CryptoJS.AES.decrypt(user.password, process.env.PASS_SEC);
     const Originalpassword = hashedPassword.toString(CryptoJS.enc.Utf8);
     originalpassword !==req.body.password && res.status(401).json("Wrong credentials!");
+    
+    const accessToekn = jwt.sign({
+      id:user._id, isAdmin: user.isAdmin,
+    }, process.env.JWT_SEC, 
+    {expiresIn: "3d"}
+  );
+
     const {password, ...others} = user._doc;
-    res.status(200).json(user)
+    res.status(200).json(...others, accessToken)
   } catch(err) {
     res.status(500).json(err)
   }
