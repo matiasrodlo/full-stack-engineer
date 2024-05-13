d69cf1a71885593783cdfc465ac4947ed277b3bc
Matías Rodlo <m@hablaqui.com>
2024-05-13 00:00:01 -0400

    user auth backned

--- full-stack/backend/ecommerce-api/routes/user.js
+++ full-stack/backend/ecommerce-api/routes/user.js
@@ -1,5 +1,5 @@
 const User = require("../models/User");
-const { verifyTokenAuthorization } = require("./verifyToken");
+const { verifyTokenAuthorization, verifyTokenAdmin } = require("./verifyToken");
 
 const router = require("express").Router();
 
@@ -18,9 +18,42 @@ router.put("/:id", verifyTokenAuthorization, async (req, res) => {
       },
       { new: true }
     );
+    res.status(200).json(updateUser);
   } catch (err) {
     res.status(500).json(err);
   }
 });
+
+router.delete("/:id", verifyTokenAuthorization, async (req, res) => {
+  try {
+    await User.findByIdAndDelete(req.params.id);
+    res.status(200).json("User has been deleted...");
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+router.get("/", verifyTokenAdmin, async (req, res) => {
+  const query = req.query.new
+  try {
+    const users = query ? await User.find().sort({_id:-1}).limit(5) : await User.find();
+    res.status(200).json(users);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+router.get("/stats", verifyTokenAndAdmin, async(req, res) => {
+  const date = new Date();
+  const lastYear = new Date(date.setFullYear(date.getFullYear() - 1));
+
+  try{
+const data = await User.aggregate([
+  {$match: {createAt:}}
+])
+  } catch(Err) {
+    res.status(500).json(err);
+  }
+})
 
 module.exports = router;
--- full-stack/backend/ecommerce-api/routes/verifyToken.js
+++ full-stack/backend/ecommerce-api/routes/verifyToken.js
@@ -3,6 +3,7 @@ const jwt = require("jsonwebtoken");
 const verifyToken = (req, res, next) => {
   const authHeader = req.headers.token;
   if (authHeader) {
+    const token = authHeader.split(" ")[1];
     jwt.verify(token, process.env.JWT_SEC, (err, user) => {
       if (err) res.status(403).json("Token is not valid!");
       req.user = user;
@@ -17,10 +18,20 @@ const verifyTokenAuthorization = (req, res, next) => {
   verifyToken(req, res, () => {
     if (req.user.id === req.params.id || req.user.isAdmin) {
       next();
-    } else {   
+    } else {
       res.status(403).json("You are not allowed to do that!");
     }
   });
 };
 
-module.exports = { verifyToken, verifyTokenAuthorization };
+const verifyTokenAdmin = (req, res, next) => {
+  verifyToken(req, res, () => {
+    if (req.user.isAdmin) {
+      next();
+    } else {
+      res.status(403).json("You are not allowed to do that!");
+    }
+  });
+};
+
+module.exports = { verifyToken, verifyTokenAuthorization, verifyTokenAdmin };
