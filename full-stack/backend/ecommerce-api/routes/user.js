264845f065ebc04d6b8ddfc0b84148161dafa41c
Matías Rodlo <m@hablaqui.com>
2024-05-12 23:04:54 -0400

    prettier documment formant

--- full-stack/backend/ecommerce-api/routes/user.js
+++ full-stack/backend/ecommerce-api/routes/user.js
@@ -1,7 +1,26 @@
-const router =  require("express").Router();
+const User = require("../models/User");
+const { verifyTokenAuthorization } = require("./verifyToken");
 
-router.put("/:id", )
+const router = require("express").Router();
 
-module.exports = router
+router.put("/:id", verifyTokenAuthorization, async (req, res) => {
+  if (req.body.password) {
+    req.body.password = CryptoJS.AES.encrypt(
+      req.body.password,
+      process.env.PASS_SEC
+    ).toString();
+  }
+  try {
+    const updatedUser = await User.findByIdAndDelete(
+      req.params.id,
+      {
+        $set: req.body,
+      },
+      { new: true }
+    );
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
 
-58.25
+module.exports = router;
