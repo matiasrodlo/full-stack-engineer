fba24ff7bfac1a377e465b991f850eeab7827fb7
Matías Rodlo <m@hablaqui.com>
2024-05-28 23:05:07 -0400

    add product

--- full-stack/backend/ecommerce-api/index.js
+++ full-stack/backend/ecommerce-api/index.js
@@ -1,18 +1,25 @@
 const express = require("express");
 const app = express();
-const dotenv = require("dotenv")
-const mongoose = require("mongoose")
-const userRoute = require("./routes/user")
-const authRoute = require("./routes/auth")
+const dotenv = require("dotenv");
+const mongoose = require("mongoose");
+const userRoute = require("./routes/user");
+const authRoute = require("./routes/auth");
+const productRoute = require("./routes/product");
 
 dotenv.config();
 
-mongoose.connect(process.env.MONGO_URL).then(() => console.log("DB Connection Successfull")).catch((err) => {console.log(err);})
+mongoose
+  .connect(process.env.MONGO_URL)
+  .then(() => console.log("DB Connection Successfull"))
+  .catch((err) => {
+    console.log(err);
+  });
 
 app.use(express.json());
 app.use("/api/auth", authRoute);
 app.use("/api/users", userRoute);
+app.use("/api/products", productRoute);
 
-app.listen(process.env.PORT || 4000, () => { 
-    console.log("backend server is running"); 
+app.listen(process.env.PORT || 4000, () => {
+  console.log("backend server is running");
 });
--- full-stack/backend/ecommerce-api/routes/product.js
+++ full-stack/backend/ecommerce-api/routes/product.js
@@ -1,3 +1,88 @@
-const router =  require("express").Router();
+const Product = require("../models/Product");
+const { verifyTokenAuthorization, verifyTokenAdmin } = require("./verifyToken");
 
-module.exports = router
+const router = require("express").Router();
+
+// CREATE
+
+route.post("/", verifyTokenAdmin, async (req, res) => {
+  const newProduct = new Product(req.body);
+
+  try {
+    const savedProduct = await newProduct;
+    res.status(200).json(savedProduct);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+/*router.put("/:id", verifyTokenAuthorization, async (req, res) => {
+  if (req.body.password) {
+    req.body.password = CryptoJS.AES.encrypt(
+      req.body.password,
+      process.env.PASS_SEC
+    ).toString();
+  }
+  try {
+    const updatedUser = await User.findByIdAndDelete(
+      req.params.id,
+      {
+        $set: req.body,
+      },
+      { new: true }
+    );
+    res.status(200).json(updateUser);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+router.get("/find/:id", verifyTokenAdmin, async (req, res) => {
+  try {
+    const users = await User.find();
+    const { password, ...others } = user._doc;
+    res.status(200).json(users);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+router.get("/", verifyTokenAdmin, async (req, res) => {
+  const query = req.query.new;
+  try {
+    const users = query
+      ? await User.find().sort({ _id: -1 }).limit(5)
+      : await User.find();
+    res.status(200).json(users);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+router.get("/stats", verifyTokenAndAdmin, async (req, res) => {
+  const date = new Date();
+  const lastYear = new Date(date.setFullYear(date.getFullYear() - 1));
+
+  try {
+    const data = await User.aggregate([
+      { $match: { createAt: { $gte: lastYear } } },
+      {
+        $project: {
+          month: { $month: "$createdAt" },
+        },
+      },
+      {
+        $group: {
+          _id: "$month",
+          total: { $sum: 1 },
+        },
+      },
+    ]);
+    res.status(200).json(data);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+module.exports = router;
+*/
