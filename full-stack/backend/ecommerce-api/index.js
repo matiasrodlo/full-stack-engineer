6a544c598951b67143aa16d27544b8667ec1b1f4
Matías Rodlo <m@hablaqui.com>
2024-05-29 21:39:48 -0400

    order

--- full-stack/backend/ecommerce-api/index.js
+++ full-stack/backend/ecommerce-api/index.js
@@ -5,7 +5,8 @@ const mongoose = require("mongoose");
 const userRoute = require("./routes/user");
 const authRoute = require("./routes/auth");
 const productRoute = require("./routes/product");
-
+const cartRoute = require("./routes/cart");
+const orderRoute = require("/routes/order");
 dotenv.config();
 
 mongoose
@@ -19,6 +20,8 @@ app.use(express.json());
 app.use("/api/auth", authRoute);
 app.use("/api/users", userRoute);
 app.use("/api/products", productRoute);
+app.use("/api/carts", cartRoute);
+app.use("/api/orders", orderRoute);
 
 app.listen(process.env.PORT || 4000, () => {
   console.log("backend server is running");
--- full-stack/backend/ecommerce-api/routes/order.js
+++ full-stack/backend/ecommerce-api/routes/order.js
@@ -1,3 +1,95 @@
-const router =  require("express").Router();
+const Order = require("../models/Product");
+const {
+  verifyTokenAuthorization,
+  verifyTokenAdmin,
+  verifyToken,
+} = require("./verifyToken");
 
-module.exports = router
+const router = require("express").Router();
+
+// CREATE
+
+router.post("/", verifyToken, async (req, res) => {
+  const newOrder = new Car(req.body);
+
+  try {
+    const savedCar = await newCar;
+    res.status(200).json(savedCar);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+router.put("/:id", verifyTokenAdmin, async (req, res) => {
+  try {
+    const updatedCart = await Cart.findByIdAndDelete(
+      req.params.id,
+      {
+        $set: req.body,
+      },
+      { new: true }
+    );
+    res.status(200).json(updatedCart);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+router.delete("/:id", verifyTokenAuthorization, async (req, res) => {
+  try {
+    await Order.findByIdAndDelete(req.params.id);
+    res.status(200).json("Order has been deleted...");
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+router.get("/find/:id", verifyTokenAuthorization, async (req, res) => {
+  try {
+    const orders = await Order.find({ userId: req.params.userId });
+    res.status(200).json(orders);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+router.get("/", verifyTokenAdmin, async (req, res) => {
+  try {
+    const orders = await Order.find();
+    res.status(200).json(orders);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+router.get("/income", verifyTokenAdmin, async (req, res) => {
+  const today = new Date();
+  const previousMonth = new Date(today.setMonth(today.getMonth() - 1));
+
+  try {
+    const income = await Order.aggregate([
+      {
+        $match: {
+          createdAt: { $gte: previousMonth }, // Filter orders from previous month onwards
+        },
+      },
+      {
+        $project: {
+          month: { $month: "$createdAt" }, // Extract month from createdAt
+          sales: "$amount", // Include sales amount
+        },
+      },
+      {
+        $group: {
+          _id: "$month", // Group by month
+          total: { $sum: "$sales" }, // Calculate total sales for each month
+        },
+      },
+    ]);
+    res.status(200).json(income);
+  } catch (err) {
+    res.status(500).json({ message: "Error fetching income data", error: err });
+  }
+});
+
+module.exports = router;
