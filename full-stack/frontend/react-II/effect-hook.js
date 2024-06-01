86cf8617f7c24c5ab713b6f20551e0508d86c8d4
Matías Rodlo <m@hablaqui.com>
2024-05-31 23:27:13 -0400

    cleanup effects

--- full-stack/frontend/react-II/effect-hook.js
+++ full-stack/frontend/react-II/effect-hook.js
@@ -20,3 +20,24 @@ export default function Counter() {
     </div>
   );
 }
+
+// Cleanup Effects
+
+import React, { useState, useEffect } from 'react';
+
+export default function Counter() {
+  const [clickCount, setClickCount] = useState(0);
+
+  const increment = () => setClickCount((prev) => prev + 1);
+
+  useEffect(() => {
+    document.addEventListener('mousedown', increment);
+    return () => {
+      document.removeEventListener('mousedown', increment);
+    };
+  });
+
+  return (
+      <h1>Document Clicks: {clickCount}</h1>
+  );
+}
