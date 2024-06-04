bace21ce775761f29809d0fc32a6d8c0109220f2
Matías Rodlo <m@hablaqui.com>
2024-06-04 18:56:56 -0400

    pasing thoughts initial commit

---
+++ full-stack/frontend/react-II/passing-thoughts/add-thoughts.js
@@ -0,0 +1,28 @@
+import React, { useState } from "react";
+import { generateId, getNewExpirationTime } from "./utilities";
+
+export function AddThoughtForm(props) {
+  const [text, setText] = useState("");
+
+  const handleTextChange = ({ target }) => {
+    const { value } = target;
+    setText(value);
+  };
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+  };
+
+  return (
+    <form onSubmit={handleSubmit} className="AddThoughtForm">
+      <input
+        onChange={handletextChange}
+        value={text}
+        type="text"
+        aria-label="What's on your mind?"
+        placeholder="What's on your mind?"
+      />
+      <input type="submit" value="Add" />
+    </form>
+  );
+}
