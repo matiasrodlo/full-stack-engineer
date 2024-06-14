6179e33431644e2aab071cd0cc5bffc4363b956f
Matías Rodlo <m@hablaqui.com>
2024-06-14 01:13:07 -0400

    introduction to redux

--- full-stack/frontend/redux/async-actions/index.js
+++ full-stack/frontend/redux/async-actions/index.js
@@ -21,3 +21,21 @@ store.dispatch({
   type: "NEW_MESSAGE",
   payload: "I WROTE A MIDDLEWARE",
 });
+
+// introduction to thunks
+
+const remindMeTo = (task) => {
+  return `Remember to ${task}!!!`;
+};
+
+const remindMeLater = (task) => {
+  return () => {
+    // call remindMeTo
+    return remindMeTo(task);
+  };
+};
+
+console.log(remindMeTo("call mom"));
+
+const reminder = remindMeLater("get groceries");
+console.log(reminder());
