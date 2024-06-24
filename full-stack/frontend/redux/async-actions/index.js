29fb95d56745b6dd5e0011b6caba9446903aea57
Matías Rodlo <m@hablaqui.com>
2024-06-23 23:32:10 -0400

    createAsyncThunk

--- full-stack/frontend/redux/async-actions/index.js
+++ full-stack/frontend/redux/async-actions/index.js
@@ -62,3 +62,29 @@ export const allRecipesSlice = createSlice({
 });
 
 export default allRecipesSlice.reducer;
+
+// createAsyncThunk
+
+import { fetchRecipes } from '../../app/api'
+import { createSlice, createAsyncThunk } from '@reduxjs/toolkit';
+
+
+const loadRecipes = createAsyncThunk('allRecipes/loadRecipes', const data = await fetchRecipes()  // fetch data
+const json = await data.json()  // transform to JSON
+return json  // return data as JSON);
+
+export const allRecipesSlice = createSlice({
+  name: 'allRecipes',
+  initialState: {
+    recipes: [],
+    isLoading: false,
+    hasError: false,
+  },
+  reducers: {
+    addRecipes(state, action) {
+      state.recipes = action.payload
+    }
+  },  
+});
+
+export default allRecipesSlice.reducer;
