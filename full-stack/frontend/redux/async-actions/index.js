f7a4b5955549f51a87d4d1bd29df5f3a1e87f5f0
Matías Rodlo <m@hablaqui.com>
2024-06-25 20:08:18 -0400

    createSlice

--- full-stack/frontend/redux/async-actions/index.js
+++ full-stack/frontend/redux/async-actions/index.js
@@ -119,3 +119,63 @@ const loadRecipes = createAsyncThunk(
 // 1. allRecipes/loadRecipes/pending
 // 2. allRecipes/loadRecipes/fulfilled
 // 3. allRecipes/loadRecipes/rejected
+
+// Using createAlice() with async action creator
+
+import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
+import {
+  addFavoriteRecipe,
+  removeFavoriteRecipe,
+} from "../favoriteRecipes/favoriteRecipesSlice";
+import { selectSearchTerm } from "../search/searchSlice";
+export const loadRecipes = createAsyncThunk(
+  "allRecipes/getAllRecipes",
+  async () => {
+    const data = await fetch("api/recipes?limit=10");
+    const json = await data.json();
+    return json;
+  }
+);
+
+const sliceOptions = {
+  name: "allRecipes",
+  initialState: {
+    recipes: [],
+    isLoading: false,
+    hasError: false,
+  },
+  reducers: {},
+  extraReducers: {
+    [loadRecipes.pending]: (state, action) => {
+      // fill out function body
+      state.isLoading = true;
+      state.hasError = false;
+    },
+    [loadRecipes.fulfilled]: (state, action) => {
+      // fill out function body
+      state.isLoading = false;
+      state.hasError = false;
+      state.recipes = action.payload;
+    },
+    [loadRecipes.rejected]: (state, action) => {
+      // fill out function body
+      state.isLoading = false;
+      state.hasError = true;
+    },
+  },
+};
+
+export const allRecipesSlice = createSlice(sliceOptions);
+
+export const selectAllRecipes = (state) => state.allRecipes.recipes;
+
+export const selectFilteredAllRecipes = (state) => {
+  const allRecipes = selectAllRecipes(state);
+  const searchTerm = selectSearchTerm(state);
+
+  return allRecipes.filter((recipe) =>
+    recipe.name.toLowerCase().includes(searchTerm.toLowerCase())
+  );
+};
+
+export default allRecipesSlice.reducer;
