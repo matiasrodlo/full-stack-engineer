5b27bf590e558a17ae020b003fc57d88bea5f33e
Matías Rodlo <m@hablaqui.com>
2024-06-26 17:06:58 -0400

    jamming app done

--- full-stack/frontend/react-II/jamming/client/src/App.js
+++ full-stack/frontend/react-II/jamming/client/src/App.js
@@ -10,6 +10,7 @@ import {
   Card,
 } from "react-bootstrap";
 import { useState, useEffect } from "react";
+const [albums, setAlbums] = useState([]);
 
 const CLIENT_ID = "XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX";
 const CLIENT_SECRET = "XXXXXXXXXXXXXXXXXXXXXXXXXXXX";
@@ -42,7 +43,7 @@ function App() {
     console.log("Search for " + searchInput); // Taylor Swift
 
     //Get request using  Artist ID
-    var artistParameters = {
+    var searchParameters = {
       method: "GET",
       headers: {
         "Content-Type": "application/json",
@@ -51,11 +52,31 @@ function App() {
     };
     var artistID = await fetch(
       "https://api.spotify.com/v1/search?q=" + searchInput + "&type=artist",
-      artistParameters
+      searchParameters
     )
       .then((response) => response.json())
-      .then((data) => console.log(data));
+      .then((data) => {
+        return data.artist.items[0].id;
+      });
+
+    console.log("Artist ID is " + artistID);
+
+    // get request with Artist ID
+    var albums = await fetch(
+      "https://api.spotify.com/v1/artists" +
+        artistID +
+        "/albums" +
+        "?include_groups=album&market=US&limit=50"
+    )
+      .then((response) => response.json())
+      .then((data) => {
+        console.log(data);
+        setAlbums(data.items);
+      });
+    // Display those albums to the user
   }
+
+  console.log(albums);
 
   return (
     <div className="App">
@@ -76,12 +97,17 @@ function App() {
       </Container>
       <Container>
         <Row className="mx-2 row row-cols-4">
-          <Card>
-            <Card.Img src="#" />
-            <Card.Body>
-              <Card.Title>Album Name Here</Card.Title>
-            </Card.Body>
-          </Card>
+          {albums.map((album, i) => {
+            console.log(album);
+            return (
+              <Card>
+                <Card.Img src={album.images[0].url} />
+                <Card.Body>
+                  <Card.Title>{album.name}</Card.Title>
+                </Card.Body>
+              </Card>
+            );
+          })}
         </Row>
       </Container>
     </div>
